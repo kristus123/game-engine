ae85f04a8f14a20fc19537f497acee37b4ff3768
kristus123 <krispetter@gmail.com>
2023-08-21 12:47:01 +0200

    remove test things

--- static/LevelOne.js
+++ static/LevelOne.js
@@ -4,9 +4,7 @@ import { Player } from '/static/Player.js';
 import { Projectile } from '/static/Projectile.js';
 import { Camera } from '/static/Camera.js';
 import { Loop } from '/static/Loop.js';
-import { Circle } from '/static/Circle.js';
 import { Draw } from '/static/Draw.js';
-import { PrettyParticles } from '/static/PrettyParticles.js';
 
 export class LevelOne {
 	constructor() {
@@ -22,7 +20,6 @@ export class LevelOne {
 
 		this.camera = new Camera(width, height, [this.world.ctx])
 		this.player = new Player()
-		this.prettyParticles = new PrettyParticles()
 		this.projectile = new Projectile(750, 360, 10, "red")
 
 		this.physics = new Physics()
@@ -30,37 +27,20 @@ export class LevelOne {
 		this.physics.applyTo(this.projectile)
 
 		document.addEventListener('click', (e) => {
-			console.log("hehiehi")
 			this.projectile.shoot(this.camera.mousePosition(e))
 		})
 	}
 
 	runFrame(deltaTime) {
+		this.world.ctx.fillStyle = "black"
+		this.world.ctx.fillRect(0, 0, this.width, this.height)
+
+		this.physics.update(deltaTime)
+
 		this.camera.context(() => {
-
-			this.world.ctx.fillStyle = "black"
-			this.world.ctx.fillRect(0, 0, this.width, this.height)
-
-			this.physics.update(deltaTime)
-			this.camera.follow(this.player) // Keep this after physics.update
-
-			this.prettyParticles.create(this.player.x, this.player.y)
-			Draw.objectThatIsMovingInRectangularPathAroundObject(
-				this.world.ctx, this.player, this.camera.currentMousePosition)
-
+			this.camera.follow(this.player) // Keep this after physics.update and within camera.context
 			this.physics.objects.forEach(o => o.update(deltaTime))
-			this.prettyParticles.draw(this.world.ctx)
 			this.physics.objects.forEach(o => o.draw(this.world.ctx))
-
-			const c = new Circle(10, 10, 200, 'red')
-			if (c.inside(this.player)) {
-				c.color = 'blue'
-				Draw.hollowCircle(this.world.ctx, 10, 10, 200)
-				Draw.text(this.world.ctx, 10, -200, 250, 100, 'Press "e" to buy"')
-			} else {
-				c.color = 'red'
-				c.draw(this.world.ctx)
-			}
 		})
 
 		Draw.text(this.gui.ctx, 10, 10, 100, 100, Loop.fps)
@@ -68,5 +48,4 @@ export class LevelOne {
 		this.world.ctx.drawImage(this.gui.canvas, 0, 0)
 		this.main.ctx.drawImage(this.world.canvas, 0, 0)
 	}
-	
 }
