335a5d3b6f10a5633087d4a7163475b5a8525e2d
kristus123 <krispetter@gmail.com>
2023-08-23 13:35:32 +0200

    x

--- static/Spaceship.js
+++ static/Spaceship.js
@@ -8,34 +8,21 @@ export class Spaceship extends GameObject {
 
 		this.player = player
 		this.entered = false
-
-		this.spent = {
-			inside: 0,
-			outside: 0,
-			
-		}
 	}
 
 	update() {
-		if (this.entered) {
-			this.spent.inside ++
-		} else {
-			this.spent.outside ++
-		}
+		if (!this.entered && this.player.keyboard.e && Distance.withinRadius(this.player, this, 100)) {
+			this.player.velocity = {
+				x: 0,
+				y: 0,
+			}
+			this.player.x = this.x
+			this.player.y = this.y
 
-		if (!this.entered && this.player.keyboard.e) {
 			this.entered = true
-			this.spent.outside = 0
-		}
-		else if (this.entered && this.player.keyboard.e) {
-			this.entered = false
 		}
 
 		if (this.entered) {
-			this.timeSpentInSpaceship++
-
-			this.x = this.player.x
-			this.y = this.player.y
 			this.velocity = this.player.velocity
 		}
 	}
@@ -45,10 +32,6 @@ export class Spaceship extends GameObject {
 
 		if (!this.entered && Distance.withinRadius(this.player, this, 100)) {
 			Draw.text(ctx, this.x, this.y, 150, 50, 'E to enter')
-		}
-		if (this.entered) {
-			Draw.text(ctx, this.x, this.y, 150, 50, 'E to exit')
-			
 		}
 	}
 
