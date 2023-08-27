bc200d0a7b851e137833ff05e611849bc144b599
kristus123 <krispetter@gmail.com>
2023-08-27 14:04:42 +0200

    x

--- static/Random.js
+++ static/Random.js
@@ -3,6 +3,12 @@ export class Random {
 		return Math.floor(Math.random() * (max - min + 1) + min)
 	}
 
+	static floatBetween(min, max) {
+		const randomValue = Math.random() * (max - min) + min;
+		return parseFloat(randomValue.toFixed(2));
+	}
+
+
 	static color() {
 		return `hsl(${Math.random() * 360}, 100%, 50%)`
 	}
