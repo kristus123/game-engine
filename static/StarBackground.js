db24b27be01a479ac544e57f19d1e5ffc4b0ba4b
kristus123 <krispetter@gmail.com>
2023-08-30 19:43:50 +0200

    x

---
+++ static/StarBackground.js
@@ -0,0 +1,34 @@
+import { Palette } from '/static/Palette.js'
+import { Random } from '/static/Random.js'
+import { Draw } from '/static/Draw.js'
+
+
+
+export class StarBackground {
+	constructor() {
+		this.palette = Palette.offscreen()
+
+		const numStars = 1000
+		this.stars = []
+		for (let i = 0; i < numStars; i++) {
+			const x = Random.numberBetween(-1000, 10000)
+			const y = Random.numberBetween(-1000, 10000)
+			const width = Math.random() * 1 + 1; // Varying star widths
+			const height = Math.random() * 1 + 1; // Varying star heights
+
+			this.stars.push({x, y, width, height})
+		}
+	}
+
+
+	draw(player) {
+		Palette.fill(this.palette, 'black')
+		this.palette.ctx.fillStyle = 'white'
+		this.stars.forEach(s => {
+			Draw.rectangle(this.palette.ctx, s.x - player.x / 100, s.y - player.y / 100, s.width, s.height)
+		})
+
+		return this.palette
+	}
+	
+}
