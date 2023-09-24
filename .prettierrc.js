9c2c28bb606ab6c2a884e4e1e7a354e716e1c518
kristus123 <krispetter@gmail.com>
2023-09-24 10:29:10 +0100

    x

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	tabWidth: 4, // Set the tab width to your desired number (e.g., 4 for 4 spaces)
-	useTabs: true, // Use tabs instead of spaces
-	semi: false, // Add semicolons at the end of statements (optional)
-	singleQuote: true, // Use single quotes for strings (optional)
-	trailingComma: 'all', // Add trailing commas to objects and arrays (optional)
-	arrowParens: 'always', // Add parentheses to arrow function parameters (optional)
-}
