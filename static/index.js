dae084c636da97ec462ab12778daf008cd127410
kristus123 <krispetter@gmail.com>
2023-09-26 16:56:50 +0100

    remove stars. no need for them now

--- static/index.js
+++ static/index.js
@@ -4,25 +4,10 @@ import { Camera } from '/static/Camera.js'
 import { Palette } from '/static/Palette.js'
 import { Mouse } from '/static/Mouse.js'
 import { LevelHandler } from '/static/LevelHandler.js'
-import { Random } from '/static/Random.js'
-import { Schedule } from '/static/Schedule.js'
-
-const numStars = 1000
-const stars = []
-for (let i = 0; i < numStars; i++) {
-	const x = Random.numberBetween(-1000, 10000)
-	const y = Random.numberBetween(-1000, 10000)
-	const width = Math.random() * 1 + 1 // Varying star widths
-	const height = Math.random() * 1 + 1 // Varying star heights
-
-	stars.push({ x, y, width, height })
-}
 
 const mainPalette = Palette.main()
 const guiPalette = Palette.offscreen()
 const backgroundPalette = Palette.offscreen()
-
-const schedule = new Schedule()
 
 let objectToFollow = { x: 0, y: 0 }
 const camera = new Camera()
@@ -41,26 +26,6 @@ Loop.everyFrame((deltaTime) => {
 		level.update()
 		level.draw(camera.palette.ctx)
 	})
-
-	schedule.everyFrame(
-		10,
-		() => {
-			// Palette.clear([backgroundPalette])
-			Palette.fill(backgroundPalette, 'black')
-			const player = level.outsideLevel.player
-			backgroundPalette.ctx.fillStyle = 'white'
-			stars.forEach((s) => {
-				Draw.rectangle(
-					backgroundPalette.ctx,
-					s.x - player.x / 100,
-					s.y - player.y / 100,
-					s.width,
-					s.height,
-				)
-			})
-		},
-		60 * 2,
-	)
 
 	Draw.text(guiPalette.ctx, 20, 20, 80, 80, Loop.fps)
 	Palette.apply(mainPalette, [backgroundPalette, camera.palette, guiPalette])
