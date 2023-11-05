22756642493d8840c0f849eeb97a20a1c78952a8
kristus123 <krispetter@gmail.com>
2023-11-05 20:47:19 +0000

    remove stars

--- static/index.js
+++ static/index.js
@@ -4,18 +4,6 @@ import { Camera } from '/static/Camera.js'
 import { Palette } from '/static/Palette.js'
 import { Mouse } from '/static/Mouse.js'
 import { LevelHandler } from '/static/LevelHandler.js'
-import { Random } from '/static/Random.js'
-
-const numStars = 1000
-const stars = []
-for (let i = 0; i < numStars; i++) {
-	const x = Random.numberBetween(-1000, 10000)
-	const y = Random.numberBetween(-1000, 10000)
-	const width = Math.random() * 1 + 1 // Varying star widths
-	const height = Math.random() * 1 + 1 // Varying star heights
-
-	stars.push({ x, y, width, height })
-}
 
 const mainPalette = Palette.main()
 const guiPalette = Palette.offscreen()
@@ -40,18 +28,6 @@ Loop.everyFrame((deltaTime) => {
 	})
 
 	Palette.fill(backgroundPalette, 'black')
-	const player = level.outsideLevel.player
-	backgroundPalette.ctx.fillStyle = 'white'
-	stars.forEach((s) => {
-		Draw.rectangle(
-			backgroundPalette.ctx,
-			s.x - player.x / 100,
-			s.y - player.y / 100,
-			s.width,
-			s.height,
-		)
-	})
-
 	Draw.text(guiPalette.ctx, 20, 20, 80, 80, Loop.fps)
 	Palette.apply(mainPalette, [backgroundPalette, camera.palette, guiPalette])
 })
