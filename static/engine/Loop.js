ff826c61bd1e3bc6f5360cbb4e6b7e8f83a641d4
kristus123 <krispetter@gmail.com>
2023-11-18 21:30:47 +0000

    changed Loop.js. this might fix the fps speedup bug

--- static/engine/Loop.js
+++ static/engine/Loop.js
@@ -1,26 +1,34 @@
 export class Loop {
-	static fps = 0
-	static lastUpdated = 0
+  static fps = 0;
+  static lastUpdated = 0;
 
-	static everyFrame(run) {
-		let lastTimestamp = performance.now()
+  static everyFrame(run, targetFPS = 60) {
+    const targetFrameTime = 1000 / targetFPS;
+    let lastTimestamp = performance.now();
+    let accumulatedTime = 0;
 
-		function loop(currentTimestamp) {
-			const deltaTime = (currentTimestamp - lastTimestamp) / 1000
-			lastTimestamp = currentTimestamp
+    function loop(currentTimestamp) {
+      const deltaTime = currentTimestamp - lastTimestamp;
+      lastTimestamp = currentTimestamp;
 
-			run(deltaTime)
-			if (Loop.lastUpdated > 10) {
-				Loop.fps = Math.floor(Math.floor(1000 / deltaTime) / 1000)
-				Loop.lastUpdated = 0
-			}
-			else {
-				Loop.lastUpdated += 1
-			}
+      accumulatedTime += deltaTime;
 
-			requestAnimationFrame(loop)
-		}
+      // Update the game logic at a fixed time step
+      while (accumulatedTime >= targetFrameTime) {
+        run(targetFrameTime / 1000); // Pass fixed time step to the game logic
+        accumulatedTime -= targetFrameTime;
+      }
 
-		requestAnimationFrame(loop)
-	}
+      if (Loop.lastUpdated > 10) {
+        Loop.fps = Math.floor(1000 / deltaTime);
+        Loop.lastUpdated = 0;
+      } else {
+        Loop.lastUpdated += 1;
+      }
+
+      requestAnimationFrame(loop);
+    }
+
+    requestAnimationFrame(loop);
+  }
 }
