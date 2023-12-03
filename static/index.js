881e70b5cf40a08fbc007fca939f92222d550f2d
kristus123 <krispetter@gmail.com>
2023-12-03 19:59:43 +0000

    x

--- static/index.js
+++ static/index.js
@@ -5,12 +5,13 @@ const guiPalette = Palette.offscreen()
 const backgroundPalette = Palette.offscreen()
 const starBackground = new StarBackground()
 
-let objectToFollow = { x: 0, y: 0 }
 const camera = new Camera()
-
 const mouse = new Mouse(camera)
 
-const level = new LevelHandler((f) => (objectToFollow = f), mouse)
+let objectToFollow = { x: 0, y: 0 }
+const cameraFollow = (f) => (objectToFollow = f)
+
+const level = new LevelHandler(cameraFollow, mouse)
 
 Loop.everyFrame((deltaTime) => {
 	Palette.clear([camera.palette, guiPalette])
