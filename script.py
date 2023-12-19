8a0e646657b169aa76863999dd8236bd3aa585c3
kristus123 <krispetter@gmail.com>
2023-12-19 20:33:51 +0000

    x

--- script.py
+++ script.py
@@ -3,17 +3,10 @@ from flesk import init_dist, new_dist_file
 
 init_dist()
 
-def get_all_js_files(directory_path):
-    js_files=[]
-
-    for root, _, files in os.walk(directory_path):
-        for file in files:
-            if file.endswith('.js'):
-                js_files.append(os.path.join(root, file))
-
-    return js_files
-
-folder_path = 'static'
-js_files = get_all_js_files(folder_path)
+js_files=[]
+for root, _, files in os.walk('static'):
+    for file in files:
+        if file.endswith('.js'):
+            js_files.append(os.path.join(root, file))
 
 new_dist_file("index.html", js_files=js_files)
