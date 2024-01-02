06d6df911af64c4d4aae5296fb669e852e7ffb23
awaismehr <awaismehr001@gmail.com>
2024-01-02 20:04:49 +0500

    added world.js file

--- static/game/MainLevel.js
+++ static/game/MainLevel.js
@@ -1,40 +1,37 @@
+
 export class MainLevel {
 	constructor(levelSelector, camera, mouse) {
-		this.levelSelector = levelSelector
-		this.camera = camera
-		this.mouse = mouse
-		this.controller = new Controller()
+	this.world=new World(levelSelector,camera,mouse)
 
-		this.player = new Player(mouse)
-		this.camera.follow(this.player)
-		this.controller.control(this.player)
+	this.npc = new Npc()
+	this.piss = new Piss(this.world.player, mouse, new Position(-1000, 100, 100, 100))
 
-		this.npc = new Npc()
+	this.piss.onFinish = () => {
+		// Push(this.player).towards(new Position(400, this.player.y), 1000)
 
-		this.piss = new Piss(this.player, this.mouse, new Position(-1000, 100, 100, 100))
-		this.piss.onFinish = () => {
-			Push(this.player).towards(new Position(400, this.player.y), 1000)
 		}
 
-		this.chat = new FirstChat(this.npc.position, this.piss, mouse)
+	this.chat = new FirstChat(this.npc.position, this.piss, mouse)
 
-		this.runAll = new RunAll('mainlevel', [
-			new StarBackground(),
-			// new Planets(),
-			this.player,
-			this.controller,
-			this.npc,
-			this.piss,
-			this.chat,
-			new Picture(this.npc, '/static/sun.png'),
-		])
-	}
+
+	this.runAll = new RunAll('mainlevel', [
+		this.npc,this.piss,this.world,this.chat
+	])
+
+
+
+}
+
+
+
 
 	update() {
+	
 		this.runAll.update()
+		
 	}
 
 	draw(ctx) {
-		this.runAll.draw(ctx)
+	this.runAll.draw(ctx)
 	}
 }
---
+++ static/game/World.js
@@ -0,0 +1,28 @@
+export class World {
+	constructor(levelSelector, camera, mouse) {
+		this.levelSelector = levelSelector
+		this.camera = camera
+		this.mouse = mouse
+		this.controller = new Controller()
+
+		this.player = new Player(mouse)
+		this.camera.follow(this.player)
+		this.controller.control(this.player)
+
+		this.runAll = new RunAll('mainlevel', [
+			new StarBackground(),
+			// new Planets(),
+			this.player,
+			this.controller,
+			// new Picture(this.npc, '/static/sun.png'),
+		])
+	}
+
+	update() {
+		this.runAll.update()
+	}
+
+	draw(ctx) {
+		this.runAll.draw(ctx)
+	}
+}
