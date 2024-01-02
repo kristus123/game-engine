8ac7f2c8220be7108c640bdfe3825146583915d4
kristus123 <krispetter@gmail.com>
2024-01-02 15:17:00 +0000

    x

--- static/game/MainLevel.js
+++ static/game/MainLevel.js
@@ -1,26 +1,26 @@
 
 export class MainLevel {
 	constructor(levelSelector, camera, mouse) {
-	this.world=new World(levelSelector,camera,mouse)
+		this.world=new World(levelSelector,camera,mouse)
 
-	this.npc = new Npc()
-	this.piss = new Piss(this.world.player, mouse, new Position(-1000, 100, 100, 100))
+		this.npc = new Npc()
+		this.piss = new Piss(this.world.player, mouse, new Position(-1000, 100, 100, 100))
 
-	this.piss.onFinish = () => {
+		this.piss.onFinish = () => {
 		// Push(this.player).towards(new Position(400, this.player.y), 1000)
 
 		}
 
-	this.chat = new FirstChat(this.npc.position, this.piss, mouse)
+		this.chat = new FirstChat(this.npc.position, this.piss, mouse)
 
 
-	this.runAll = new RunAll('mainlevel', [
-		this.npc,this.piss,this.world,this.chat
-	])
+		this.runAll = new RunAll('mainlevel', [
+			this.npc,this.piss,this.world,this.chat
+		])
 
 
 
-}
+	}
 
 
 
@@ -32,6 +32,6 @@ export class MainLevel {
 	}
 
 	draw(ctx) {
-	this.runAll.draw(ctx)
+		this.runAll.draw(ctx)
 	}
 }
