31c1f15597528b135024be661a67dce5e10c22b3
kristus123 <krispetter@gmail.com>
2024-01-12 22:05:03 +0000

    x

--- static/game/NextLevel.js
+++ static/game/NextLevel.js
@@ -1,19 +1,19 @@
 
 export class NextLevel {
-    constructor(world, npc, levelSelector) {
+	constructor(world, npc, levelSelector) {
 
-        this.runAll = new RunAll('NextLevel', [
-            world,
-            npc,
-            new ThirdChat(npc.position, world.mouse),
-        ])
-    }
+		this.runAll = new RunAll('NextLevel', [
+			world,
+			npc,
+			new ThirdChat(npc.position, world.mouse),
+		])
+	}
 
-    update() {
-        this.runAll.update()
-    }
+	update() {
+		this.runAll.update()
+	}
 
-    draw(ctx) {
-        this.runAll.draw(ctx)
-    }
+	draw(ctx) {
+		this.runAll.draw(ctx)
+	}
 }
