4dd3f19dbd9dbd230f793400752069d3f63194d4
Muhammad Awais <64785317+awaismehr@users.noreply.github.com>
2024-01-13 20:00:47 +0500

    Shoot level (#16)
    * added chicken class
    
    * solve sprite error
    
    * conflict resolved

--- static/game/Chicken.js
+++ static/game/Chicken.js
@@ -1,33 +1,33 @@
 
 export class Chicken extends GameObject {
-    constructor() {
-        super(-1500, -400, 150, 150, 10, 10)
+	constructor() {
+		super(-1500, -400, 150, 150, 10, 10)
 
-        const frameWidth = 32
-        const frameHeight = 32
-        const scale = 5;
-        const frameSequence = [
-            { x: 1, y: 0 },
-            { x: 2, y: 1 },
-            { x: 2, y: 2 },
-            { x: 3, y: 3 },
-            { x: 3, y: 3 },
-        ]
+		const frameWidth = 32
+		const frameHeight = 32
+		const scale = 5;
+		const frameSequence = [
+			{ x: 1, y: 0 },
+			{ x: 2, y: 1 },
+			{ x: 2, y: 2 },
+			{ x: 3, y: 3 },
+			{ x: 3, y: 3 },
+		]
 
-        this.sprite = new Sprite('/static/assets/Chicken_Sprite_Sheet.png', this, frameHeight, frameWidth, scale, frameSequence)
+		this.sprite = new Sprite('/static/assets/Chicken_Sprite_Sheet.png', this, frameHeight, frameWidth, scale, frameSequence)
 
-    }
+	}
 
-    onCollision(o) {
-        Push(this).awayFrom(o, 50)
-    }
+	onCollision(o) {
+		Push(this).awayFrom(o, 50)
+	}
 
-    update() {
-    }
+	update() {
+	}
 
-    draw(ctx) {
-        this.sprite.draw(ctx)
+	draw(ctx) {
+		this.sprite.draw(ctx)
 
 
-    }
+	}
 }
