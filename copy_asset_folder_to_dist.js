c020ab2b06e87af4a667975ef34520254ee8c100
kristus123 <krispetter@gmail.com>
2024-01-19 19:28:15 +0000

    x

--- copy_asset_folder_to_dist.js
+++ copy_asset_folder_to_dist.js
@@ -1,38 +1,39 @@
 
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-const sourceFolder = 'static/assets/';
-const destinationFolder = 'dist/static/assets/';
+const sourceFolder = 'static/assets/'
+const destinationFolder = 'dist/static/assets/'
 
 // Function to recursively copy a folder
 function copyFolderRecursive(source, destination) {
-  if (!fs.existsSync(destination)) {
-    fs.mkdirSync(destination);
-  }
+	if (!fs.existsSync(destination)) {
+		fs.mkdirSync(destination)
+	}
 
-  const files = fs.readdirSync(source);
+	const files = fs.readdirSync(source)
 
-  files.forEach((file) => {
-    const sourcePath = path.join(source, file);
-    const destinationPath = path.join(destination, file);
+	files.forEach((file) => {
+		const sourcePath = path.join(source, file)
+		const destinationPath = path.join(destination, file)
 
-    if (fs.statSync(sourcePath).isDirectory()) {
-      copyFolderRecursive(sourcePath, destinationPath);
-    } else {
-      fs.copyFileSync(sourcePath, destinationPath);
-    }
-  });
+		if (fs.statSync(sourcePath).isDirectory()) {
+			copyFolderRecursive(sourcePath, destinationPath)
+		}
+		else {
+			fs.copyFileSync(sourcePath, destinationPath)
+		}
+	})
 }
 
 // Ensure the source folder exists
 if (!fs.existsSync(sourceFolder)) {
-  console.error('Source folder does not exist.');
-  process.exit(1);
+	console.error('Source folder does not exist.')
+	process.exit(1)
 }
 
 // Copy the folder recursively
-copyFolderRecursive(sourceFolder, destinationFolder);
+copyFolderRecursive(sourceFolder, destinationFolder)
 
-console.log('Folder copied successfully!');
+console.log('Folder copied successfully!')
 
