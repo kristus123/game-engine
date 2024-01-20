0604245f201a2cc479f0e0df38b27d1170fdd806
kristus123 <krispetter@gmail.com>
2024-01-20 09:27:31 +0000

    lint

--- get_js_files.js
+++ get_js_files.js
@@ -1,24 +1,25 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-const jsFiles = [];
+const jsFiles = []
 
 function getJsFiles(dir) {
-    const files = fs.readdirSync(dir);
+	const files = fs.readdirSync(dir)
 
-    for (const file of files) {
-        const filePath = path.join(dir, file);
+	for (const file of files) {
+		const filePath = path.join(dir, file)
 
-        if (fs.statSync(filePath).isDirectory()) {
-            getJsFiles(filePath);
-        } else if (file.endsWith('.js')) {
-            jsFiles.push(filePath);
-        }
-    }
+		if (fs.statSync(filePath).isDirectory()) {
+			getJsFiles(filePath)
+		}
+		else if (file.endsWith('.js')) {
+			jsFiles.push(filePath)
+		}
+	}
 }
 
-getJsFiles('static');
-const sortedJsFiles = jsFiles.sort();
+getJsFiles('static')
+const sortedJsFiles = jsFiles.sort()
 
-console.log(sortedJsFiles);
+console.log(sortedJsFiles)
 
