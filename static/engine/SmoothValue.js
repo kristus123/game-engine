50832b83f27fb7e27506177a15cec3f1f4421bba
kristus123 <krispetter@gmail.com>
2024-01-25 19:17:21 +0000

    lint

--- static/engine/SmoothValue.js
+++ static/engine/SmoothValue.js
@@ -5,24 +5,25 @@
 // const s = new Smooth(0, 100, 0.01, 0.1);
 
 export class SmoothValue {
-    constructor(currentValue, targetValue, smoothness, threshold=0.0001) {
-        this.currentValue = currentValue;
-        this.targetValue = targetValue;
-        this.smoothness = smoothness;
-        this.threshold = threshold;
-    }
+	constructor(currentValue, targetValue, smoothness, threshold=0.0001) {
+		this.currentValue = currentValue
+		this.targetValue = targetValue
+		this.smoothness = smoothness
+		this.threshold = threshold
+	}
 
-    update() {
-        const diff = this.targetValue - this.currentValue;
+	update() {
+		const diff = this.targetValue - this.currentValue
 
-        if (Math.abs(diff) < this.threshold) {
-            this.currentValue = this.targetValue;
-        } else {
-            const delta = diff * this.smoothness;
+		if (Math.abs(diff) < this.threshold) {
+			this.currentValue = this.targetValue
+		}
+		else {
+			const delta = diff * this.smoothness
 
-            this.currentValue += delta;
-        }
+			this.currentValue += delta
+		}
 
-    }
+	}
 }
 
