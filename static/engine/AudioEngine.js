3ceb00e961a227307f4c74834ce4e6d477ba56bd
kristus123 <krispetter@gmail.com>
2024-01-25 12:25:49 +0000

    x

--- static/engine/AudioEngine.js
+++ static/engine/AudioEngine.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-undef, */ // Ignore Howl import warning
+
 export class AudioEngine {
+
 	static play() {
 		const sound = new Howl({
-		  src: ['/static/audio/intro.wav']
+			src: ['/static/audio/intro.wav']
 		})
 
 		sound.play()
