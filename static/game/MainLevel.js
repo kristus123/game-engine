60d72664680745cf89638b59ca27d53e9c6c9b32
Samuel Tesfaye <153354283+samueltesfayegari@users.noreply.github.com>
2024-01-26 20:43:48 +0300

    Update MainLevel.js

--- static/game/MainLevel.js
+++ static/game/MainLevel.js
@@ -6,9 +6,13 @@ export class MainLevel {
 
 		this.levelSelector = levelSelector
 
+		this.taskTracker = new TaskTracker();
+
+
 		this.piss = new Piss(this.world.player, mouse, new Position(-1000, 100, 100, 100))
 		this.piss.onFinish = () => {
 			levelSelector.changeActiveLevel(new DeliverPissLevel(this.world, this.npc, levelSelector))
+			this.taskTracker.completeTask()
 		}
 
 		this.runAll = new RunAll([
@@ -20,8 +24,11 @@ export class MainLevel {
 	}
 
 	update() {
-		this.levelSelector.changeActiveLevel(new ShootChickensLevel(this.world))
-		this.runAll.update()
+		if (this.taskTracker.isTaskCompleted())
+		{
+			this.levelSelector.changeActiveLevel(new ShootChickensLevel(this.world))
+			this.runAll.update()
+		}
 	}
 
 	draw(draw) {
