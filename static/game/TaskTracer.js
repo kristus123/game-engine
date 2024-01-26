64d20d8d0f6c1d46037b51c432fab4bb33f0c0ea
Samuel Tesfaye <153354283+samueltesfayegari@users.noreply.github.com>
2024-01-26 19:55:06 +0300

    Clear all in TaskTracer.js

--- static/game/TaskTracer.js
+++ static/game/TaskTracer.js
@@ -1,15 +1,1 @@
-class TaskTracker {
-    constructor() {
-        this.taskCompleted = false;
-    }
 
-    // Method to be called when the task is successfully completed
-    completeTask() {
-        this.taskCompleted = true;
-    }
-
-    // Getter method to check if the task is completed
-    isTaskCompleted() {
-        return this.taskCompleted;
-    }
-}
