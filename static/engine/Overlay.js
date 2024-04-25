f05e4f660fa680a289a2e3508a15c39506241a81
kristus123 <krispetter@gmail.com>
2024-04-26 00:42:44 +0100

    x

--- static/engine/Overlay.js
+++ static/engine/Overlay.js
@@ -1,54 +1,21 @@
-const html = `
-<style>
-  .grid-container {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
-    gap: 20px;
-    overflow: auto; /* Add scrollbar when content overflows */
-    max-height: 80vh; /* Limit maximum height to 80% of viewport height */
-  }
-  .grid-item {
-    background-color: #ccc;
-    padding: 20px;
-    text-align: center;
-    height:100px;
-  }
-  .container {
-    display: flex;
-    flex-wrap: wrap;
-  }
-  .box {
-    width: 600px;
-    padding: 20px;
-    box-sizing: border-box;
-    margin: 20px;
-  }
-</style>
+function fetchHTML() {
+const xhr = new XMLHttpRequest();
+xhr.open('GET', '/static/gui/test.html', false);
+xhr.send();
 
-<div tabindex="-1" style="user-select: none; position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); display: flex; justify-content: center; align-items: center;">
+if (xhr.status === 200) {
+  const html = xhr.responseText;
+	return html
+} else {
+  console.error('Error fetching HTML:', xhr.statusText);
+}
 
-  <div style="text-align: center; color: white;">
-    <div class="container">
-      <div class="box" style="background-color: black;">
-        <h1>hei</h1>
-      </div>
-      <div class="box" style="background-color: white;">
-        <div class="grid-container">
-          <div class="grid-item">Task 1</div>
-          <div class="grid-item">Task 1</div>
-          <div class="grid-item">Task 1</div>
-          <div class="grid-item">Task 1</div>
-          <div class="grid-item">Task 1</div>
-          <div class="grid-item">Task 1</div>
-          <div class="grid-item">Task 1</div>
-          <div class="grid-item">Task 1</div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+}
 
-`
+const html = fetchHTML();
+
+console.log(html)
+
 export class Overlay {
     static create(player) {
         const div = document.createElement('div');
