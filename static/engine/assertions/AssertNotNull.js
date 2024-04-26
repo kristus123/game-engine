f2ea7a3b4ea5b9d5879f23a8c8ca2f6399a83435
kristus123 <krispetter@gmail.com>
2024-04-26 12:56:56 +0100

    x

--- static/engine/assertions/AssertNotNull.js
+++ static/engine/assertions/AssertNotNull.js
@@ -1,4 +1,4 @@
-export function AssertNotNull(x, errorMessage) {
+export function AssertNotNull(x, errorMessage="add own message!") {
 	if (x === null) {
 		throw new Error(errorMessage + ' NULL IS CONSIDERED BAD. BITCH')
 	}
