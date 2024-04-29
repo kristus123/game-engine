975b5ab73ce521e8a9b9ed34eab89921b99c2f66
kristus123 <krispetter@gmail.com>
2024-04-29 11:20:19 +0100

    x

--- http_server/FileExplorer.js
+++ http_server/FileExplorer.js
@@ -1,34 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = class Files {
+// module.exports = 
 
-  static inFolder(path) {
+class Files {
+  static inFolder(directoryPath) {
+    const files = fs.readdirSync(directoryPath);
+    const filesAndFolders = {};
 
-    return new Promise((resolve, reject) => {
-      fs.readdir(path, (err, files) => {
-        if (err) {
-          reject(err);
-        } else {
-          const filesAndFolders = {};
-
-          files.forEach(file => {
-            const stat = fs.statSync(path.join(path, file));
-            if (stat.isDirectory()) {
-              filesAndFolders[file] = [];
-            } else {
-              const folderName = path.dirname(file);
-              if (!filesAndFolders[folderName]) {
-                filesAndFolders[folderName] = [];
-              }
-              filesAndFolders[folderName].push(path.basename(file));
-            }
-          });
-
-          resolve(filesAndFolders);
+    files.forEach(file => {
+      const stat = fs.statSync(path.join(directoryPath, file));
+      if (stat.isDirectory()) {
+        filesAndFolders[file] = fs.readdirSync(path.join(directoryPath, file));
+      } else {
+        const folderName = path.dirname(file);
+        if (!filesAndFolders[folderName]) {
+          filesAndFolders[folderName] = [];
         }
-      });
+        filesAndFolders[folderName].push(path.basename(file));
+      }
     });
+
+    return filesAndFolders;
   }
-}
+};
+
+
+console.log(Files.inFolder("static/assets"))
 
