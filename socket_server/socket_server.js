2f435338a9694ffb27085feea99420e84338810d
kristus123 <krispetter@gmail.com>
2024-05-01 11:47:29 +0100

    x

--- socket_server/socket_server.js
+++ socket_server/socket_server.js
@@ -1,12 +1,24 @@
+const fs = require('fs');
 const Server = require('./Server')
 const crypto = require('crypto')
 
 const server = new Server()
 
+
+const world = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+
+
+
+
 server.onConnection = client => {
 	server.clientsAndPlayerIds.push({
 		client: client,
 		playerId: crypto.randomUUID(),
+	})
+
+	server.sendToClient(client, {
+		action: "WORLD",
+		world: world,
 	})
 }
 
