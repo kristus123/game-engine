bca6e12bf9413c86a8571e7d3c60696c428447e4
kristus123 <krispetter@gmail.com>
2024-05-03 00:38:42 +0100

    x

--- static/engine/core/index.js
+++ static/engine/core/index.js
@@ -18,6 +18,8 @@ ErrorHandler.run(() => {
 	const levelSelector = new LevelSelector()
 	levelSelector.changeActiveLevel(new World(levelSelector, allGameObjects, camera, mouse, controller))
 
+	Overlay.create()
+
 	Loop.everyFrame((deltaTime) => {
 		ErrorHandler.run(() => {
 			Palette.clear([camera.palette, guiPalette])
--- static/game/world/World.js
+++ static/game/world/World.js
@@ -5,10 +5,10 @@ export class World {
 		camera.followInstantly(this.player)
 		controller.control(this.player)
 
-		this.onlinePlayers = new OnlinePlayers(this.player)
-		this.onlinePlayers.playersOnline.forEach(p => {
-			allGameObjects.register(this, p)
-		})
+		// this.onlinePlayers = new OnlinePlayers(this.player)
+		// this.onlinePlayers.playersOnline.forEach(p => {
+		// 	allGameObjects.register(this, p)
+		// })
 
 		// this.deliveryDrone = new DeliveryDrone(this.player, camera, controller, new Position(2000, 2000), -100, 0)
 
@@ -25,11 +25,11 @@ export class World {
 	}
 
 	update() {
-		this.onlinePlayers.update()
-		this.onlinePlayers.updatePositionForPlayer(this.player)
+		// this.onlinePlayers.update()
+		// this.onlinePlayers.updatePositionForPlayer(this.player)
 	}
 
 	draw(draw, guiDraw) {
-		this.onlinePlayers.draw(draw, guiDraw)
+		// this.onlinePlayers.draw(draw, guiDraw)
 	}
 }
