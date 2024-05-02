b754df3af771dc489d347d0aaa551c346f26fac3
kristus123 <krispetter@gmail.com>
2024-05-02 12:17:36 +0100

    x

--- static/game/world/World.js
+++ static/game/world/World.js
@@ -1,11 +1,17 @@
 export class World {
 	constructor(levelSelector, allGameObjects, camera, mouse, controller) {
 
+		const client = new SocketClient()
+		client.on("WORLD", (client, data) => {
+			console.log(data)
+		})
+		client.connect()
+
 		this.player = new Player(mouse)
 		camera.followInstantly(this.player)
 		controller.control(this.player)
 
-		this.deliveryDrone = new DeliveryDrone(this.player, camera, controller, this.player, -100, 0)
+		this.deliveryDrone = new DeliveryDrone(this.player, camera, controller, new Position(2000, 2000), -100, 0)
 
 		allGameObjects.register(this, [
 			new StarBackground(camera),
