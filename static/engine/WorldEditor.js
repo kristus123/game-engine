5ea4802e161004b4e6cfd52c75f64aa55ed95d24
vinay <vinaylatwal123@gmail.com>
2024-05-11 21:54:32 +0530

    added select image functionality

--- static/engine/WorldEditor.js
+++ static/engine/WorldEditor.js
@@ -3,8 +3,6 @@ export class WorldEditor {
     camera.followInstantly(
       new DynamicGameObject(new Position(0, 0, 10, 10), 4500, 50)
     );
-
-    this.data = Http.get("/canvas-image");
     this.selectedImage = "";
 
     this.runAll = new RunAll([
@@ -17,43 +15,34 @@ export class WorldEditor {
     ObjectPersistence.get().forEach((o) => {
       this.runAll.add(o);
     });
+    mouse.addOnClick("paint", async (p, c) => {
+      if (
+        c.contains("item") ||
+        c.contains("selector") ||
+        c.contains("side-item") ||
+        !Overlay.selectedImage
+      ) {
+      } else {
+        // making file path for randomise image
+        this.selectedImage = `../${Overlay.selectedImage}`;
 
-    mouse.addOnClick("paint", async (p) => {
-      // making file path for randomise image
-      this.selectedImage = `../static/assets/planets/${
-        this.data.planets[Math.floor(Math.random() * this.data.planets.length)]
-      }`;
+        p.height = this.selectedImage.width;
+        p.width = this.selectedImage.height;
+        const o = new StaticPicture(
+          new Position(p.x, p.y, p.width, p.height),
+          this.selectedImage
+        );
 
-      p.height = this.selectedImage.width;
-      p.width = this.selectedImage.height;
-      const o = new StaticPicture(
-        new Position(p.x, p.y, p.width, p.height),
-        this.selectedImage
-      );
+        this.runAll.add(o);
+        ObjectPersistence.save(o);
+      }
+
       //   const o = new DynamicGameObject(
       //     new Position(p.x, p.y, p.width, p.height),
       //     100,
       //     100
       //   );
-      this.runAll.add(o);
-      ObjectPersistence.save(o);
     });
-
-    // mouse.addOnClick("paint", async (p) => {
-    //   p.height = 100;
-    //   p.width = 100;
-    //   // const o = new StaticPicture(
-    //   //   new Position(p.x, p.y, p.width, p.height),
-    //   //   this.selectedImage
-    //   // );
-    //   const o = new DynamicGameObject(
-    //     new Position(p.x, p.y, p.width, p.height),
-    //     100,
-    //     100
-    //   );
-    //   this.runAll.add(o);
-    //   ObjectPersistence.save(o);
-    // });
 
     KeyDown("-", () => {
       console.log("zooming out");
