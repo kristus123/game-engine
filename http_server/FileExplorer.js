6ae86465af5d808420f40a3a906af4046967e4d2
vinay <vinaylatwal123@gmail.com>
2024-05-11 22:21:50 +0530

    reverted wrong commit

--- http_server/FileExplorer.js
+++ http_server/FileExplorer.js
@@ -1,5 +1,7 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
+
+// module.exports =
 
 class Files {
   static inFolder(directoryPath) {
--- http_server/main.js
+++ http_server/main.js
@@ -1,54 +1,54 @@
-const express = require("express");
-const cors = require("cors");
-const Files = require("./FileExplorer");
-const fs = require("fs");
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const Files = require("./FileExplorer")
+const fs = require('fs')
+const app = express()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-app.use(express.json()); // Automatically parses JSON bodies
-app.use(cors());
+app.use(express.json()) // Automatically parses JSON bodies
+app.use(cors())
 
-app.post("/world-editor", (req, res) => {
-  const jsonData = req.body;
+app.post('/world-editor', (req, res) => {
+	const jsonData = req.body
+	fs.writeFile('data.json', JSON.stringify(jsonData), (err) => {
+		if (err) {
+			console.error('Error saving JSON data:', err)
+			res.status(500).send('Error saving JSON data')
+			return
+		}
+		console.log('JSON data saved successfully')
+		res.status(200).send('JSON data saved successfully')
+	})
+})
 
-  fs.writeFile("data.json", JSON.stringify(jsonData), (err) => {
-    if (err) {
-      console.error("Error saving JSON data:", err);
-      res.status(500).send("Error saving JSON data");
-      return;
-    }
-    console.log("JSON data saved successfully");
-    res.status(200).send("JSON data saved successfully");
-  });
-});
-
-app.get("/world-editor", (req, res) => {
-  fs.readFile("data.json", "utf8", (err, data) => {
-    if (!err) {
-      res.setHeader("Content-Type", "application/json");
-      res.send(data);
-    } else {
-      res.status(500).send("Error reading JSON data");
-      return;
-    }
-  });
-});
+app.get('/world-editor', (req, res) => {
+	fs.readFile('data.json', 'utf8', (err, data) => {
+		if (!err) {
+			res.setHeader('Content-Type', 'application/json')
+			res.send(data)
+		}
+		else {
+			res.status(500).send('Error reading JSON data')
+			return
+		}
+	})
+})
 
 app.get("/canvas-image", (req, res) => {
   try {
-    const data = Files.inFolder("static/assets");
+    const data = Files.inFolder("static/assets")
     if (data) {
-      res.status(200).send(data);
+      res.status(200).send(data)
     } else {
-      res.status(404).send("no data");
+      res.status(404).send("no data")
     }
   } catch (error) {
-    res.status(500).send("Error while fetching Images");
-    return;
+    res.status(500).send("Error while fetching Images")
+    return
   }
-});
+})
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+	console.log(`Server running at http://localhost:${PORT}/`)
+})
