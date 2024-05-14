1458e27e2264318c203d273196b395682bb96b41
kristus123 <krispetter@gmail.com>
2024-05-14 10:10:07 +0100

    x

--- static/engine/Button.js
+++ static/engine/Button.js
@@ -1,20 +1,20 @@
 export class Button {
 	constructor(text, camera) {
 
-	  const button = document.createElement('button');
-	  button.textContent = text;
+	  const button = document.createElement('button')
+	  button.textContent = text
 		this.button = button
-	  
-	const attributes = { tabindex: -1, class: 'button',id: 'myButton' }
+
+		const attributes = { tabindex: -1, class: 'button', id: 'myButton' }
 	  Object.entries(attributes).forEach(([key, value]) => {
-		button.setAttribute(key, value);
-	  });
+			button.setAttribute(key, value)
+	  })
 
 		const buttons = document.getElementById('buttons')
 		buttons.appendChild(button)
 
 	   button.addEventListener('click', () => {
-	   button.innerText = "xxx"
+	   button.innerText = 'xxx'
 		 button.parentNode.removeChild(button)
 	   })
 
@@ -31,9 +31,9 @@ export class Button {
 		const offsetX = 0 - this.camera.position.x + (Palette.width/2)
 		const offsetY = 0 - this.camera.position.y + (Palette.height/2)
 
-			this.button.style.left = `${offsetX}px`;
-			this.button.style.top = `${offsetY}px`;
-			// button.style.transform = `translate(${offsetX}px, ${offsetY}px)`
+		this.button.style.left = `${offsetX}px`
+		this.button.style.top = `${offsetY}px`
+		// button.style.transform = `translate(${offsetX}px, ${offsetY}px)`
 	}
 
 	draw(draw, guiDraw) {
