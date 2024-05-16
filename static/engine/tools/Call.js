17467117c703e88f8a8f414fa15d16fc4d04715b
kristus123 <krispetter@gmail.com>
2024-05-16 10:57:21 +0100

    x

--- static/engine/tools/Call.js
+++ static/engine/tools/Call.js
@@ -1,6 +1,6 @@
-export const Call = (methodName) => {
-	if (methodName) {
-		methodName()
+export const Call = method => {
+	if (method) {
+		method()
 	}
 	else {
 		// throw new Error(methodName + ' is not implemented')
