09e76dcaee9c770e80a862398557f39b568a4ea9
kristus123 <krispetter@gmail.com>
2024-05-21 12:05:27 +0100

    remove mirroring from sprite.js

--- static/engine/Sprite.js
+++ static/engine/Sprite.js
@@ -33,32 +33,4 @@ export class Sprite {
 		}
 	}
 
-	mirror(draw) {
-		if (this.spriteSheet.complete) {
-			const frameInfo = this.frameSequence[this.currentFrameIndex]
-			const currentFrameX = frameInfo.x
-			const currentFrameY = frameInfo.y
-
-			draw.ctx.imageSmoothingEnabled = false
-
-			// Mirror the image horizontally
-			draw.ctx.save()
-			draw.ctx.scale(-1, 1)
-
-			draw.ctx.drawImage(
-				this.spriteSheet,
-				currentFrameX * this.frameWidth,
-				currentFrameY * this.frameHeight,
-				this.frameWidth,
-				this.frameHeight,
-				-this.dynamicGameObject.x - this.frameWidth * this.scale, // Adjusting x position for mirroring
-				this.dynamicGameObject.y,
-				this.frameWidth * this.scale,
-				this.frameHeight * this.scale,
-			)
-
-			draw.ctx.restore() // Restore the transformation to prevent affecting other drawings
-		}
-	}
-
 }
