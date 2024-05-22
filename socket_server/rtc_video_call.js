298e044beafce23ba722ee6575ff1bfd6b8070f5
kristus123 <krispetter@gmail.com>
2024-05-22 13:30:29 +0100

    x

--- socket_server/rtc_video_call.js
+++ socket_server/rtc_video_call.js
@@ -1,32 +1,25 @@
 const SocketServer = require('./SocketServer')
 const server = new SocketServer(8082)
-const rooms = {}
 
 server.onConnection = (client, clientId) => {
-	console.log(clientId)
 	server.sendToClient(client, {
 		action: 'RTC_CLIENT_INFO',
 		clientId: clientId
 	})
+
 	for (let i = 0; i < server.allClientIds.length; i++) {
 		const id = server.allClientIds[i]
-		if (id == clientId) continue
+		if (id == clientId) {
+			continue
+		}
+
 		server.sendToClient(server.clientFrom[id], {
 			action: 'RTC_CLIENT_CONNECTED',
 			clientId: clientId
 		})
+
 		break
 	}
-
-	/*for (let i = 0; i < server.allClientIds.length; i++) {
-		const id = server.allClientIds[i];
-		if(id == clientId)continue;
-		server.sendToClient(client,{
-			action: 'RTC_CLIENT_CONNECTED',
-			clientId: id
-		})
-	}*/
-
 }
 
 server.onClose = (client, clientId) => {
@@ -35,28 +28,38 @@ server.onClose = (client, clientId) => {
 		clientId: clientId,
 	})
 }
+
 server.on('RTC_CLIENT_CONNECTED', (client, clientId, data) => {
 	server.sendToClient(server.clientFrom[data.clientId], {
 		action: 'RTC_CLIENT_CONNECTED',
 		clientId: clientId
 	})
+
 	for (let i = 0; i < server.allClientIds.length; i++) {
 		const id = server.allClientIds[i]
 		let skipId = false
+
 		for (let j = 0; j < data.connectedClientId.length; j++) {
 			const cid = data.connectedClientId[j]
 			if (cid == id) {
-				skipId = true;break
+				skipId = true
+				break
 			}
 		}
-		if (id == clientId || id == data.clientId||skipId) continue
+
+		if (id == clientId || id == data.clientId || skipId) {
+			continue
+		}
+
 		server.sendToClient(client, {
 			action: 'RTC_CLIENT_CONNECTED',
 			clientId: id
 		})
+
 		break
 	}
 })
+
 server.on('RTC_ICE_CANDIDATE', (client, clientId, data) => {
 	server.sendToClient(server.clientFrom[data.clientId], {
 		action: 'RTC_ICE_CANDIDATE',
@@ -64,6 +67,7 @@ server.on('RTC_ICE_CANDIDATE', (client, clientId, data) => {
 		clientId: data.clientId
 	})
 })
+
 server.on('RTC_OFFER', (client, clientId, data) => {
 	console.log(clientId + ' to '+data.clientId)
 	server.sendToClient(server.clientFrom[data.clientId], {
@@ -90,4 +94,5 @@ server.on('RTC_HANGUP', (client, clientId, data) => {
 		clientId: data.clientId
 	})
 })
+
 server.start()
