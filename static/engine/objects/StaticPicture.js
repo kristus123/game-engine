2fafd524dcd678b20c3380c47c9ac9a0fb5684b5
kristus123 <krispetter@gmail.com>
2024-05-23 16:18:25 +0100

    p-master

--- static/engine/objects/StaticPicture.js
+++ static/engine/objects/StaticPicture.js
@@ -5,23 +5,6 @@ export class StaticPicture {
 		this.image.src = imagePath
 	}
 
-	r(draw, rotation=2) {
-		const newWidth = this.position.width
-		const newHeight = this.position.height
-
-		draw.ctx.save()
-
-		draw.ctx.translate(this.position.center.x, this.position.center.y)
-
-		const rotationAngle = Math.atan2(this.position.velocity.y, this.position.velocity.x)
-		draw.ctx.rotate(rotationAngle)
-		draw.ctx.rotate(Math.PI / rotation) // 90 degrees
-
-		draw.ctx.drawImage(this.image, -newWidth / 2, -newHeight / 2, newWidth, newHeight)
-
-		draw.ctx.restore()
-	}
-
 	draw(draw, guiDraw) { // todo
 		if (this.image.complete) {
 			const newWidth = this.image.width
@@ -36,28 +19,6 @@ export class StaticPicture {
 
 			draw.ctx.restore()
 		}
-	}
-
-	old_draw(draw, size) {
-		const aspectRatio = this.image.width / this.image.height
-
-		let newWidth = size
-		let newHeight = size
-
-		if (this.image.width > size) {
-			newWidth = size
-			newHeight = newWidth / aspectRatio
-		}
-
-		if (newHeight > size) {
-			newHeight = size
-			newWidth = newHeight * aspectRatio
-		}
-
-		draw.ctx.save()
-		draw.ctx.translate(this.position.x, this.position.y)
-		draw.ctx.drawImage(this.image, -newWidth / 2, -newHeight / 2, newWidth, newHeight)
-		draw.ctx.restore()
 	}
 }
 
