44026f5622240395b75386e47d514b21e9863f38
kristus123 <krispetter@gmail.com>
2024-05-28 20:12:17 +0100

    x

--- build_tools/generate_dist.js
+++ build_tools/generate_dist.js
@@ -24,39 +24,39 @@ function uuid() {
 const jsFiles = require('./get_js_files')
 
 for (const jsFilePath of jsFiles) {
-	let content = fs.readFileSync(jsFilePath, 'utf-8')
+	let fileContent = fs.readFileSync(jsFilePath, 'utf-8')
 
-	content = content.replaceAll('RunOnce(', 'RunOnce(this, TEMP_UUID, ')
-	let count = countOccurrences(content, 'TEMP_UUID')
+	fileContent = fileContent.replaceAll('RunOnce(', 'RunOnce(this, TEMP_UUID, ')
+	let count = countOccurrences(fileContent, 'TEMP_UUID')
 	for (let i = 0; i < count; i++) {
-		content = content.replace('TEMP_UUID', uuid())
+		fileContent = fileContent.replace('TEMP_UUID', uuid())
 	}
 
-	content = content.replaceAll('RunUntil(', 'RunUntil(this, TEMP_UUID_1, ')
-	for (let i = 0; i < countOccurrences(content, 'TEMP_UUID_1'); i++) {
-		content = content.replace('TEMP_UUID_1', uuid())
+	fileContent = fileContent.replaceAll('RunUntil(', 'RunUntil(this, TEMP_UUID_1, ')
+	for (let i = 0; i < countOccurrences(fileContent, 'TEMP_UUID_1'); i++) {
+		fileContent = fileContent.replace('TEMP_UUID_1', uuid())
 	}
 
-	if (content.includes('export class')) {
-		let lines = content.split('\n')
+	if (fileContent.includes('export class')) {
+		let lines = fileContent.split('\n')
 
 		for (let i = 0; i < lines.length; i++) {
 			if (lines[i].includes('constructor(') && lines[i+1].includes('super(')) {
-				lines[i+1] = lines[i+1] + '\n' + Parameters.nullCheckForConstructorArguments(content)
-				lines[i+1] = lines[i+1] + '\n' + Parameters.initVariablesFromConstructor(content)
+				lines[i+1] = lines[i+1] + '\n' + Parameters.nullCheckForConstructorArguments(fileContent)
+				lines[i+1] = lines[i+1] + '\n' + Parameters.initVariablesFromConstructor(fileContent)
 			}
 			else if (lines[i].includes('constructor(')) {
-				lines[i] = lines[i] + '\n' + Parameters.nullCheckForConstructorArguments(content)
-				lines[i] = lines[i] + '\n' + Parameters.initVariablesFromConstructor(content)
+				lines[i] = lines[i] + '\n' + Parameters.nullCheckForConstructorArguments(fileContent)
+				lines[i] = lines[i] + '\n' + Parameters.initVariablesFromConstructor(fileContent)
 			}
 		}
 
-		content = lines.join('\n')
+		fileContent = lines.join('\n')
 	}
 
-	content = Imports.needed(content, jsFiles) + '\n' + content
+	fileContent = Imports.needed(fileContent, jsFiles) + '\n' + fileContent
 
-	Files.writeFileToDist(jsFilePath, content)
+	Files.writeFileToDist(jsFilePath, fileContent)
 }
 
 require('./copy_asset_folder_to_dist')
