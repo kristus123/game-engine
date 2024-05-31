fc38719ea74fb8b7cc71793e9d429d5f990d4206
kristus123 <krispetter@gmail.com>
2024-05-31 14:43:42 +0100

    x

--- static/engine/misc/Picture.js
+++ static/engine/misc/Picture.js
@@ -1,23 +1,12 @@
 export class Picture {
-	constructor(o, imagePath) {
-		this.dynamicGameObject = o
+	constructor(position, imagePath) {
 
 		this.image = new Image()
 		this.image.src = imagePath
 	}
 
 	draw(draw, guiDraw) { // todo
-		if (this.image.complete) {
-			const newWidth = this.dynamicGameObject.width
-			const newHeight = this.dynamicGameObject.height
-
-			draw.ctx.imageSmoothingEnabled = false
-
-			draw.ctx.save()
-			draw.ctx.translate(this.dynamicGameObject.x + this.dynamicGameObject.width, this.dynamicGameObject.y + this.dynamicGameObject.height)
-			draw.ctx.drawImage(this.image, -newWidth, -newHeight, newWidth, newHeight)
-			draw.ctx.restore()
-		}
+		draw.image(this.image, this.position)
 	}
 
 }
