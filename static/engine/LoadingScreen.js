a6c1f3847fdadc8de069a5bd520c7333fcab5fc4
Junaith1315256 <87628427+Junaith1315256@users.noreply.github.com>
2024-06-06 23:40:47 +0530

    loading update

--- static/engine/LoadingScreen.js
+++ static/engine/LoadingScreen.js
@@ -1,13 +1,12 @@
 export class LoadingScreen{
-    static LOADING_SCREEN = document.getElementById("loading_screen")
     static show(text = "Loading..."){
-        this.LOADING_SCREEN.style.display = "flex";
+        document.getElementById("loading_screen").style.display = "flex";
         const text_div = document.getElementById("loading_text");
         text_div.innerText = text;
-        this.LOADING_SCREEN.appendChild(text_div)
+        document.getElementById("loading_screen").appendChild(text_div)
     }
     
     static close(){
-        this.LOADING_SCREEN.style.display = "none"
+        document.getElementById("loading_screen").style.display = "none"
     }
 }
