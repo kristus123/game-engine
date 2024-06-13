e0e859f13c959086f72f842c5fbec5120cdd0e95
kristus123 <krispetter@gmail.com>
2024-06-13 22:10:53 +0100

    x

--- static/engine/LoadingScreen.js
+++ static/engine/LoadingScreen.js
@@ -1,16 +1,16 @@
+const loading_screen = document.getElementById('loading_screen')
+
 export class LoadingScreen {
 	static show(text = 'Loading...') {
-		const div = document.getElementById('loading_screen')
 
 		const p = Html.p(text)
-		div.appendChild(p)
+		loading_screen.appendChild(p)
 
-		div.style.display = 'flex'
+		loading_screen.style.display = 'flex'
 	}
 
 	static close() {
-		document.getElementById('loading_screen').style.display = 'none'
+		loading_screen.style.display = 'none'
 		Html.removeChildElementsInId('loading_screen')
 	}
 }
-
