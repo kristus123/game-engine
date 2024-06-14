dc8536a981d5dc8e543cc84a588d050c08a1ac7a
Junaith1315256 <87628427+Junaith1315256@users.noreply.github.com>
2024-06-14 21:46:39 +0530

    update

--- static/engine/SpinningSpriteFrame.js
+++ static/engine/SpinningSpriteFrame.js
@@ -11,6 +11,25 @@ export class SpinningSpriteFrame {
 	draw(draw, guiDraw) {
         this.rotate+=Math.floor(Math.random()*20+1)/100;
 
-		this.frame.spinning_draw(draw, guiDraw,this.rotate)
+		if (this.frame.image.complete) {
+			const frame = this.frame.frameSequence[this.frame.currentFrame]
+			draw.ctx.save()
+			draw.ctx.translate(this.frame.position.x+this.frame.position.width*0.5,this.frame.position.y +this.frame.position.height*0.5)
+			draw.ctx.imageSmoothingEnabled = false
+			
+			draw.ctx.rotate(this.rotate)
+			draw.ctx.drawImage(
+				this.frame.image,
+				frame.x * this.frame.width,
+				frame.y * this.frame.height,
+				this.frame.width,
+				this.frame.height,
+				this.frame.position.width*-0.5,
+				this.frame.position.height *-0.5,
+				this.frame.position.width,
+				this.frame.position.height 
+			)
+			draw.ctx.restore()
+		}
 	}
 }
