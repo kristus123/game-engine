32d1d40a769277bb6c1a0e7196dcba39ba73c62b
Junaith1315256 <87628427+Junaith1315256@users.noreply.github.com>
2024-06-14 22:25:54 +0530

    squash

--- static/game/world/FirstQuest.js
+++ static/game/world/FirstQuest.js
@@ -102,7 +102,7 @@ export class FirstQuest {
 		this.localObjects = new LocalObjects([
 			//new MovableObjects(player, chickens),
 			new Quest([
-				//() => new ONE_DeliverChickens(chickens),
+				() => new ONE_DeliverChickens(chickens),
 				() => new TWO_DriveChickens(chickens, player),
 			]),
 			...chickens,
