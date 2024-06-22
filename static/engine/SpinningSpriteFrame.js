a5adaa555aefe7f38f0a599131ff688111085684
Your Name <you@example.com>
2024-06-22 15:55:41 +0200

    x
    x
    
    update
    
    Describe your changes
    
    squash
    
    x
    
    x
    
    squash
    
    x
    
    x
    
    squash
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    update
    
    update
    
    update
    
    Update
    
    Kill Chicken
    
    x
    
    squash

---
+++ static/engine/SpinningSpriteFrame.js
@@ -0,0 +1,35 @@
+export class SpinningSpriteFrame {
+	constructor(position, imagePath, frame) {
+		this.frame = new Sprite(position, imagePath, [frame])
+        this.rotate = Math.floor(Math.random()*360)
+	}
+
+	update() {
+
+	}
+
+	draw(draw, guiDraw) {
+        this.rotate+=Math.floor(Math.random()*20+1)/100;
+
+		if (this.frame.image.complete) {
+			const frame = this.frame.frameSequence[this.frame.currentFrame]
+			draw.ctx.save()
+			draw.ctx.translate(this.frame.position.x+this.frame.position.width*0.5,this.frame.position.y +this.frame.position.height*0.5)
+			draw.ctx.imageSmoothingEnabled = false
+			
+			draw.ctx.rotate(this.rotate)
+			draw.ctx.drawImage(
+				this.frame.image,
+				frame.x * this.frame.width,
+				frame.y * this.frame.height,
+				this.frame.width,
+				this.frame.height,
+				this.frame.position.width*-0.5,
+				this.frame.position.height *-0.5,
+				this.frame.position.width,
+				this.frame.position.height 
+			)
+			draw.ctx.restore()
+		}
+	}
+}
