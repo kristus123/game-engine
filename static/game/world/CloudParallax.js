7db8c5ff4526a5da299e72abec4a747cc25cbbdf
Your Name <you@example.com>
2024-07-19 23:04:17 +0200

    x

--- static/game/world/CloudParallax.js
+++ static/game/world/CloudParallax.js
@@ -1,9 +1,9 @@
 export class CloudParallax {
 	constructor() {
-		this.picture = new StaticPicture(new Position(0,0, 400, 200), '/static/assets/cloud.png')
 
 		this.cloudPositions = Iterate(10, i => ({
 			position: Random.direction(Cam.position, 1000),
+			picture: new Picture(new Position(0,0, 400, 200), Random.choice([ '/static/assets/cloud.png', '/static/assets/cloud_2.png'])),
 		}))
 	}
 
@@ -11,11 +11,11 @@ export class CloudParallax {
 	}
 
 	draw(draw, guiDraw) {
-		for (const {position} of this.cloudPositions) {
-			const p = Parallax(position, 0.2)
-			this.picture.position.x = p.x
-			this.picture.position.y = p.y
-			this.picture.draw(draw, guiDraw)
+		for (const {position, picture} of this.cloudPositions) {
+			const p = Parallax(position, -0.5)
+			picture.position.x = p.x
+			picture.position.y = p.y
+			picture.draw(draw, guiDraw)
 		}
 	}
 }
