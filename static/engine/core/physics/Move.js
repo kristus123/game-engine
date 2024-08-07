cf6e61806c43744f1d6fb06e290453008716e0d4
Your Name <you@example.com>
2024-08-07 23:06:54 +0200

    x

--- static/engine/core/physics/Move.js
+++ static/engine/core/physics/Move.js
@@ -11,4 +11,12 @@ export const Move = (o) => ({
 		o.x += Math.cos(dir) * o.velocityFactor * multiplier
 		o.y += Math.sin(dir) * o.velocityFactor * multiplier
 	},
+
+	 to: (position, degreesOffset, multiplier = 1) => {
+        const dir = Math.atan2(position.y - o.y, position.x - o.x);
+        const offsetDir = dir + (degreesOffset * Math.PI / 180); // Apply the offset in radians
+        o.x += Math.cos(offsetDir) * o.velocityFactor * multiplier;
+        o.y += Math.sin(offsetDir) * o.velocityFactor * multiplier;
+    },
+
 })
--- static/engine/mechanics/SimplePathFinder.js
+++ static/engine/mechanics/SimplePathFinder.js
@@ -1,37 +1,75 @@
 export class SimplePathFinder {
 	constructor(target, invisibleWalls) {
-		this.object = new Square(new Position(400, 10), 100)
+		this.startPosition = new Square(new Position(400, 10), 100)
 
 		this.c1 = new Square(new Position(400, 10), 20)
+		this.c2 = new Square(new Position(400, 10), 20)
+		this.c2.draw = (draw, guiDraw) => {
+			draw.rectangle(this.c2.position, 'orange')
+		}
 
 		this.localObjects = new LocalObjects([
-			this.object,
+			this.startPosition,
 			this.c1,
+			this.c2,
 		])
 
 		this.rotationAmount = 0
 
 		this.speed = 5
 
-		Move(this.c1).towards(this.target, this.speed)
-
 		this.angle1 = this.c1.position.copy()
+
+		this.c1Active = true
+
+		this.success = false
+		this.touchedC1 = false
 	}
 
 	update() {
+
 		this.localObjects.update()
 
-		for (const wall of this.invisibleWalls) {
-			if (Collision.between(this.c1, wall)) {
-				this.c1.position = this.object.position.copy()
-				this.c1.velocity.reset()
-				console.log("collision. resetting")
-				this.rotationAmount += 10
 
-				Move(this.c1).towards(this.target, this.speed)
-				this.c1.velocity.rotate(this.rotationAmount)
+		let  closestWall = this.invisibleWalls[0]
+		if (this.c1Active) {
+			Move(this.c1).to(this.target, this.rotationAmount, this.speed)
 
-				this.angle1 = this.c1.position.copy()
+			for (const wall of this.invisibleWalls) {
+				if (Distance.between(this.c1, wall) < Distance.between(this.c1, closestWall)) {
+					closestWall = wall
+				}
+
+				if (this.c1.touches(wall)) {
+					this.c1.position = this.startPosition.position.copy()
+					this.rotationAmount += 100
+				}
+			}
+
+			if (Distance.between(this.c1, closestWall) > 100) {
+				Move(this.c1).to(this.target, 0, this.speed)
+			}
+
+			this.c1Active = false
+		}
+		else {
+			Move(this.c2).to(this.target, 0, this.speed)
+			let c2Collision = false
+			for (const wall of this.invisibleWalls) {
+				if (this.c2.touches(wall)) {
+					c2Collision = true
+					break
+				}
+			}
+
+			if (c2Collision) {
+				this.c2.position = this.c1.position.copy()
+				this.c1Active = true
+			}
+
+			if (this.c2.within(100, this.target)) {
+				console.log("sucesss")
+				this.success = true
 			}
 		}
 	}
