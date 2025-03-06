28c36ab9618acb745b2ea6c7c59128a8f4e0a006
Your Name <you@example.com>
2025-03-06 10:55:27 +0800

    x

--- static/game/world/World.js
+++ static/game/world/World.js
@@ -1,3 +1,35 @@
+class CanvasRenderer {
+    constructor(width = 100, height = 100) {
+        this.canvas = new OffscreenCanvas(width, height);
+        this.ctx = this.canvas.getContext("2d");
+        this.width = width;
+        this.height = height;
+    }
+
+    drawRect(x, y, width, height, color = "blue") {
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect(x, y, width, height);
+    }
+
+    getImageBitmap() {
+        return this.canvas.convertToBlob().then(blob => createImageBitmap(blob));
+    }
+}
+const renderer = new CanvasRenderer()
+
+
+renderer.drawRect(0, 0, 100, 100)
+let ib =  null
+renderer.getImageBitmap().then(imageBitmap => {
+	ib = imageBitmap
+});
+
+
+
+
+
+
+
 
 export class World {
 	constructor() {
@@ -6,10 +38,10 @@ export class World {
 			Init(this, {
 				store: new Store(),
 			}),
-			G.ranches,
-			G.monsters,
-			G.poop,
-			G.splash,
+			// G.ranches,
+			// G.monsters,
+			// G.poop,
+			// G.splash,
 		])
 
 		G.ranches.add(new Ranch(new Position(0,0)))
@@ -23,7 +55,7 @@ export class World {
 		BottomText.show('Welcome!', 2_000)
 
 
-		LoadingScreen.show()
+		// LoadingScreen.show()
 		// QuestList.add('eat ass')
 		// QuestList.show()
 
@@ -36,5 +68,11 @@ export class World {
 	draw(draw, guiDraw) {
 		this.localObjects.draw(draw, guiDraw)
 		// draw.test(new Position(0, 0))
+
+
+		if (ib) {
+	draw.ctx.drawImage(ib, 0,200);
+			
+		}
 	}
 }
