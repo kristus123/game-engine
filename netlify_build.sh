52fd9198ae153e826599375939029015582727e4
Your Name <you@example.com>
2025-07-08 13:40:30 +0200

    x

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-# node build_tools/generate_dist.js
+set -e
 
-echo 1
+# Minify JavaScript files in place
+find dist -type f -name '*.js' -exec npx terser {} --compress --mangle -o {} \;
+
+# Minify CSS files in place
+find dist -type f -name '*.css' -exec npx lightningcss --minify {} -o {} \;
+
+# Minify HTML files in place
+find dist -type f -name '*.html' -exec npx html-minifier-terser --collapse-whitespace --remove-comments -o {} {} \;
+
