c4e52fc4004bc1041d54bb61fd59161dc143217d
Your Name <you@example.com>
2025-08-26 12:35:44 +0200

    x

--- watch.js
+++ watch.js
@@ -7,7 +7,7 @@ let currentId = RandomId()
 
 const app = express()
 app.use(express.static('dist'))
-app.get('/currentId', (req, res) => {
+app.get('/currentId', (req, res) => { // this is used for hot-reloading. Check index.js
 	res.json({ currentId: currentId })
 })
 app.listen(5000, () => console.log('Serving dist on port 5000'))
