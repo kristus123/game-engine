fa90d1b66c57b7dbf4a033966c5a5b3cb5cddec7
Your Name <you@example.com>
2025-08-27 19:13:40 +0200

    x

--- static/game/World.js
+++ static/game/World.js
@@ -5,7 +5,7 @@ export class World {
 		Controller.control(G.player)
 
 		const inverseExponentialNumber = new InverseExponentialNumber(10, 100)
-		
+
 		this.localObjects = new LocalObjects([
 			G.Sprite.world(new Position(0, 0)).idle.show(0),
 			G.player,
