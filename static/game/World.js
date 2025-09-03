d7b5d63fe905ad9b2dc069dc57b0c06bb8183c3c
janeduardo19 <50849662+janeduardo19@users.noreply.github.com>
2025-09-03 21:25:53 +0100

    lefting just grid

--- static/game/World.js
+++ static/game/World.js
@@ -1,11 +1,7 @@
 export class World {
 	constructor() {
 
-		const inverseExponentialNumber = new InverseExponentialNumber(10, 100)
-
-
 		this.localObjects = new LocalObjects([
-			//G.Sprite.world(new Position(0, 0)).idle.show(0),
 			new DisplayGrid(new Position(0, 0)),
 			new Grid(),
 		])
