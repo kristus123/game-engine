4586d5f29673897bf15e53f5a2bfce68ee8ee6a0
janeduardo19 <50849662+janeduardo19@users.noreply.github.com>
2025-09-03 21:22:07 +0100

    lefting just grid

--- static/game/World.js
+++ static/game/World.js
@@ -1,36 +1,13 @@
 export class World {
 	constructor() {
-		G.player = new Player(new Position(700, 2800))
-		Camera.followInstantly(G.player)
-		Controller.control(G.player)
 
 		const inverseExponentialNumber = new InverseExponentialNumber(10, 100)
 
 
 		this.localObjects = new LocalObjects([
-			G.Sprite.world(new Position(0, 0)).idle.show(0),
-			G.player,
-			new DisplayGrid(G.player),
+			//G.Sprite.world(new Position(0, 0)).idle.show(0),
+			new DisplayGrid(new Position(0, 0)),
 			new Grid(),
-
-
-			new Quest(Iterate(100, i =>
-				() => new MonsterWave(inverseExponentialNumber.value, () => {
-					G.pause = true
-					setTimeout(() => {
-						G.wave = i + 1
-						DeathText.show(G.wave)
-						inverseExponentialNumber.next()
-						G.pause = false
-					}, 2000)
-				}),
-			)),
-
-
-			Money.init(),
-			Store.init(),
-			G.monsters,
-			G.allies,
 		])
 	}
 
