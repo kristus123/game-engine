40894a8899a05ff0659fbd738c54ab556eb364dd
Your Name <you@example.com>
2025-09-04 19:05:31 +0200

    x

--- static/game/World.js
+++ static/game/World.js
@@ -1,5 +1,7 @@
 export class World {
 	constructor() {
+		Camera.follow(new Position(800, 800))
+
 		this.localObjects = new LocalObjects([
 			// G.Sprite.world(new Position(0, 0)).idle.show(0),
 			new GrassGrid(),
@@ -7,11 +9,25 @@ export class World {
 			OnTrue(() => Keyboard.q, () => {
 				this.p.changeText('dirt')
 				G.tile = 'dirt'
+				Sound.click()
 			}),
 			OnTrue(() => Keyboard.w, () => {
 				this.p.changeText('water')
 				G.tile = 'water'
+				Sound.click()
 			}),
+		])
+
+		Html.upper([
+				Html.div('big', [
+					Html.p('Q to select dirt'),
+				]),
+				Html.div('big', [
+					Html.p('W to select water'),
+				]),
+				Html.div('big', [
+					Html.p('right click to erase'),
+				]),
 		])
 
 		Html.lower([
