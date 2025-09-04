21f5fdd66ae4f9e63ee9841258d0e26b8bbe7166
Your Name <you@example.com>
2025-09-04 21:05:31 +0200

    stop tracking dist

--- dist/static/engine/assertions/not.js
+++
@@ -1,8 +0,0 @@
-import { a } from '/static/engine/assertions/a.js'; 
-
-export class not {
-
-	static method(arg) {
-		return !a.method(arg)
-	}
-}
--- dist/static/engine/http/StaticHttp.js
+++
@@ -1,9 +0,0 @@
-import { _HttpClient } from '/static/engine/http/_HttpClient.js'; 
-
-export class StaticHttp {
-
-	static get(endpoint) {
-		return _HttpClient.get(endpoint)
-	}
-
-}
