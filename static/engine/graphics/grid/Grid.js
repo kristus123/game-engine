f9fdd58033bf97625a84eeeb9ce4680fa545bb25
Your Name <you@example.com>
2025-09-04 21:05:21 +0200

    x

--- static/engine/graphics/grid/Grid.js
+++ static/engine/graphics/grid/Grid.js
@@ -1,6 +1,9 @@
 export class Grid {
-	constructor(cellWidth = 16*Scale.value, cellHeight = 16*Scale.value) {
+	constructor(cellWidth = 16, cellHeight = 16) {
 		this.gridPositions = new GridPositions()
+
+		this.cellWidth *= Scale.value
+		this.cellHeight *= Scale.value
 	}
 
 	toGridPosition(position) {
@@ -9,6 +12,41 @@ export class Grid {
 			y: Math.floor(position.y / this.cellHeight),
 		}
 	}
+
+
+
+
+
+area(position, size = 96) {
+
+	const topLeft = this.toGridPosition(position)
+
+	const cells = size / 16
+
+	const positions = []
+
+	for (let gx = 0; gx < cells; gx++) {
+		for (let gy = 0; gy < cells; gy++) {
+			const gp = { x: topLeft.x + gx, y: topLeft.y + gy }
+			positions.push(this.scaled(gp))
+		}
+	}
+
+	return positions
+}
+
+
+availableArea(areas, size=96) {
+	return !areas.some(a => this.has(this.toGridPosition(a)))
+}
+
+
+placeArea(areas) {
+	for (const a of areas) {
+		this.add(a)
+	}
+}
+
 
 	snappedPosition(position) {
 		const gp = this.toGridPosition(position)
--- static/game/World.js
+++ static/game/World.js
@@ -5,6 +5,7 @@ export class World {
 		this.localObjects = new LocalObjects([
 			// G.Sprite.world(new Position(0, 0)).idle.show(0),
 			new GrassGrid(),
+			new HouseGrid(),
 
 			OnTrue(() => Keyboard.q, () => {
 				this.p.changeText('dirt')
