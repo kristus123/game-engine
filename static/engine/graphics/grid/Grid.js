ae0b8af3e29368df6fc6e862ef44435dbf9d98ef
Your Name <you@example.com>
2025-09-04 11:16:15 +0200

    x

--- static/engine/graphics/grid/Grid.js
+++ static/engine/graphics/grid/Grid.js
@@ -1,26 +1,64 @@
 export class Grid {
 	constructor(gridWidth = 16, gridHeight = 10, cellWidth = 16*Scale.value, cellHeight = 16*Scale.value) {
-		this.positions = []
+		this.tiles = new Set()
+	}
+
+	gridPosition(position) {
+		return {
+			x: Math.floor(position.x / this.cellWidth),
+			y: Math.floor(position.y / this.cellHeight),
+		}
 	}
 
 	snappedPosition(position) {
-		const cellX = Math.floor(position.x / this.cellWidth)
-		const cellY = Math.floor(position.y / this.cellHeight)
+		const gp = this.gridPosition(position)
+		return new Position(
+			gp.x * this.cellWidth,
+			gp.y * this.cellHeight,
+			this.cellWidth,
+			this.cellHeight
+		)
+	}
 
-		const x = cellX * this.cellWidth
-		const y = cellY * this.cellHeight
+	addTileAt(position) {
+		const gp = this.gridPosition(position)
+		this.tiles.add(`${gp.x},${gp.y}`)
+	}
 
-		return new Position(x, y, this.cellWidth, this.cellHeight)
+	hasTileAt(gp) {
+		return this.tiles.has(`${gp.x},${gp.y}`)
+	}
+
+	scaledTiles() {
+		const result = []
+		for (const key of this.tiles) {
+			const [x, y] = key.split(',').map(Number)
+			result.push(new Position(x * this.cellWidth, y * this.cellHeight, this.cellWidth, this.cellHeight))
+		}
+		return result
 	}
 
 	draw(draw) {
-		if (Mouse.down) {
-			this.positions.push(this.snappedPosition(Mouse.position))
+		const gp = this.gridPosition(Mouse.position)
+		const p = this.snappedPosition(Mouse.position)
+
+		draw.transparentGreenRectangle(p)
+
+		if (this.hasTileAt(gp)) {
+			draw.text(p, 'full')
+
+			if (Mouse.down) {
+				draw.text(p, 'cant place here')
+			}
+
+		} else if (Mouse.down) {
+			this.addTileAt(Mouse.position)
 		}
 
-		for (const p of this.positions) {
-			draw.rectangle(p)
+		for (const tile of this.scaledTiles()) {
+			draw.rectangle(tile)
 		}
 	}
 }
+
 
