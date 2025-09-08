aa63c7aca0e24aa69a6b1f21aae6dde51f75b257
kristian <krispetter@gmail.com>
2025-09-08 14:14:37 +0200

    x

---
+++ static/engine/os/AudioDb.js
@@ -0,0 +1,51 @@
+class AudioDB {
+	static dbName = 'audioDB'
+	static storeName = 'clips'
+	static db = null
+	static ready = false
+
+	static {
+		const req = indexedDB.open(this.dbName, 1)
+		req.onupgradeneeded = () => req.result.createObjectStore(this.storeName)
+		req.onsuccess = () => {
+  	this.db = req.result
+  	this.ready = true
+		}
+	}
+
+	static _onReady(callback) {
+		const check = () => {
+  	if (this.ready) {
+				callback(this.db)
+			}
+  	else {
+				setTimeout(check, 10)
+			}
+		}
+		check()
+	}
+
+	static save(key, blob) {
+		this._onReady(db => {
+  	const tx = db.transaction(this.storeName, 'readwrite')
+  	tx.objectStore(this.storeName).put(blob, key)
+		})
+	}
+
+	static load(key, callback) {
+		this._onReady(db => {
+  	const tx = db.transaction(this.storeName, 'readonly')
+  	const req = tx.objectStore(this.storeName).get(key)
+  	req.onsuccess = () => callback(req.result)
+		})
+	}
+
+	static delete(key) {
+		this._onReady(db => {
+  	const tx = db.transaction(this.storeName, 'readwrite')
+  	tx.objectStore(this.storeName).delete(key)
+		})
+	}
+
+}
+
--- static/engine/os/Microphone.js
+++ static/engine/os/Microphone.js
@@ -1,11 +1,10 @@
-// doesn't behave same on phone and pc
-
 export class Microphone {
-	static {
-		this.recorder = null
-		this.chunks = []
-		this.ready = false
+	static recorder = null
+	static chunks = []
+	static ready = false
+	static state = 'idle' // idle, recording, stopped
 
+	static {
 		navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
   	this.recorder = new MediaRecorder(stream)
   	this.recorder.ondataavailable = e => this.chunks.push(e.data)
@@ -18,8 +17,12 @@ export class Microphone {
   	if (!this.ready) {
 				return setTimeout(wait, 10)
 			}
+  	if (this.state === 'recording') {
+				throw new Error('Microphone already recording')
+			}
   	this.chunks = []
   	this.recorder.start()
+  	this.state = 'recording'
   	if (callback) {
 				callback()
 			}
@@ -32,10 +35,14 @@ export class Microphone {
   	if (!this.ready) {
 				return setTimeout(wait, 10)
 			}
+  	if (this.state !== 'recording') {
+				throw new Error('Microphone not recording')
+			}
   	this.recorder.onstop = () => {
-				const blob = new Blob(this.chunks, { type: 'audio/webm' })
-				this.chunks = []
-				if (callback) {
+    	const blob = new Blob(this.chunks, { type: 'audio/webm' })
+    	this.chunks = []
+    	this.state = 'stopped'
+    	if (callback) {
 					callback(blob)
 				}
   	}
