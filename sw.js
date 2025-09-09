aa8ef3cb6ba3460d6065d02d7e730f8991fb30df
Your Name <you@example.com>
2025-09-09 19:19:31 +0200

    x

--- sw.js
+++ sw.js
@@ -1,22 +1,30 @@
 const CACHE_NAME = RANDOM_UUID
 
 self.addEventListener('install', event => {
-	event.waitUntil(
-		caches.open(CACHE_NAME).then(async cache => {
-			for (const url of ['/', '/manifest.json', ...ALL_FILES]) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(async cache => {
+      for (const url of ['/', '/manifest.json', ...ALL_FILES]) {
+        try {
+          console.log('Trying to cache', url)
+          const resp = await cache.add(url)
+          console.log('Cached successfully:', url)
+        } catch (e) {
+          console.error('Failed to cache', url, e)
+        }
 
-				console.log("caching " + url)
-
-				await Promise.all([
-					cache.add(url)
-						.catch(e => console.error('Failed to cache', url, e)),
-					cache.add('https://romskip.netlify.app' + url)
-						.catch(e => console.error('Failed to cache ROMSKIP.NETLIFY.APP', url, e)),
-				])
-			}
-		})
-	)
+        try {
+          const fullUrl = 'https://romskip.netlify.app' + url
+          console.log('Trying to cache', fullUrl)
+          const resp = await cache.add(fullUrl)
+          console.log('Cached successfully:', fullUrl)
+        } catch (e) {
+          console.error('Failed to cache', fullUrl, e)
+        }
+      }
+    })
+  )
 })
+
 
 self.addEventListener('activate', e => {
 	e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))))
@@ -24,16 +32,14 @@ self.addEventListener('activate', e => {
 
 
 self.addEventListener('fetch', e => {
-    const url = new URL(e.request.url)
-    const relativeRequest = new Request(url.pathname, e.request)
-    
-    e.respondWith(
-        caches.match(relativeRequest)
-            .then(response => response || fetch(relativeRequest))
-            .catch(err => {
-                console.error('Fetch handler error for URL:', relativeRequest.url, err);
-                return fetch(relativeRequest);
-            })
-    )
+  const url = new URL(e.request.url)
+  const pathnameReq = new Request(url.pathname + url.search, e.request)
+
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => r || caches.match(pathnameReq))
+      .then(r => r || fetch(e.request))
+      .catch(() => fetch(e.request))
+  )
 })
 
