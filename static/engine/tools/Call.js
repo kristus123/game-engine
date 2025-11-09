f4999ae15a6d8e7b6225a665e5a4bc47f31e803a
Nabir14 <mushfiquefarhannabir@gmail.com>
2025-11-09 19:03:41 +0600

    GridTile Class (Fix)

--- static/engine/tools/Call.js
+++ static/engine/tools/Call.js
@@ -4,6 +4,7 @@
 // Call(o.onHit)
 // That won't work
 // to fix it you need to bind object
+// this class can just be deleted it is garbage
 // https://chat.openai.com/share/123d17a0-321b-461f-80b9-f8eedb7ae687
 
 export const Call = method => {
