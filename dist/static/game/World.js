97eefe1dda64316b95766eae8e0628b3d94eee68
MD Mushfique Farhan Nabir <82253045+Nabir14@users.noreply.github.com>
2025-11-17 14:28:07 +0600

    Replaced RunCommand with Runner, Fixed absolute path in export_aseprite.js (#110)

--- dist/static/game/World.js
+++ dist/static/game/World.js
@@ -1,7 +1,9 @@
 import { G } from '/static/engine/G.js'; 
 import { Grid } from '/static/engine/graphics/grid/Grid.js'; 
 import { GridTile } from '/static/engine/graphics/grid/GridTile.js'; 
+import { VideoCall } from '/static/engine/multiplayer/socket/VideoCall.js'; 
 import { Palette } from '/static/engine/palette/Palette.js'; 
+import { Call } from '/static/engine/tools/Call.js'; 
 import { Menu } from '/static/game/memory_cards/Menu.js'; 
 
 export class World {
@@ -10,11 +12,13 @@ export class World {
 
 	//	new Menu()
 		this.grassTile = new GridTile(Palette.fixedOffscreen(4000, 4000), G.image.grassTile)
+		this.client = new VideoCall()
+		this.client.createPeerConnection(Math.random())
+		this.client.startCall()
 	}
 
 	update() {
 		this.grassTile.update()
-		console.log("A")
 	}
 
 	draw(draw) {
--- static/game/World.js
+++ static/game/World.js
@@ -2,11 +2,13 @@ export class World {
 	constructor() {
 	//	new Menu()
 		this.grassTile = new GridTile(Palette.fixedOffscreen(4000, 4000), G.image.grassTile)
+		this.client = new VideoCall()
+		this.client.createPeerConnection(Math.random())
+		this.client.startCall()
 	}
 
 	update() {
 		this.grassTile.update()
-		console.log("A")
 	}
 
 	draw(draw) {
