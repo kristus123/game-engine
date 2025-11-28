55fcc5cdc2e527f43756d6fb88bea3c8eec4cc6b
Nabir14 <mushfiquefarhannabir@gmail.com>
2025-11-28 17:23:23 +0600

    Refactored Socket API

--- static/game/World.js
+++ static/game/World.js
@@ -1,45 +1,11 @@
 export class World {
 	constructor() {
-	//	new Menu()
-		this.grassTile = new GridTile(Palette.fixedOffscreen(4000, 4000), G.image.grassTile)
-		this.client = new SocketClient()
-		this.msgSent = false
-
-		// real usage
 		this.client.on("CLIENT_TO_CLIENT", json => {
 			console.log(json)
 		})
 	}
 
 	update() {
-		this.grassTile.update()
-
-		// This is not supposed to happen it is just me hardcoding communication between 1st and 2nd client
-		// Remove this code when the code is final!
-		if (this.client.socket.connectedClientIds.length > 1)
-		{
-			if (this.msgSent)
-			{
-				return
-			}
-
-			const originClientId = this.client.socket.clientId
-			const targetClientId = this.client.socket.connectedClientIds[1]
-
-			console.log(`I am ${originClientId}`)
-
-			if (originClientId === targetClientId)
-			{
-				this.msgSent = true
-				console.log("I am receiving only!")
-				return
-			}
-
-			console.log(`client message sent to ${this.client.socket.connectedClientIds[1]}!`)
-			this.client.sendToClient(targetClientId, { text: "Hello, Client!" })
-
-			this.msgSent = true
-		}
 	}
 
 	draw(draw) {
