c289f868d51d0e99e053041b5f9b2992382d6672
Nabir14 <mushfiquefarhannabir@gmail.com>
2025-12-06 21:57:41 +0600

    changed /game/code -> /game

--- build_tools/js_files.js
+++ build_tools/js_files.js
@@ -1,5 +1,5 @@
 const Files = require('./Files')
 
-module.exports = [...Files.at("engine/core"), ...Files.at("game/code")]
+module.exports = [...Files.at("engine/core"), ...Files.at("game/")]
 	.filter(f => f.endsWith('.js'))
 	.map(f => f.replaceAll('\\', '/'))
