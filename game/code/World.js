387bf4571011f450020625d059b60636f1a1f53e
Your Name <you@example.com>
2025-12-22 18:21:56 +0100

    layout

--- game/code/World.js
+++ game/code/World.js
@@ -4,6 +4,18 @@ export class World {
 		this.client.on('CLIENT_TO_CLIENT', json => {
 			console.log(json)
 		})
+<<<<<<< HEAD
+||||||| parent of 9cfb5777 (layout)
+
+		window.r = new RtcClient()
+=======
+
+		Html.center([
+			Html.button('hei'),
+			Html.button('hei'),
+		])
+
+>>>>>>> 9cfb5777 (layout)
 	}
 
 	update() {
