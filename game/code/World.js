26c88034084cd549755b3c6cc3655e964e7eb73f
Your Name <you@example.com>
2025-12-22 19:02:31 +0100

    x

--- game/code/World.js
+++ game/code/World.js
@@ -1,31 +1,6 @@
 export class World {
 	constructor() {
-		this.client = new SocketClient()
-		this.client.on('CLIENT_TO_CLIENT', json => {
-			console.log(json)
-		})
-<<<<<<< HEAD
-||||||| parent of 9cfb5777 (layout)
-
-		window.r = new RtcClient()
-=======
-
-<<<<<<< HEAD
-		Html.center([
-			Html.button('hei'),
-			Html.button('hei'),
-		])
-
->>>>>>> 9cfb5777 (layout)
-||||||| parent of d613e544 (x)
-		Html.center([
-			Html.button('hei'),
-			Html.button('hei'),
-		])
-
-=======
 		Layout.test()
->>>>>>> d613e544 (x)
 	}
 
 	update() {
