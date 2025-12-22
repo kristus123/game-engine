f3dab57d57659f3069fd1ab97af756c410bb98a6
Your Name <you@example.com>
2025-12-22 18:44:32 +0100

    x

--- engine/html/Layout.js
+++ engine/html/Layout.js
@@ -1,16 +1,18 @@
 export class Layout {
 	static test() {
 		return Html.addToScreen(
-			Html.div('default-grid', [
-				Html.div('grid-left', [
-					Html.button('left'),
-				]),
-				Html.div('grid-top', [
-					Html.button('top'),
-				]),
-				Html.div('grid-mid', [
-					Html.button('mid'),
-				]),
-			]))
+			Html.div('overlay', [
+				Html.div('default-grid', [
+					Html.div('grid-left', [
+						Html.button('left'),
+					]),
+					Html.div('grid-top', [
+						Html.button('top'),
+					]),
+					Html.div('grid-mid', [
+						Html.button('mid'),
+					]),
+			])])
+		)
 	}
 }
