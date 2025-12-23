7122e4bc025bc6c2a1a998d3cdbc06ac390d9b4d
Your Name <you@example.com>
2025-12-23 10:50:18 +0100

    x

--- engine/html/UiGrid.js
+++ engine/html/UiGrid.js
@@ -2,15 +2,15 @@ export class UiGrid {
 	static test() {
 		Ui.overlay([
 			Html.div('default-grid', [
+				Html.div('grid-top', [
+					Html.button('top'),
+				]),
 				Html.div('grid-left', [
 					Html.button('left'),
 					Html.p('wow not me'),
 				]),
 				Html.div('grid-right', [
 					Html.button('right'),
-				]),
-				Html.div('grid-top', [
-					Html.button('top'),
 				]),
 				Html.div('grid-mid', [
 					Html.button('mid'),
