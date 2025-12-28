652fa11cab3c330fbc665857157cfeb42219f8af
Your Name <you@example.com>
2025-12-28 19:47:26 +0100

    move .md files into its own folder

--- deploy_dist_to_netlify.sh
+++ deploy_dist_to_netlify.sh
@@ -12,6 +12,7 @@ find dist -type f -name '*.css' -exec npx lightningcss --minify {} -o {} \;
 find dist -type f -name '*.html' -exec npx html-minifier-terser --collapse-whitespace --remove-comments -o {} {} \;
 
 
+# the id is 'romskip' aka romskip.netlify.app
 netlify deploy --prod --dir=dist --site=30335998-1c5b-49e4-b97c-cb0f1f5b3893
 
 rm -r .netlify
--- lint.sh
+++ lint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 clear
+echo "linting project"
 
 node build_tools/update_eslint.js
-
 
 find . -name "*.js" -type f -exec sed -i 's/^\(\s*\)    /\1\t/g' {} +
 
 npx eslint . --fix
-
 
 npx stylelint "**/*.css" --fix
