df7dd13222ef233b4957fa533586f77e68771587
Your Name <you@example.com>
2026-01-02 20:15:48 +0100

    testsfu

--- engine/multiplayer/socket/client/RtcClient.js
+++ engine/multiplayer/socket/client/RtcClient.js
@@ -1,4 +1,6 @@
 // ClientId(
+//
+import * as mediasoupClient from 'mediasoup-client'
 
 export class RtcClient {
 
@@ -6,11 +8,12 @@ export class RtcClient {
 		this.peers = {}
 		this.offers = {}
 		this.localStream = null
-		this.startLocalStream()
+		this.sfuChannel = null
 
 		this.onData = () => {}
-
 		this.onIncomingCall = (callerClientId) => {}
+
+		this.init()
 
 		SocketClient.onClientMessage('INCOMING_CALL', data => {
 			this.offers[data.originClientId] = data.offer
@@ -18,47 +21,36 @@ export class RtcClient {
 		})
 
 		SocketClient.onClientMessage('ACCEPT_INCOMING_CALL', data => {
-			const peerConnection = this.peers[data.originClientId].peerConnection
-			if (peerConnection) {
-				peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer))
-					.catch(err => {
-						console.warn('setRemoteDescription failed:', err)
-					})
-			}
-			else {
-				console.warn('ACCEPT_INCOMING_CALL received but no valid RTCPeerConnection found for', data.originClientId)
-			}
+			const peerConnection = this.peers[data.originClientId]?.peerConnection
+			if (peerConnection) peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer))
+				.catch(err => console.warn('setRemoteDescription failed:', err))
 		})
 
 		SocketClient.onClientMessage('ICE_CANDIDATE', data => {
-			const peerConnection = this.peers[data.originClientId].peerConnection
-			if (peerConnection) {
-				peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate))
-					.catch(err => {
-						console.warn('addIceCandidate failed:', err)
-					})
-			}
-			else {
-				console.warn('ICE_CANDIDATE received but no valid RTCPeerConnection found for', data.originClientId)
-			}
+			const peerConnection = this.peers[data.originClientId]?.peerConnection
+			if (peerConnection) peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate))
+				.catch(err => console.warn('addIceCandidate failed:', err))
 		})
 	}
+
+	static async init() {
+		await this.startLocalStream()
+		await this.createMediasoupChannel()
+	}
+
+	// ------------------- P2P -------------------
 
 	static call(targetClientId) {
 		const { peerConnection, dataChannel } = this.createPeerConnectionWith(targetClientId)
 		this.peers[targetClientId] = { peerConnection, dataChannel }
 
-		// this.localStream.getTracks().forEach(track => {
-		// 	peerConnection.addTrack(track, this.localStream)
-		// }) // my pc does not have a webcam
+		// this.localStream.getTracks().forEach(track => peerConnection.addTrack(track, this.localStream))
 
 		peerConnection.createOffer()
 			.then(offer => peerConnection.setLocalDescription(offer))
-			.then(() => {
-				SocketClient.sendToClient('INCOMING_CALL', targetClientId, {
-					offer: peerConnection.localDescription
-				})
-			})
+			.then(() => SocketClient.sendToClient('INCOMING_CALL', targetClientId, {
+				offer: peerConnection.localDescription
+			}))
 	}
 
 	static acceptCall(callerClientId) {
@@ -66,16 +58,12 @@ export class RtcClient {
 		this.peers[callerClientId] = { peerConnection, dataChannel }
 
 		peerConnection.setRemoteDescription(new RTCSessionDescription(this.offers[callerClientId]))
-			// .then(() => this.localStream.getTracks().forEach(track => {
-			// 	peerConnection.addTrack(track, this.localStream)
-			// }))
+			// .then(() => this.localStream.getTracks().forEach(track => peerConnection.addTrack(track, this.localStream)))
 			.then(() => peerConnection.createAnswer())
 			.then(answer => peerConnection.setLocalDescription(answer))
-			.then(() => {
-				SocketClient.sendToClient('ACCEPT_INCOMING_CALL', callerClientId, {
-					answer: peerConnection.localDescription
-				})
-			})
+			.then(() => SocketClient.sendToClient('ACCEPT_INCOMING_CALL', callerClientId, {
+				answer: peerConnection.localDescription
+			}))
 	}
 
 	static send(targetClientId, data) {
@@ -83,56 +71,74 @@ export class RtcClient {
 	}
 
 	static createPeerConnectionWith(targetClientId) {
-		const peerConnection = new RTCPeerConnection({
-			iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
-		})
+		const peerConnection = new RTCPeerConnection({ iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] })
 
-		peerConnection.ontrack = () => {
-			// Handle remote stream
-		}
+		peerConnection.ontrack = () => {}
 
 		peerConnection.onicecandidate = e => {
-			if (e.candidate) {
-				SocketClient.sendToClient('ICE_CANDIDATE', targetClientId, {
-					candidate: e.candidate,
-				})
-			}
+			if (e.candidate) SocketClient.sendToClient('ICE_CANDIDATE', targetClientId, { candidate: e.candidate })
 		}
 
 		const dataChannel = peerConnection.createDataChannel('data')
-
 		dataChannel.onopen = () => console.log('Data channel opened')
-		dataChannel.onmessage = (e) => {
-			console.log(e)
-			this.onData(JSON.parse(e.data))
-		}
+		dataChannel.onmessage = (e) => this.onData(JSON.parse(e.data))
 
 		peerConnection.ondatachannel = (e) => {
 			const channel = e.channel
 			channel.onopen = () => console.log('Data channel opened')
-			channel.onmessage = (e) => {
-				this.onData(JSON.parse(e.data))
-			}
+			channel.onmessage = (e) => this.onData(JSON.parse(e.data))
 		}
 
 		return { peerConnection, dataChannel }
 	}
 
-	static startLocalStream() {
-		navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-			.then(stream => {
-				this.localStream = stream
-				console.log('localStream has been set!')
-			})
+	// ------------------- Mediasoup SFU -------------------
+
+	static async createMediasoupChannel() {
+		this.sfuChannel = {
+			sendTransport: null,
+			producer: null,
+			consumers: new Map(),
+		}
+
+		const routerRtpCapabilities = await SocketClient.request('getRouterRtpCapabilities')
+
+		const params = await SocketClient.request('createWebRtcTransport', { direction: 'send' })
+		const device = new mediasoupClient.Device()
+		await device.load({ routerRtpCapabilities })
+		const transport = device.createSendTransport(params)
+		this.sfuChannel.sendTransport = transport
+
+		transport.on('connect', ({ dtlsParameters }, callback, errback) => {
+			SocketClient.send('connectTransport', { dtlsParameters })
+			callback()
+		})
+
+		transport.on('produce', async ({ kind, rtpParameters }, callback, errback) => {
+			const id = await SocketClient.request('produce', { kind, rtpParameters })
+			callback({ id })
+		})
+
+		if (this.localStream) {
+			const track = this.localStream.getAudioTracks()[0] || this.localStream.getVideoTracks()[0]
+			this.sfuChannel.producer = await transport.produce({ track })
+		}
+	}
+
+	// ------------------- Utilities -------------------
+
+	static async startLocalStream() {
+		const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+		this.localStream = stream
+		console.log('localStream has been set!')
 	}
 
 	static stopCall() {
-		for (const clientId in this.peers) {
-			this.peers[clientId].close()
-		}
+		for (const clientId in this.peers) this.peers[clientId].close?.()
 		this.peers = {}
+		this.sfuChannel = null
 	}
 }
 
-
 window.RtcClient = RtcClient
+
--- server/socket/SocketServer.js
+++ server/socket/SocketServer.js
@@ -1,33 +1,147 @@
 import SocketServer from './SimplifiedSocketServerAPI.js'
+import mediasoup from 'mediasoup'
 
 const socketServer = new SocketServer(8082)
 
-socketServer.onConnection = (client, clientId) => {
-	console.log(`${clientId} has connected`)
+// --- Mediasoup setup ---
+const workers = []
+let router
+const rooms = new Map()
 
-	socketServer.sendToEveryone({
-		action: 'UPDATE_CLIENTS_LIST',
-		clientIds: socketServer.allClientIds,
-		originClientId: clientId
-	})
+async function createWorker() {
+    const worker = await mediasoup.createWorker({
+        rtcMinPort: 20000,
+        rtcMaxPort: 20100,
+        logLevel: 'warn',
+        logTags: ['info', 'ice', 'dtls', 'rtp', 'srtp']
+    })
+
+    worker.on('died', () => {
+        console.error('mediasoup worker died, exiting...')
+        process.exit(1)
+    })
+
+    return worker
+}
+
+async function initMediasoup() {
+    const worker = await createWorker()
+
+    router = await worker.createRouter({
+        mediaCodecs: [
+            { kind: 'audio', mimeType: 'audio/opus', clockRate: 48000, channels: 2 },
+            { kind: 'video', mimeType: 'video/VP8', clockRate: 90000 }
+        ]
+    })
+
+    rooms.set('default', { router, peers: new Map() })
+}
+
+await initMediasoup()
+
+// --- SocketServer handlers ---
+socketServer.onConnection = (client, clientId) => {
+    console.log(`${clientId} connected`)
+
+    socketServer.sendToEveryone({
+        action: 'UPDATE_CLIENTS_LIST',
+        clientIds: socketServer.allClientIds,
+        originClientId: clientId
+    })
 }
 
 socketServer.on('CLIENT_TO_CLIENT', (client, clientId, data) => {
-	console.log(`Server Passing Message: ${JSON.stringify(data)}`)
+    const index = socketServer.allClientIds.indexOf(data.targetClientId)
+    const targetClient = socketServer.allClients[index]
+    socketServer.sendToClient(targetClient, data)
+})
 
-	const index = socketServer.allClientIds.indexOf(data.targetClientId)
-	const targetClient = socketServer.allClients[index]
+// --- Mediasoup signaling ---
+socketServer.on('getRouterRtpCapabilities', (client, clientId, callback) => {
+    callback(router.rtpCapabilities)
+})
 
-	socketServer.sendToClient(targetClient, data)
+socketServer.on('createWebRtcTransport', async (client, clientId, { direction }, callback) => {
+    const transport = await rooms.get('default').router.createWebRtcTransport({
+        listenIps: [{ ip: '0.0.0.0', announcedIp: null }],
+        enableUdp: true,
+        enableTcp: true,
+        preferUdp: true
+    })
+
+    rooms.get('default').peers.set(clientId, rooms.get('default').peers.get(clientId) || {})
+    const peer = rooms.get('default').peers.get(clientId)
+    peer[direction + 'Transport'] = transport
+
+    callback({
+        id: transport.id,
+        iceParameters: transport.iceParameters,
+        iceCandidates: transport.iceCandidates,
+        dtlsParameters: transport.dtlsParameters
+    })
+})
+
+socketServer.on('connectTransport', (client, clientId, { dtlsParameters }) => {
+    const peer = rooms.get('default').peers.get(clientId)
+    const transport = peer.sendTransport || peer.recvTransport
+    transport.connect({ dtlsParameters })
+})
+
+socketServer.on('produce', async (client, clientId, { kind, rtpParameters }, callback) => {
+    const peer = rooms.get('default').peers.get(clientId)
+    const transport = peer.sendTransport
+    const producer = await transport.produce({ kind, rtpParameters })
+    peer.producer = producer
+
+    // Notify all other clients to consume this producer
+    for (const [otherId, otherPeer] of rooms.get('default').peers) {
+        if (otherId !== clientId) {
+            socketServer.sendToClient(socketServer.allClients[socketServer.allClientIds.indexOf(otherId)], {
+                action: 'NEW_PRODUCER',
+                producerId: producer.id,
+                producerClientId: clientId
+            })
+        }
+    }
+
+    callback(producer.id)
+})
+
+socketServer.on('consume', async (client, clientId, { producerId }, callback) => {
+    const peer = rooms.get('default').peers.get(clientId)
+    peer.recvTransport = peer.recvTransport || await rooms.get('default').router.createWebRtcTransport({
+        listenIps: [{ ip: '0.0.0.0', announcedIp: null }],
+        enableUdp: true,
+        enableTcp: true,
+        preferUdp: true
+    })
+
+    const consumer = await peer.recvTransport.consume({
+        producerId,
+        rtpCapabilities: rooms.get('default').router.rtpCapabilities,
+        paused: false
+    })
+
+    peer.consumers = peer.consumers || new Map()
+    peer.consumers.set(producerId, consumer)
+
+    callback({
+        producerId,
+        id: consumer.id,
+        kind: consumer.kind,
+        rtpParameters: consumer.rtpParameters
+    })
 })
 
 socketServer.onClose = (client, clientId) => {
-	console.log(`${clientId} has disconnected`)
+    console.log(`${clientId} disconnected`)
+    rooms.get('default').peers.delete(clientId)
 
-	socketServer.sendToEveryone({
-		action: 'REMOVE_CLIENT',
-		clientId: clientId
-	})
+    socketServer.sendToEveryone({
+        action: 'REMOVE_CLIENT',
+        clientId
+    })
 }
 
 socketServer.start()
+
