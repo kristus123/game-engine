5987400ef53568526ef2a8be592e257c09a128e9
Your Name <you@example.com>
2026-01-03 12:04:38 +0100

    x

--- server/http/main.js
+++ server/http/main.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 
+import { FileDb } from './FileDb.js'
+
 const app = express()
 app.use(express.json()) // Automatically parses JSON bodies
 app.use(cors())
 
 app.post('/*', (req, res) => {
-	const xxx = req.params[0] // This captures everything after /path/
+	const xxx = req.params[0]
+
+	FileDb.save('test', req.body)
 
 	res.send({
 		test: xxx,
