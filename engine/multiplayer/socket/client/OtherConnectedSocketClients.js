3e025dc43ff6b75ce967c29d20578f0774b1c42a
ojogunemmy <ojogunemmy@gmail.com>
2026-01-06 19:50:20 +0100

    x

--- engine/multiplayer/socket/client/OtherConnectedSocketClients.js
+++ engine/multiplayer/socket/client/OtherConnectedSocketClients.js
@@ -3,6 +3,7 @@
 export class OtherConnectedSocketClients {
 	static {
 		this.ids = []
+		this.clientButtons = new Map()
 
 		this.onConnect = (clientId) => {}
 		this.onDisconnect = (clientId) => {}
@@ -19,9 +20,21 @@ export class OtherConnectedSocketClients {
 	static remove(clientId) {
 		if (this.ids.includes(clientId)) {
 			this.ids.remove(clientId)
+			
+			// Clean up button
+			const button = this.clientButtons.get(clientId)
+			if (button) {
+				button.remove()
+				this.clientButtons.delete(clientId)
+			}
+			
 			this.onDisconnect(clientId)
 		}
 		console.log(this.ids)
+	}
+
+	static registerButton(clientId, button) {
+		this.clientButtons.set(clientId, button)
 	}
 
 	static [Symbol.iterator]() {
