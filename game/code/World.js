28761bf8e0c9f909a9252a8d2cc87dff4293b25c
Andreas <als0825a@gmail.com>
2026-01-11 14:24:58 +0100

    lint

--- game/code/World.js
+++ game/code/World.js
@@ -3,6 +3,7 @@
 export class World {
 	constructor() {
 		const player = new DynamicGameObject(new Position(8000, 6000))
+		this.player=player
 
 		Controller.control(player)
 		Camera.followInstantly(player)
@@ -11,34 +12,12 @@ export class World {
 			Sprite.snow(new Position(0, 0), 6),
 			Sprite.samurai(player.position, 0.5),
 		])
-
-		SocketClient.onServerMessage('UPDATE_CLIENTS_LIST', data => {
-			console.log(`Logging From Game: ${JSON.stringify(data)}.`)
-		})
-
-		GridUi.top.set(Html.input('json goes here', (value) => {
-			GridUi.mid.push([
-				Html.button('write json', (json) => {
-					HttpClient.uploadFile({ 'test': value }, body => {
-						console.log('___')
-						console.log(body)
-						console.log('___')
-					})
-				}),
-				Html.button('get json', (json) => {
-					HttpClient.readFile({ 'filename': 'test' }, body => {
-						console.log('___')
-						console.log(body)
-						console.log('___')
-					})
-				})
-			])
-		}))
 	}
 
 
 	update() {
 		this.localObjects.update()
+		const x = Ui.floating(Html.p('Hei'), this.player.position)
 	}
 
 	draw(draw) {}
