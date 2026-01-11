53bee1ed7d3d2e7ab3421e612874a98214adce8c
Your Name <you@example.com>
2026-01-11 14:55:21 +0100

    x

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,12 +1,7 @@
 export default {
-	extends: ['stylelint-config-standard'],
-	rules: {
-		'indentation': 'tab',
-		'string-quotes': 'double',
-		'color-hex-case': 'lower',
-		'color-hex-length': 'short',
-		'max-empty-lines': 2,
-		'no-descending-specificity': null
-	},
+	'extends': ['stylelint-config-standard'],
+	'rules': {
+		'indentation': 'tab'
+	}
 }
 
