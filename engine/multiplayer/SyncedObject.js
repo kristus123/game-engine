f60de9c05f6f391316aaae1adb2e0b71d33a93af
Nabir14 <mushfiquefarhannabir@gmail.com>
2026-01-12 17:19:10 +0600

    lint

--- engine/multiplayer/SyncedObject.js
+++ engine/multiplayer/SyncedObject.js
@@ -1,18 +1,20 @@
 export class SyncedObject {
-    static link(targetClientId, objectId, obj) {
-        const proxy = ProxyObject(obj, (key, value) => {
-            SocketClient.sendToClient('SYNC_OBJECT', targetClientId, {
-                fields: { [key]: value }
-            })
-        })
+	static link(targetClientId, objectId, obj) {
+    	const proxy = ProxyObject(obj, (key, value) => {
+        	SocketClient.sendToClient('SYNC_OBJECT', targetClientId, {
+            	fields: { [key]: value }
+        	})
+    	})
 
-        SocketClient.onClientMessage('SYNC_OBJECT', data => {
-            if (!data.fields) return
-            for (const key in data.fields) {
-                proxy[key] = data.fields[key]
-            }
-        })
+    	SocketClient.onClientMessage('SYNC_OBJECT', data => {
+        	if (!data.fields) {
+				return
+			}
+        	for (const key in data.fields) {
+            	proxy[key] = data.fields[key]
+        	}
+    	})
 
-        return proxy
-    }
+    	return proxy
+	}
 }
