c9e952c9ae976afad9ea74eb05678771e404c14d
Andreas <als0825a@gmail.com>
2026-01-13 11:37:37 +0100

    lint

--- game/code/Story.js
+++ game/code/Story.js
@@ -2,15 +2,15 @@ export class Story {
 	static show(viking) {
 		this.hide()
 		StoryUi.top.add(Html.p(viking.text))
-    	StoryUi.mid.add(Html.image(viking.image))
+		StoryUi.mid.add(Html.image(viking.image))
 		for (const choice of viking.choices) {
   			StoryUi.bottom.add(Html.button(choice.text, choice.onSelect))
 		}
 	}
 
 	static hide() {
-    	StoryUi.top.clear()
-    	StoryUi.mid.clear()
+		StoryUi.top.clear()
+		StoryUi.mid.clear()
 		StoryUi.bottom.clear()
 	}
 }
