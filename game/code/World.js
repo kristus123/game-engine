7321f622617662e49e27bfbbd4ca0dc48eccb761
Your Name <you@example.com>
2026-01-14 12:57:01 +0100

    lint

--- game/code/World.js
+++ game/code/World.js
@@ -2,30 +2,17 @@
 
 export class World {
 	constructor() {
-		const player = new DynamicGameObject(new Position(8000, 6000))
+		const div = Dom.overlay()
 
-		Controller.control(player)
-		Camera.followInstantly(player)
+		OtherConnectedSocketClients.register((onConnect, onDisconnect) => {
 
-		this.localObjects = new LocalObjects([
-			Sprite.snow(new Position(0, 0), 6),
-			Sprite.samurai(player.position, 0.5),
-		])
+			onConnect(connectingClientId => {
+			})
 
-		OtherConnectedSocketClients.onConnect = connectingClientId => {
-			console.log("client connected")
-			const player = SyncedObject.link(connectingClientId, "PLAYER", { hp: 100 })
+			onDisconnect(disconnectingClientId => {
 
-			Dom.add(Html.button('damage person', () => {
-				player.hp -= 1
-				console.log(player.hp)
-			}))
-			console.log("all done")
-
-			setInterval(() => {
-				Dom.add(Html.p(player.hp))
-			}, 200);
-		}
+			})
+		})
 	}
 
 
