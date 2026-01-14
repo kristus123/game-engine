2ffeeff7ab252f428598c46e187c31499a0beb97
kristian <krispetter@gmail.com>
2026-01-14 10:33:15 +0100

    PR (#165)
    * cxxxxxx
    
    * x
    
    * lint

--- build_tools/Aseprite.js
+++ build_tools/Aseprite.js
@@ -68,8 +68,20 @@ export class Aseprite {
 			'--filename-format',
 			'{layer}_{frame}_{tag}',
 		], { shell: true })
-
 	}
+
+	static groups(srcFile, destBase) {
+		execFileSync(bin, [
+			'-b',
+			'--list-layers',
+			srcFile,
+			'--data',
+			destBase + 'Groups.json',
+			'--format',
+			'json-array',
+		], { shell: true })
+	}
+
 
 	static tilemaps(srcFile, destBase) {
 		execFileSync(bin, [
