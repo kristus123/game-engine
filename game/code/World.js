8f99ea1485c46d8b03e23fcfa52294996030556b
Your Name <you@example.com>
2026-01-16 12:57:15 +0100

    x

--- game/code/World.js
+++ game/code/World.js
@@ -2,7 +2,7 @@
 
 export class World {
 	constructor() {
-		const mePosition = SyncedObject.create('PLAYER', {x: 0, y:0})
+		const syncedPlayer = SyncedObject.create('PLAYER', {x: 0, y:0})
 
 		SyncedObject.link('clientId_2', 'PLAYER', player => {
 			// here is the synced player object
