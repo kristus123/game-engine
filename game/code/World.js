96fa9aa3e44f7ad57885df11be8b3c3946fdd846
als0825a-sketch <als0825a@gmail.com>
2026-01-16 12:06:03 +0100

    lint (#177)

--- game/code/World.js
+++ game/code/World.js
@@ -1,22 +1,8 @@
 // ClientId(
 
-
 export class World {
 	constructor() {
-		OtherConnectedSocketClients.add('Mom')
-		OtherConnectedSocketClients.onJoin((clientId, onLeave) => {
-			console.log('Someone joined')
-			const b = Html.button('Player')
-			Dom.overlay(b)
-			console.log(onLeave)
-			onLeave(() => {
-				console.log('Someone left')
-				b.remove()
-			})
-		})
-		setTimeout(() => {
-			OtherConnectedSocketClients.remove('Mom')
-		}, 2000)
+		Page.go(LandingPage)
 	}
 
 
