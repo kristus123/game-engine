7959b7794a7cbe3f20d750b97e6f6f41c1a29e99
Andreas <als0825a@gmail.com>
2026-01-24 13:24:52 +0100

    lint

--- build_tools/transpiler.js
+++ build_tools/transpiler.js
@@ -1,3 +1,6 @@
+import path from 'path'
+
+
 const ENVIRONMENT = process.argv[2] || false
 
 if (!ENVIRONMENT) {
@@ -12,7 +15,15 @@ import Files from './Files.js'
 import jsFiles from './js_files.js'
 
 for (const jsFilePath of jsFiles) {
+
 	let fileContent = Files.read(jsFilePath)
+
+	jsFiles.forEach(f => {
+		const className = path.parse(f).name
+		if (Files.read(f).includes(`export class ${className}`)) {
+			fileContent = fileContent.replaceAll(` ${className}(`, ` new ${className}(`)
+		}
+	})
 
 	fileContent = fileContent.replaceAll('tla(', 'this.localObjects.add(')
 	fileContent = fileContent.replaceAll('OnChange(', 'new OnChange(')
