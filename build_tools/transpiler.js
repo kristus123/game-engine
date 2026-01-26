5759b6c01ce6c1ebbd92065c2668d588e8b3394a
Your Name <you@example.com>
2026-01-26 10:36:11 +0100

    x

--- build_tools/transpiler.js
+++ build_tools/transpiler.js
@@ -1,13 +1,10 @@
-import path from 'path'
-
-
 const ENVIRONMENT = process.argv[2] || false
 
 if (!ENVIRONMENT) {
 	throw new Error('you need to include ENVIRONMENT when calling generate_dist.js')
 }
 
-import path from 'path'
+
 import Imports from './Imports.js'
 import Parameters from './Parameters.js'
 import Files from './Files.js'
@@ -15,15 +12,7 @@ import Files from './Files.js'
 import jsFiles from './js_files.js'
 
 for (const jsFilePath of jsFiles) {
-
 	let fileContent = Files.read(jsFilePath)
-
-	jsFiles.forEach(f => {
-		const className = path.parse(f).name
-		if (Files.read(f).includes(`export class ${className}`)) {
-			fileContent = fileContent.replaceAll(` ${className}(`, ` new ${className}(`)
-		}
-	})
 
 	fileContent = fileContent.replaceAll('tla(', 'this.localObjects.add(')
 	fileContent = fileContent.replaceAll('OnChange(', 'new OnChange(')
