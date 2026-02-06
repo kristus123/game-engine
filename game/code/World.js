00f7d32740aa55a7c3fe8787319970e66757fd00
Your Name <you@example.com>
2026-02-06 17:24:22 +0100

    x

--- game/code/World.js
+++ game/code/World.js
@@ -4,6 +4,7 @@ export class World {
 			this.player = Player(WorldPosition(0, 0)),
 			Sprite.snow(D3, WorldPosition(0, 0), 4)
 		])
+
 		Controller.control(this.player)
 		for (const p of Random.positions(WorldPosition(0, 0, 500, 500), 100)) {
 			p.width = 20
