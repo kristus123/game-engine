225b106787f7c00663154ff7217a73cc7dfad618
Andreas <als0825a@gmail.com>
2026-02-09 12:38:28 +0100

    lint

--- game/code/World.js
+++ game/code/World.js
@@ -1,21 +1,17 @@
 export class World {
 	constructor() {
-		this.objects = Objects([
-			this.player = Player(WorldPosition(0, 0)),
-			Sprite.snow(D3, WorldPosition(0, 0), 4)
+		Dom.overlay([
+			Flex.v([
+				Html.p('HEIIIEIE'),
+				Html.p('lolololol'),
+			]),
+			Flex.h([
+				Html.p('HEIIIEIE'),
+				Html.p('lolololol'),
+			]),
 		])
-
-		for (const p of Random.positions(WorldPosition(0, 0, 500, 500), 100)) {
-			p.width = 20
-			p.height = 20
-			G.stones.add(new Stone(p))
-		}
-
-		Controller.control(this.player)
 	}
 
 	update() {
-		this.objects.update()
-		G.stones.update()
 	}
 }
