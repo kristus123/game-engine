9594fda198f2e613b07a82da19c0209a7ba6a1f9
Nabir14 <mushfiquefarhannabir@gmail.com>
2026-02-15 22:24:43 +0600

    using toDistPath

--- dev/build_tools/copy_manifest_to_dist.js
+++ dev/build_tools/copy_manifest_to_dist.js
@@ -6,19 +6,19 @@ import crypto from 'crypto'
 
 
 import { fileURLToPath } from 'url'
-import path, { dirname } from 'path'
+import { dirname } from 'path'
 import FileConfig from '#root/FileConfig.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const mainFilename = process.argv[1]
 
-fs.copyFileSync('manifest.json', path.join(FileConfig.dist, 'manifest.json'))
-fs.copyFileSync('sw.js', path.join(FileConfig.dist, 'sw.js'))
-fs.copyFileSync('sw-push.js', path.join(FileConfig.dist, 'sw-push.js'))
+fs.copyFileSync('manifest.json', FileConfig.toDistPath('manifest.json'))
+fs.copyFileSync('sw.js', FileConfig.toDistPath('sw.js'))
+fs.copyFileSync('sw-push.js', FileConfig.toDistPath('sw-push.js'))
 
 const sw = Files.read('sw.js')
 	.replace('RANDOM_UUID', '"' + crypto.randomUUID() + '"')
 	.replace('ALL_FILES', '[' + Files.at(Path.dirname(mainFilename)).map(f => '"/' + f + '"').join(',') + ']')
 
-Files.write(path.join(FileConfig.dist, 'sw.js'), sw)
+Files.write(FileConfig.toDistPath('sw.js'), sw)
