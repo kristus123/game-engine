9ae8c3364cb8110fcdfcc9b711d2e1e85b19d473
Muhammad Kashan Khan <kashankhanpak265@gmail.com>
2026-02-27 05:32:40 +0500

    feat: add normal map lighting system
    fix: fix indentation and linting in normal map lighting files
    
    fix: revert unrelated changes to AsepriteJson, World, and Parameters
    
    fix: move all normal map logic to NormalMap.js, update eslintrc

--- client/engine/graphics/sprite/SpriteLayers.js
+++ client/engine/graphics/sprite/SpriteLayers.js
@@ -1,53 +1,83 @@
 export class SpriteLayers extends Entity {
-	constructor(sprite, d, position, image, layersJson, scale) {
+	constructor(sprite, d, position, image, layersJson) {
 		super(position)
-
 		this.layers = new Set()
 		this._layers = {}
 		this.tags = {}
 		this.frames = {}
+		const normalFrames = {}
 
+		// First pass: collect all normal map frames
+		AsepriteLayerJson(layersJson).forEachFrame((
+			layer, frame, x, y, width, height,
+		) => {
+			if (layer.toLowerCase().includes("normal")) {
+				normalFrames[frame] = { x, y, width, height }
+			}
+		})
+
+		// Second pass: build layer pictures with normal data
 		AsepriteLayerJson(layersJson).forEachFrame((
 			layer, frame, x, y, width, height, tag,
 		) => {
+			//          console.log("Detected layer:", layer)
 			this.layers.add(layer)
 			this.tags[tag] ??= []
 			this._layers[layer] ??= []
-
 			this.width = width
 			this.height = height
-
-			const xxx = WorldPosition(x, y, width*Scale.value, height*Scale.value)
-			const yyy = {
-				position: xxx,
-				x: x,
-				y: y,
-				width: width,
-				height: height,
-				picture: LayerPicture(this.d, position, this.image, xxx),
+			const frameRect = { x, y, width, height }
+			if (layer.toLowerCase().includes("normal")) {
+				return
 			}
-
-			this._layers[layer].push(yyy)
-			this.tags[tag].push(yyy)
-			this.frames[frame] = yyy
+			const normalFrame = normalFrames[frame] ?? null
+			const picture = new LayerPicture(
+				d,
+				position,
+				image,
+				frameRect,
+				normalFrame
+					? { image, frame: normalFrame }
+					: null
+			)
+			const frameData = {
+				position: WorldPosition(
+					x,
+					y,
+					width * Scale.value,
+					height * Scale.value
+				),
+				x,
+				y,
+				width,
+				height,
+				picture,
+			}
+			this._layers[layer].push(frameData)
+			this.tags[tag].push(frameData)
+			this.frames[frame] = frameData
 		})
 
-		this.position.width = this.width * Scale.vaue
-		this.position.height = this.height * Scale.vaue
-
+		this.position.width = this.width * Scale.value
+		this.position.height = this.height * Scale.value
 		for (const layer of this.layers) {
-			Getter(this, layer, () => this._layers[layer][sprite.currentTagFrame].picture)
+			if (layer.toLowerCase().includes("normal")) {
+				continue
+			}
+			Getter(this, layer, () =>
+				this._layers[layer][sprite.currentTagFrame].picture
+			)
 		}
 	}
 
 	forEachLayer(run) {
 		for (const [layer, spriteFrames] of Object.entries(this._layers)) {
+			if (layer.toLowerCase().includes("normal")) {
+				continue
+			}
 			run(layer, spriteFrames)
 		}
 	}
 
-	update() {
-		// seems like it can be empty, but if it is removed
-		// it will use super's update
-	}
+	update() {}
 }
