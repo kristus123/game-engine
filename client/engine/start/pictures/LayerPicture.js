748db8fea52b344d6bcfa4caa63d760479e29339
Muhammad Kashan Khan <kashankhanpak265@gmail.com>
2026-02-27 05:32:40 +0500

    feat: add normal map lighting system
    fix: fix indentation and linting in normal map lighting files
    
    fix: revert unrelated changes to AsepriteJson, World, and Parameters
    
    fix: move all normal map logic to NormalMap.js, update eslintrc

--- client/engine/start/pictures/LayerPicture.js
+++ client/engine/start/pictures/LayerPicture.js
@@ -1,9 +1,10 @@
 export class LayerPicture extends Picture {
-	constructor(d, position, image, frame) {
+	constructor(d, position, image, frame, normalData = null) {
 		super(position, image)
-
 		super.clear()
-
+		this.canvas.width = frame.width
+		this.canvas.height = frame.height
+		this.ctx = this.canvas.getContext("2d")
 		this.ctx.drawImage(
 			image,
 			frame.x,
@@ -15,14 +16,20 @@ export class LayerPicture extends Picture {
 			frame.width,
 			frame.height
 		)
+		this.baseImageData = this.ctx.getImageData(0, 0, frame.width, frame.height)
+		this.normalMap = normalData
+			? new NormalMap(normalData.image, normalData.frame, frame.width, frame.height)
+			: null
+		if (this.normalMap) {
+			this.normalMap.apply(this.baseImageData, { x: 0.5, y: -0.5, z: 1 }, this.ctx)
+		}
 	}
 
-	draw(d = false) {
-		const dToUse = (d ? d : this.d)
-		const trueScale = Scale.value*7
-
-		const pos = WorldPosition(0, 0, this.canvas.width*trueScale, this.canvas.height*trueScale)
-
-		dToUse.picture(pos, this.canvas)
+	draw(d, lightDir = null) {
+		if (lightDir && this.normalMap) {
+			this.normalMap.apply(this.baseImageData, lightDir, this.ctx)
+		}
+		const dToUse = d || this.d
+		dToUse.picture(this.position, this.canvas)
 	}
 }
