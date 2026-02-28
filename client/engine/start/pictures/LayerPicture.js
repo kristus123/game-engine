dfd6d8422b603d695c11e8ea00c35b3f9381a843
Muhammad Kashan Khan <kashankhanpak265@gmail.com>
2026-02-28 23:20:05 +0500

    feat: add normal map lighting system

--- client/engine/start/pictures/LayerPicture.js
+++ client/engine/start/pictures/LayerPicture.js
@@ -5,29 +5,15 @@ export class LayerPicture extends Picture {
 		this.canvas.width = frame.width
 		this.canvas.height = frame.height
 		this.ctx = this.canvas.getContext("2d")
-		this.ctx.drawImage(
-			image,
-			frame.x,
-			frame.y,
-			frame.width,
-			frame.height,
-			0,
-			0,
-			frame.width,
-			frame.height
-		)
-		this.baseImageData = this.ctx.getImageData(0, 0, frame.width, frame.height)
-		this.normalMap = normalData
-			? new NormalMap(normalData.image, normalData.frame, frame.width, frame.height)
-			: null
-		if (this.normalMap) {
-			this.normalMap.apply(this.baseImageData, { x: 0.5, y: -0.5, z: 1 }, this.ctx)
+		this.ctx.drawImage(image, frame.x, frame.y, frame.width, frame.height, 0, 0, frame.width, frame.height)
+		if (normalData) {
+			this.setNormalMap(normalData, frame)
 		}
 	}
 
 	draw(d, lightDir = null) {
-		if (lightDir && this.normalMap) {
-			this.normalMap.apply(this.baseImageData, lightDir, this.ctx)
+		if (lightDir) {
+			this.applyLighting(lightDir)
 		}
 		const dToUse = d || this.d
 		dToUse.picture(this.position, this.canvas)
--- client/game/code/World.js
+++ client/game/code/World.js
@@ -2,24 +2,25 @@ export class World {
 	constructor() {
 		this.objects = Objects([
 			this.player = Player(WorldPosition(0, 0)),
-			Sprite.snow(D3, WorldPosition(0, 0), 4)
+			Sprite.snow(D3, WorldPosition(0, 0), 4),
+			Sprite.box(D3, WorldPosition(200, 200)),
 		])
-
 		for (const p of WorldPosition(0, 0, 500, 500).randomPoints(100, 20, 20)) {
 			G.stones.add(new Stone(p))
 		}
-
+		G.lightSource = WorldPosition(100, 100)
 		Controller.control(this.player)
 		Camera.follow(this.player)
-		setInterval(() => {
-		}, 200)
-		// this.player.pushTowards(Mouse.position, 100)
 	}
 
 	update() {
 		this.objects.update()
 		G.stones.update()
-		if (Mouse.down) {
-		}
+		//Normal map test: uncomment to see dynamic lighting effect
+
+
+		//const t = Date.now() / 1000
+		//G.lightSource.x = 200 + Math.cos(t) * 150
+		//G.lightSource.y = 200 + Math.sin(t) * 150
 	}
 }
