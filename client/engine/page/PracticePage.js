5ccbad6dc894760f1163618a6f0576ab123573c3
Your Name <you@example.com>
2026-04-05 11:49:27 +0200

    x

--- client/engine/page/PracticePage.js
+++ client/engine/page/PracticePage.js
@@ -6,59 +6,64 @@ p.addCards.onClick(() => {
 	Page.go(AddCardPage)
 })
 
-db.all(cards => {
-	let c = null
+let cards = []
+let c = null
 
-	function practiceNewCard() {
-		if (cards.empty) {
-			p.mid.clear()
-			p.bot.clear()
-			p.mid.add(H.p('all cards pracitced'))
-			p.mid.add(H.button('practice one more time'))
-		}
-		else {
-			c = cards.random()
-			Sound.playBlob(c.front)
-
-			p.message.text(cards.length + " cards left")
-		}
+function practiceNewCard() {
+	if (cards.empty) {
+		p.message.text('all cards pracitced')
+		p.mid.add(H.button('practice one more time'), (b) => {
+			init()
+			b.remove()
+			practiceNewCard()
+		})
 	}
+	else {
+		c = cards.random()
+		Sound.playBlob(c.front)
+		p.message.text(cards.length + " cards left")
+	}
+}
+
+function init() {
+	db.all(dbCards => {
+		cards = dbCards
+		practiceNewCard()
+	})
+}
+
+function applyScore(score) {
+	c.score += score
+
+	db.update(c)
+
+	cards.remove(c)
 
 	practiceNewCard()
+}
 
-	function applyScore(score) {
-		c.score += score
-
-		db.update(c)
-
-		cards.remove(c)
-
-		practiceNewCard()
-	}
-
-	p.playFront.onClick(() => {
-		console.log(c)
-		Sound.playBlob(c.front)
-	})
-
-	p.playBack.onClick(() => {
-		Sound.playBlob(c.back)
-	})
-
-	p.easy.onClick(() => {
-		applyScore(+1)
-	})
-
-	p.hard.onClick(() => {
-		applyScore(-1)
-	})
-
-	p.delete.onClick(() => {
-		db.delete(c)
-		practiceNewCard
-	})
-
-	
+p.playFront.onClick(() => {
+	Sound.playBlob(c.front)
 })
+
+p.playBack.onClick(() => {
+	Sound.playBlob(c.back)
+})
+
+p.easy.onClick(() => {
+	applyScore(+1)
+})
+
+p.hard.onClick(() => {
+	applyScore(-1)
+})
+
+p.delete.onClick(() => {
+	db.delete(c)
+	practiceNewCard
+})
+
+
+init()
 
 export const PracticePage = Page.init(p)
