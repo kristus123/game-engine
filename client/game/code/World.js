ef24a86cc5edd98f461f84afe26f26d88bb45a4e
kristian <krispetter@gmail.com>
2026-04-07 14:12:23 +0200

    x

--- client/game/code/World.js
+++ client/game/code/World.js
@@ -1,6 +1,6 @@
 export class World {
 	constructor() {
-		Page.go(PracticePage)
+		// Page.go(PracticePage)
 
 		// let x = null
 		// Dom.overlay([
@@ -34,8 +34,12 @@ export class World {
 		// 	])
 		// })
 
+		this.p = Sprite.player(D1, WorldPosition(0,0))
+		this.p2 = Sprite.player(D1, WorldPosition(200,0))
 	}
 
 	update() {
+		this.p.update()
+		this.p2.update()
 	}
 }
