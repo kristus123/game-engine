31ab0581f6670cfd8b5a7a436a7538ccfddfff8c
Nabir14 <mushfiquefarhannabir@gmail.com>
2026-04-11 14:04:32 +0600

    add proxy object logic back to LobbyApi

---
+++ client/engine/lobby/LobbyManager.js
@@ -0,0 +1,56 @@
+// ClientId(
+
+export class LobbyManager {
+
+	static lobbies = {}
+
+	static createLobby(lobbyId, hostClientId, onUpdate) {
+
+		this.lobbies.assertKeyNotPresent(lobbyId)
+
+		this.lobbies[lobbyId] = {
+			lobbyId: lobbyId,
+			hostClientId: hostClientId,
+			clients: {}
+		}
+
+		if (hostClientId === ClientId) {
+			this.lobbies[lobbyId].clients[hostClientId] = ProxyObject((prop, value) => onUpdate(lobbyId, prop, value))
+		} else {
+			this.lobbies[lobbyId].clients[hostClientId] = {}
+		}
+
+		return this.lobbies[lobbyId]
+	}
+
+	static joinLobby(lobbyId, clientId, onUpdate) {
+		this.lobbies.assertKeyPresent(lobbyId)
+
+		this.lobbies[lobbyId].clients.assertKeyNotPresent(clientId)
+
+		if (clientId === ClientId) {
+    		this.lobbies[lobbyId].clients[clientId] = ProxyObject((prop, value) => onUpdate(lobbyId, prop, value))
+		} else {
+			this.lobbies[lobbyId].clients[clientId] = {}
+		}
+
+		return this.lobbies[lobbyId]
+	}
+
+
+	static leaveLobby(lobbyId, clientId) {
+		this.lobbies.assertKeyPresent(lobbyId)
+
+		this.lobbies[lobbyId].connectedClientIds.assertKeyPresent(clientId)
+
+		delete this.lobbies[lobbyId].clients[clientId]
+	}
+
+	static* myLobbies() {
+		for (const [lobbyId, lobby] of Object.entries(this.lobbies)) {
+			if (lobby.hostClientId === ClientId) {
+				yield lobby
+			}
+		}
+	}
+}
--- client/game/code/World.js
+++ client/game/code/World.js
@@ -1,45 +1,43 @@
 export class World {
 	constructor() {
-		Page.go(PracticePage)
+		//Page.go(PracticePage)
 
-		// let x = null
-		// Dom.overlay([
-		// 	Html.p("Actions:"),
-		// 	Html.button("Create New Lobby", () => {
-		// 		x = Lobby.create()
-		// 		x.clients[ClientId].x = 0
+		let x = null
+		Dom.add([
+			Html.p("Actions:"),
+		 	Html.button("Create New Lobby", () => {
+		 		x = Lobby.create()
+				x.clients[ClientId].x = 0
 
-		// 		console.log(x)
-		// 	}),
-		// 	Html.button("Update Object Data", () => {
-		// 		x.clients[ClientId].x += 1
-		// 	}),
-		// 	Html.button("Check Lobby Object", () => {
-		// 		console.log(x)
-		// 	}),
-		// 	Html.p("Other Lobbies:"),
-		// 	this.x = H.div(),
-		// ]).addClass("white")
+				console.log(x)
+			}),
+			Html.button("Update Object Data", () => {
+				x.clients[ClientId].x += 1
+			}),
+			Html.button("Check Lobby Object", () => {
+				console.log(x)
+			}),
+			Html.p("Other Lobbies:"),
+			this.x = H.div(),
+		])
 
-		// Lobby.onNewLobby(lobby => {
-		// 	this.x.add([
-		// 		Html.button(lobby.lobbyId, () => {
-		// 			x = Lobby.join(lobby.lobbyId)
+		Lobby.onNewLobby(lobby => {
+			this.x.add([
+				Html.button(lobby.lobbyId, () => {
+					x = Lobby.join(lobby.lobbyId)
+					x.clients[ClientId].x = 0
 
-		// 			x.clients[ClientId].x ??= 0
-		// 			x.clients[ClientId].x += 1
+					console.log(x)
+				})
+			])
+		})
 
-		// 			console.log(x)
-		// 		})
-		// 	])
-		// })
-
-		this.p = Sprite.player(D1, WorldPosition(0, 0))
-		this.p2 = Sprite.player(D1, WorldPosition(200, 0))
+		//this.p = Sprite.player(D1, WorldPosition(0, 0))
+		//this.p2 = Sprite.player(D1, WorldPosition(200, 0))
 	}
 
 	update() {
-		this.p.update()
-		this.p2.update()
+		//this.p.update()
+		//this.p2.update()
 	}
 }
