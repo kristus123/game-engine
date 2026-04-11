a32d007e1742255d5d4f166bb0b1d309a50842e1
kristian <krispetter@gmail.com>
2026-04-11 11:24:02 +0200

    x

--- client/game/code/World.js
+++ client/game/code/World.js
@@ -1,43 +1,45 @@
 export class World {
 	constructor() {
-		//Page.go(PracticePage)
+		Page.go(PracticePage)
 
-		let x = null
-		Dom.add([
-			Html.p("Actions:"),
-		 	Html.button("Create New Lobby", () => {
-		 		x = Lobby.create()
-				x.clients[ClientId].x = 0
+		// let x = null
+		// Dom.overlay([
+		// 	Html.p("Actions:"),
+		// 	Html.button("Create New Lobby", () => {
+		// 		x = Lobby.create()
+		// 		x.clients[ClientId].x = 0
 
-				console.log(x)
-			}),
-			Html.button("Update Object Data", () => {
-				x.clients[ClientId].x += 1
-			}),
-			Html.button("Check Lobby Object", () => {
-				console.log(x)
-			}),
-			Html.p("Other Lobbies:"),
-			this.x = H.div(),
-		])
+		// 		console.log(x)
+		// 	}),
+		// 	Html.button("Update Object Data", () => {
+		// 		x.clients[ClientId].x += 1
+		// 	}),
+		// 	Html.button("Check Lobby Object", () => {
+		// 		console.log(x)
+		// 	}),
+		// 	Html.p("Other Lobbies:"),
+		// 	this.x = H.div(),
+		// ]).addClass("white")
 
-		Lobby.onNewLobby(lobby => {
-			this.x.add([
-				Html.button(lobby.lobbyId, () => {
-					x = Lobby.join(lobby.lobbyId)
-					x.clients[ClientId].x = 0
+		// Lobby.onNewLobby(lobby => {
+		// 	this.x.add([
+		// 		Html.button(lobby.lobbyId, () => {
+		// 			x = Lobby.join(lobby.lobbyId)
 
-					console.log(x)
-				})
-			])
-		})
+		// 			x.clients[ClientId].x ??= 0
+		// 			x.clients[ClientId].x += 1
 
-		//this.p = Sprite.player(D1, WorldPosition(0, 0))
-		//this.p2 = Sprite.player(D1, WorldPosition(200, 0))
+		// 			console.log(x)
+		// 		})
+		// 	])
+		// })
+
+		this.p = Sprite.player(D1, WorldPosition(0, 0))
+		this.p2 = Sprite.player(D1, WorldPosition(200, 0))
 	}
 
 	update() {
-		//this.p.update()
-		//this.p2.update()
+		this.p.update()
+		this.p2.update()
 	}
 }
