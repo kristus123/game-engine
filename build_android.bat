2ad8f1e5d48efa7e4bc9d4e1f09aeda8c8b275de
kristian <krispetter@gmail.com>
2026-04-13 17:43:18 +0200

    x

---
+++ build_android.bat
@@ -0,0 +1,20 @@
+mkdir capacitor_setup
+robocopy dist capacitor_setup\dist /E
+
+cd capacitor_setup
+
+npm i @capacitor/core
+npm i -D @capacitor/cli
+
+npx cap init MyApp com.example.myapp --web-dir=dist
+
+npm i @capacitor/android 
+npx cap add android
+
+REM npm i @capacitor/ios 
+REM npx cap add ios
+
+npx cap sync
+
+npx cap open android
+npx cap run android
