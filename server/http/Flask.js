a42704d24f01eb7b4fae9fdceedf8b836cd25e88
kristian <krispetter@gmail.com>
2026-04-13 17:19:23 +0200

    x

--- server/http/Flask.js
+++ server/http/Flask.js
@@ -12,7 +12,7 @@ export class Flask {
 		this.routes.push({ path, callback })
 	}
 
-	static listen(port) {
+	static listen(port, bind="0.0.0.0") {
 		const app = express()
 
 		Flask.server = http.createServer(app)
@@ -41,6 +41,6 @@ export class Flask {
 			res.status(400).json({ error: "Route not found" })
 		})
 
-		Flask.server.listen(port)
+		Flask.server.listen(port, bind)
 	}
 }
