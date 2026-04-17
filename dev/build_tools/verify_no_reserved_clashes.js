f06e68d020827207951a6dd6f573f1da3554306f
kristian <krispetter@gmail.com>
2026-04-17 11:39:04 +0200

    x

--- dev/build_tools/verify_no_reserved_clashes.js
+++ dev/build_tools/verify_no_reserved_clashes.js
@@ -1,14 +1,14 @@
 import jsFiles from "#root/dev/build_tools/js_files.js"
 
-// better safe and strict than sorry and loosey goosey
 import reservedJsKeywords from "#root/dev/build_tools/reservedJsKeywords.js"
+
+
+// better safe and strict than sorry and loosey goosey
 const keywords = reservedJsKeywords
 	.map(w => w.toLowerCase())
 
 for (const file of jsFiles) {
-	let name = file.split("/").pop().replace(".js", "")
-
-	name = name.toLowerCase() // strict !
+	const name = file.split("/").pop().replace(".js", "").toLowerCase() // ! Strict
 
 	if (keywords.includes(name)) {
 		throw new Error(`${file} can not be named as such, because it clashes with a reserved js keywords`)
