60d227ae8f99537e3a673827edff51ac6ad9cc58
kristian <krispetter@gmail.com>
2026-04-19 11:23:31 +0200

    x

--- client/engine/controller/GamePad.js
+++ client/engine/controller/GamePad.js
@@ -1,71 +1,34 @@
+function fix(num) {
+	return Math.round(num * 100) / 100
+}
+
 export class GamePad {
-  static index = 0;
-  static gamepad = null;
-  static buttons = [];
-  static axes = [];
-  static connected = false;
 
-  static {
-    window.addEventListener("gamepadconnected", (e) => {
-		console.log(e.gamepad.index)
-      console.log("Gamepad connected:", e.gamepad);
-      if (e.gamepad.index === this.index) {
-        this.connected = true;
-      }
-    });
+	static {
+		window.addEventListener("gamepadconnected", (e) => {
+			console.log("Gamepad connected:", e.gamepad)
+		})
 
-    window.addEventListener("gamepaddisconnected", (e) => {
-      console.log("Gamepad disconnected:", e.gamepad);
-      if (e.gamepad.index === this.index) {
-        this.connected = false;
-        this.gamepad = null;
-        this.buttons = [];
-        this.axes = [];
-      }
-    });
-  }
+		window.addEventListener("gamepaddisconnected", (e) => {
+			console.log("Gamepad disconnected:", e.gamepad)
+		})
+	}
 
-  static setIndex(i) {
-    this.index = i;
-  }
+	static update() {
+		for (const gp of navigator.getGamepads()) {
+			if (gp && gp.index == 1) {
 
-  static update() {
-    const pads = navigator.getGamepads();
-    const gp = pads[this.index];
+				const x = fix(gp.axes[0])
+				const y = fix(gp.axes[1])
 
-    if (!gp) {
-      this.gamepad = null;
-      return false;
-    }
+				const deadzone = 0.4
 
-    this.gamepad = gp;
-    this.connected = true;
+				this.right = x > deadzone
+				this.left = x < -deadzone
 
-    this.buttons = gp.buttons.map(b => b.pressed);
-    this.axes = gp.axes.slice();
-
-    return true;
-  }
-
-  static isPressed(buttonIndex) {
-    return !!this.buttons[buttonIndex];
-  }
-
-  static getAxis(axisIndex) {
-    return this.axes[axisIndex] || 0;
-  }
-
-  static leftStick() {
-    return {
-      x: this.getAxis(0),
-      y: this.getAxis(1)
-    };
-  }
-
-  static rightStick() {
-    return {
-      x: this.getAxis(2),
-      y: this.getAxis(3)
-    };
-  }
+				this.down = y > deadzone
+				this.up = y < -deadzone
+			}
+		}
+	}
 }
