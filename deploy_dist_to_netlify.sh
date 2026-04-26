9273526153b37a7373a6959a1d29ca2a353ee8d2
kristian <krispetter@gmail.com>
2026-04-26 13:40:07 +0200

    x

--- deploy_dist_to_netlify.sh
+++ deploy_dist_to_netlify.sh
@@ -9,6 +9,8 @@ find dist -type f -name '*.js' -exec npx --yes terser {} --compress --mangle -o 
 find dist -type f -name '*.css' -exec npx --yes lightningcss --minify {} -o {} \;
 find dist -type f -name '*.html' -exec npx --yes html-minifier-terser --collapse-whitespace --remove-comments -o {} {} \;
 
+
+# needed in order to use shared array buffers between main and worker
 cat > dist/netlify.toml <<EOF
 [[headers]]
 	for = "/*"
@@ -17,7 +19,7 @@ cat > dist/netlify.toml <<EOF
 		Cross-Origin-Embedder-Policy = "require-corp"
 EOF
 
-# the id is 'romskip' aka romskip.netlify.app
+# the id of 'romskip' aka. romskip.netlify.app
 netlify deploy --prod --dir=dist --site=30335998-1c5b-49e4-b97c-cb0f1f5b3893
 
 rm -r .netlify
