700da8994b0a2b7414f6f5f68d19970858872c19
Nabir14 <82253045+Nabir14@users.noreply.github.com>
2026-04-26 18:47:43 +0600

    minor tweaks to bundle.js

--- bundle.js
+++ bundle.js
@@ -1,5 +1,5 @@
 // If this file is updated do:
-// Run "npm install esbuild"
+// Run "npm install esbuild" If esbuild is not installed already
 // Use "npx esbuild bundle.js --bundle --outfile=out.js"
 // Add this line at the bottom of out.js: export const out = ""
 // Then Move out.js to client/engine/extras
