bbdc29d64a78e99858a94ec45988a0747caf982c
kristian <krispetter@gmail.com>
2026-04-28 12:11:58 +0200

    x

--- client/engine/audio/Sound.js
+++ client/engine/audio/Sound.js
@@ -1,37 +1,15 @@
 export class Sound {
 
 	static init() {
-		this._click = AudioEngine(G.Sound.click)
-		this._placeDirt = AudioEngine(G.Sound.placeDirt)
-	}
-
-	static click() {
-		// this._click.play()
-	}
-
-	static placeDirt() {
-		this._placeDirt.play()
-	}
-
-	static get nya() {
-		return AudioSheet(G.Sound.nyaSheet)
-	}
-
-	static get sheet() {
-		return AudioSheet(G.Sound.sheet)
+		this._click = AudioEngine(SoundBlob.click)
+		this._placeDirt = AudioEngine(SoundBlob.placeDirt)
 	}
 
 	static playBlob(blob) {
-		if (!blob) {
-			throw new Error("no blob present")
-		}
-		else {
-			const url = URL.createObjectURL(blob)
-			const audio = new Audio(url)
+		const url = URL.createObjectURL(blob)
+		const audio = new Audio(url)
 
-			audio.play()
-		}
+		audio.play()
 	}
 
 }
-
--- client/engine/start/LoadAllAudio.js
+++ client/engine/start/LoadAllAudio.js
@@ -2,7 +2,7 @@ export async function LoadAllAudio(AUDIO_FILES) {
 	return Promise.all(AUDIO_FILES.map(async path => {
 		const audio = await LoadAudio(path)
 		const key = path.split("/").pop().replace(".mp3", "")
-		G.Sound[key] = audio
+		SoundBlob[key] = audio
 	}))
 }
 
