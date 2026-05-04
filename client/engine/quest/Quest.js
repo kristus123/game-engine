245fd33614e9e32b2bf715e52c343f1a1702216a
kristian <krispetter@gmail.com>
2026-05-04 12:15:56 +0200

    x

--- client/engine/quest/Quest.js
+++ client/engine/quest/Quest.js
@@ -1,36 +1,24 @@
-/*
-a task is anything that has a lambda that returns something that has a completed() method
-it will append a .markTaskComplete() method to be able to mark tasks as completed
- */
-
 export class Quest {
 	constructor(tasks=[], onQuestCompleted=() => {}) {
 
 		Assert.method(onQuestCompleted)
 		Assert.array(tasks, t => t.assertOfType(Task))
 
-		let index = 0
-
-		this.objects = Objects([
-			OnTrue(() => tasks[index].completed, () => {
-				index += 1
-				if (tasks.validIndex(index)) {
-					tasks[index].start()
-				}
-				else {
-					onQuestCompleted()
-					this.objects.clear()
-				}
-			}),
-			{
-				update: () => {
-					tasks[index].update()
-				},
-			},
-		])
+		this.index = 0
 	}
 
 	update() {
-		this.objects.update()
+		tasks[this.index].update()
+
+		if (tasks[this.index].completed) {
+			this.index += 1
+			if (tasks.validIndex(this.index)) {
+				tasks[this.index].start()
+			}
+			else {
+				onQuestCompleted()
+				this.removeItself()
+			}
+		}
 	}
 }
