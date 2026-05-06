8dfdc00459bd38d711534cae06bb0d1252ad2a74
kristian <krispetter@gmail.com>
2026-05-06 14:46:17 +0200

    x

--- build_android.bat
+++ build_android.bat
@@ -1,24 +1,27 @@
+@echo off
+
 mkdir capacitor_setup
 
 robocopy dist capacitor_setup\dist /E
+if %errorlevel% GEQ 8 exit /b %errorlevel%
 
-cd capacitor_setup
+cd /d capacitor_setup
 
 npm init -y
 
 npm i @capacitor/core
 npm i -D @capacitor/cli
 
-npx -y cap init MyApp com.example.myapp --web-dir=dist
+npx cap init MyApp com.example.myapp --web-dir=dist
 
-npm i @capacitor/android 
-npx -y cap add android
+npm i @capacitor/android
+npx cap add android
 
 npm install @capacitor-community/microphone
 
-REM npm i @capacitor/ios 
+REM npm i @capacitor/ios
 REM npx cap add ios
 
-npx -y cap sync
+npx cap sync
 
-npx -y cap open android
+npx cap open android
