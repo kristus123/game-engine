dc0e40d3ef1726eda40404ef3b299bf8034eadda
kristian <krispetter@gmail.com>
2026-05-09 12:14:15 +0200

    refactor-FileConfig

--- FileConfig.js
+++ FileConfig.js
@@ -1,44 +1,27 @@
-import path from "path"
-
 export const FileConfig = {
-	game: "client/game",
-	engine: "client/engine",
-	buildTools: "dev/build_tools",
 
-	client: "client",
-	dist: "dist",
+	clientFolder: "client/",
 
 	asepriteToJson: "dev/build_tools/aseprite_to_json.lua",
+
 	exportAseprite: "dev/build_tools/ExportAseprite.js",
+
 	generateDist: "dev/build_tools/GenerateDist.js",
+
 	engineIndex: "dist/client/engine/start/index.js",
 
-	get gameAssets() {
-		return path.join(this.game, "assets")
-	},
+	gameAssets: "client/game/assets",
 
-	get gameAudio() {
-		return path.join(this.game, "audio")
-	},
+	gameAudio: "client/game/audio",
 
-	get asepriteAssets() {
-		return path.join(this.game, "assets/aseprite")
-	},
+	asepriteAssets: "client/game/assets/aseprite",
 
-	get gameUiCss() {
-		return path.join(this.game, "ui/css")
-	},
+	gameUiCss: "client/game/ui/css",
 
-	get gameIndexHtml() {
-		return path.join(this.game, "index.html")
-	},
-
-	removeClientPathPrefix(filePath) {
-		const regex = new RegExp(`^${FileConfig.client}[\\/\\\\]`)
-		return filePath.replace(regex, "")
-	},
+	gameIndexHtml: "client/game/index.html",
 
 	toDistPath(filePath) {
-		return path.join(FileConfig.dist, filePath)
+		return "dist/" + filePath
 	},
+
 }
