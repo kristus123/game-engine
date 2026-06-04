b24ea9766fcf60cb1ecbe89aef70f2682a912b4f
MD Mushfique Farhan Nabir <82253045+Nabir14@users.noreply.github.com>
2026-06-04 19:16:16 +0000

    PR (#261)
    * Tree Collision
    
    * lint
    
    * added sliding
    
    * fix getting stuck bug and fix jitter
    
    * fix minor velocity bug

--- client/game/code/Tree.js
+++ client/game/code/Tree.js
@@ -5,12 +5,56 @@ export class Tree extends Entity {
 		this.objects = Objects([
 			this.sprite = Sprite.tree(this.position),
 		])
+		console.log(this.sprite.collider)
 	}
 
 	update() {
 		this.objects.update()
 
-		// Collision.js is a reference
+		
+		if (this.sprite.collider.touches(G.player.sprite.collider)) {
+			const treeCollider = this.sprite.collider
+			const playerCollider = G.player.sprite.collider
+			
+			const overlapX = Math.min(treeCollider.x + treeCollider.width - playerCollider.x,
+									  playerCollider.x + playerCollider.width - treeCollider.x)
+
+			const overlapY = Math.min(treeCollider.y + treeCollider.height - playerCollider.y,
+									  playerCollider.y + playerCollider.height - treeCollider.y)
+		
+			const centerTreeX =
+				treeCollider.x + treeCollider.width * 0.5
+			const centerTreeY =
+				treeCollider.y + treeCollider.height * 0.5
+
+			const centerPlayerX =
+				playerCollider.x + playerCollider.width * 0.5
+			const centerPlayerY =
+				playerCollider.y + playerCollider.height * 0.5
+
+			let pushX
+			let pushY
+
+			if (centerPlayerX < centerTreeX) {
+				pushX = -overlapX
+			} else {
+				pushX = overlapX
+			}
+
+			if (centerPlayerY < centerTreeY) {
+				pushY = -overlapY
+			} else {
+				pushY = overlapY
+			}
+
+			if (overlapX < overlapY) {
+				G.player.position.x += pushX
+			}
+			
+			if(overlapX > overlapY) {
+				G.player.position.y += pushY
+			}
+		}
 
 		D1.box(this.sprite.collider)
 		D1.box(G.player.sprite.collider)
