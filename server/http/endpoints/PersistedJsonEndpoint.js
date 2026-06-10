2b641b4eb891c5c726ce6c4b788f439a30dd3f38
kristian <krispetter@gmail.com>
2026-06-10 22:03:36 +0200

    x

--- server/http/endpoints/PersistedJsonEndpoint.js
+++ server/http/endpoints/PersistedJsonEndpoint.js
@@ -2,11 +2,10 @@ import { Files } from "#root/dev/build_tools/Files.js"
 import { Methods } from "#root/server/http/Methods.js"
 
 Methods.add("allPersistedJsonFiles", ({ body, req }) => {
-	const x = Files.inFolder("persistedjson").map(path => ({
+	return Files.inFolder("persistedjson").map(path => ({
 		name: path.split("/").pop().split(".")[0],
 		content: JSON.parse(Files.read(path)),
 	}))
-	return x
 })
 
 Methods.add("getPersistedJson", ({ body, req }) => {
@@ -15,16 +14,11 @@ Methods.add("getPersistedJson", ({ body, req }) => {
 
 Methods.add("savePersistedJson", ({ body }) => {
 	try {
-		console.log("Writing now")
-		console.log(body)
-		console.log(typeof body)
 		Files.write(`persistedjson/${body.name}.json`, JSON.stringify(body.content, null, 4))
-		console.log("Writing now")
 	}
 	catch (e) {
-		console.log("____X___________")
-		console.log(e)
-		console.log("____X___________")
-
+		throw new Error(e)
 	}
+
+	return {}
 })
