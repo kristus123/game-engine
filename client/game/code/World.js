f0a665cd8bef7598385c175cc46faa1be33781ce
kristian <krispetter@gmail.com>
2026-06-14 09:55:56 +0200

    x

--- client/game/code/World.js
+++ client/game/code/World.js
@@ -1,43 +1,119 @@
 export class World {
 
 	constructor() {
-		const t = Html.test()
+		ClientToken.create(() => {
+			console.log(ClientToken.get().internal)
+		})
 
-		Dom.add(t)
+		// const test = Dom.add(Html.test())
 
-		let placeholder = null
+		// let placeholder = null
 
-		DomMouse.onDrag = (e) => {
-			placeholder = e.cloneNode(true).css("font-size: 50px")
-			Dom.floating(placeholder)
+		// Dom.onDrag = e => {
+		// 	test.list.forEach(c => {
+		// 		c.addClass("blue")
+		// 	})
+		// }
 
-			e.invisible()
-		}
+		// Dom.onDrop = e => {
+		// 	test.list.forEach(c => {
+		// 		c.removeClass("blue")
+		// 	})
+		// }
 
-		DomMouse.onDrop = (e) => {
-			Dom.remove(placeholder)
-			placeholder = null
+		// Dom.whileDragging = e => {
 
-			e.visible()
-		}
+		// 	const nextItem = test.list.children.find(i => {
+		// 		const r = i.getBoundingClientRect()
+		// 		return Mouse.screen.y < r.top + r.height / 2
+		// 	})
 
-		DomMouse.onMouseMove = (e) => {
-		}
+		// 	if (nextItem) {
+		// 		test.list.insertBefore(e, nextItem)
+		// 	}
+		// 	else {
+		// 		test.list.appendChild(e)
+		// 	}
+		// }
 
-		DomMouse.whileDragging = (e) => {
-			placeholder?.followMouse()
+		// test.list.onDragChild({
+		// 	onDrag: (e) => {
+		// 		console.log(e)
+		// 	},
+		// 	onDrop: (e) => {
+		// 		console.log(e)
+		// 	},
+		// 	whileDragging: () => {
 
-			for (const h of DomMouse.hovering) { // find better solution for this
-				const list = h.closest("[user-sortable]")
-				if (list) {
-					list.orderBasedOnMousePosition(e)
-					break
-				}
-			}
-		}
+		// 	},
+		// })
+
+		this.player = Player(WorldPosition(2000, 2000)),
+		G.player = this.player
+
+		Internet.onOnline(() => {
+			console.log("Server connection restored")
+		})
+
+		Internet.onOffline(() => {
+			console.log("Server connection lost")
+		})
+
+		this.objects = Objects([
+			this.fireplace = Sprite.fireplace(WorldPosition(1512, 2100)),
+			this.world = Sprite.world(WorldPosition(0, 0)),
+			{
+				update: () => {
+					// D1.lightSource(WorldPosition(1600, 2238))
+				},
+			},
+			G.player,
+			...Iterate(200, () => {
+				return Tree(Random.direction(WorldPosition(2000, 2000), 4000))
+			}),
+			this.bush = Sprite.bush(WorldPosition(2000, 1500)).loopTag("berries"),
+			this.oldSami = OldSami(),
+			RandomIntegerEveryMs(0, 100, 5000, (v) => {
+				console.log(v)
+			}),
+			IntroQuest(this.player, this.oldSami, this.bush),
+			// Light.add(WorldPosition(1600, 2238), 200, "255,165,0", 0.5),
+		])
+
+		Camera.follow(this.player)
+
+		this.cameraViewTest = Dom.add(H.p("FIREPLACE IN VIEW: ..."))
+		this.cameraViewTest.style.position = "fixed"
+		this.cameraViewTest.style.left = "20px"
+		this.cameraViewTest.style.top = "20px"
+		this.cameraViewTest.style.zIndex = "1000"
+		this.cameraViewTest.style.padding = "8px"
+		this.cameraViewTest.style.backgroundColor = "black"
+		this.cameraViewTest.style.pointerEvents = "none"
+
+		Controller.control(this.player)
+
+		this.fireplace.shake(20, 10)
+
+		console.log(this.fireplace)
 	}
 
 	update() {
+		this.objects.update()
+
+		const fireplaceInsideView = Camera.insideView(this.fireplace)
+		H.changeText(
+			this.cameraViewTest,
+			`FIREPLACE IN VIEW: ${fireplaceInsideView ? "YES" : "NO"}`
+		)
+		this.cameraViewTest.style.color = fireplaceInsideView ? "lime" : "red"
+
+		G.player.enforceCollision(this.fireplace)
+
+		D1.box(this.fireplace.position)
+
+		D2.circle(WorldPosition(2000, 2000), 50)
+
 	}
 
 }
