12adb500f33f9c0363688fa1dc278380aff667af
Nabir14 <mushfiquefarhannabir@gmail.com>
2026-06-15 17:05:22 +0600

    complete test

--- client/game/code/World.js
+++ client/game/code/World.js
@@ -1,26 +1,8 @@
 export class World {
 	constructor() {
-		// TEST: Sprite Copy
-
-		// this.objects = Objects([
-		// 	this.x = Sprite.bush(WorldPosition(0, 0)),
-		// 	this.y = this.x.copy()
-		// ])
-
-		// this.y.position = WorldPosition(
-		// 	this.y.position.x + 500,
-		// 	this.y.position.y,
-		// 	this.y.position.width,
-		// 	this.y.position.height
-		// )
-
-		// TEST: Slice And Shuffle
-
 		this.normalObjects = Objects([
 			this.normalBush = Sprite.bush(WorldPosition(0, -256))
 		])
-
-		// TODO: Fix Scaling Bug
 
 		this.slicedObjects = Objects([
 			SpriteSlicer.slice(Sprite.bush(WorldPosition(0, 0)), 2, 2)
