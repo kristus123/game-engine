8f808474b69842979c003348f9f6807460faf33f
Nabir14 <mushfiquefarhannabir@gmail.com>
2026-06-15 16:58:01 +0600

    Fixed blur and warping bug. SpriteSlicer Is Complete

--- client/engine/graphics/sprite/SpriteSlicer.js
+++ client/engine/graphics/sprite/SpriteSlicer.js
@@ -1,30 +1,43 @@
 export class SpriteSlicer {
-	static slice(sprite, sliceX, sliceY) {
+	static slice(sprite, sliceCountX, sliceCountY) {
 		const sprites = []
 
 		// Same value for both x and y axis because... uniform slices so they look good
-		let spriteSize = 0
+		let spriteWidth = 0
+		let spriteHeight = 0
 
 		// Little hacky? I think it is good because we want to slice uniformly
 		// Also we won't need a better implementation until we want to slice animated sprites
 		for (const picture of sprite.getAllPicture()) {
-			spriteSize = picture.canvas.width
+			spriteWidth = picture.canvas.width
+			spriteHeight = picture.canvas.height
+			
+			break; // We only need the first picture
 		}
 
-		for (let x = 0; x < sliceX; x++) {
-    		for (let y = 0; y < sliceY; y++) {
+		for (let x = 0; x < sliceCountX; x++) {
+    		for (let y = 0; y < sliceCountY; y++) {
         		const spriteSlice = sprite.copy()
 
-        		const sx = (spriteSize / sliceX) * x
-        		const sy = (spriteSize / sliceY) * y
-        		const sw = spriteSize / sliceX
-        		const sh = spriteSize / sliceY
+        		const sx = Math.round((spriteWidth / sliceCountX)) * x
+        		const sy = Math.round((spriteHeight / sliceCountY)) * y
+        		const sw = Math.round(spriteWidth / sliceCountX)
+        		const sh = Math.round(spriteHeight / sliceCountY)
 
         		for (const picture of spriteSlice.getAllPicture()) {
             		picture.crop(sx, sy, sw, sh)
+					
+					const spriteSliceWidth = picture.canvas.width * Scale.value
+					const spriteSliceHeight = picture.canvas.height * Scale.value
+					
+					spriteSlice.position = WorldPosition(
+						spriteSliceWidth * x ,
+						spriteSliceHeight * y ,
+						spriteSliceWidth,
+						spriteSliceHeight
+					)
         		}
 
-        		spriteSlice.position = WorldPosition(spriteSlice.position.width * x, spriteSlice.position.height * y, spriteSlice.position.width, spriteSlice.position.height)
 
         		sprites.push(spriteSlice)
     		}
@@ -34,20 +47,13 @@ export class SpriteSlicer {
 	}
 
 	static shuffle(slicedSprites) {
-		console.log("SLICED_SPRITES BEFORE SHUFFLE: ", slicedSprites)
-
 		let nextSliceIndex = 0
 
 		for (let i = 0; i < slicedSprites.length; i++) {
     		const destSliceIndex = Math.round(Math.random() * (slicedSprites.length - 1))
-
-    		console.log("FROM: ", nextSliceIndex)
-    		console.log("TO: ", destSliceIndex)
-
-    		slicedSprites[nextSliceIndex].position = slicedSprites[destSliceIndex].position
-    		nextSliceIndex = destSliceIndex
+    		
+			slicedSprites[nextSliceIndex].position = slicedSprites[destSliceIndex].position
+			nextSliceIndex = destSliceIndex
 		}
-
-		console.log("SLICED_SPRITES AFTER SHUFFLE: ", slicedSprites)
 	}
 }
