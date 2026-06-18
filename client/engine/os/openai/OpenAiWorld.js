12b65526d7d7f700ef58651367c28a1a3edd39ae
kristian <krispetter@gmail.com>
2026-06-18 14:51:00 +0200

    x

--- client/engine/os/openai/OpenAiWorld.js
+++ client/engine/os/openai/OpenAiWorld.js
@@ -15,12 +15,16 @@ export class OpenAiWorld {
 		}
 
 		Gp.left = () => {
+			Vibrate(50)
+
 			console.log("pressed left start record")
 			html.h1.text("recording")
 			Mic.start()
 		}
 
 		Gp.right = () => {
+			Vibrate(200)
+
 			html.h1.textContent = "transciribng"
 			Mic.stop(async blob => {
 				const transcribedText = await Transcribe(blob)
