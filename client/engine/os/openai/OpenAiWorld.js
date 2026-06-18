c77a394ca1cb33facf37f3e58fb1081dfb1ea9bf
kristian <krispetter@gmail.com>
2026-06-18 14:25:33 +0200

    x

--- client/engine/os/openai/OpenAiWorld.js
+++ client/engine/os/openai/OpenAiWorld.js
@@ -3,6 +3,20 @@ export class OpenAiWorld {
 	constructor() {
 		const html = Dom.add(Html.translator())
 
+		let language = "Chinese"
+
+		html.languages.add(H.button("Chinese", () => {
+			language = "Chinese"
+		}))
+
+		html.languages.add(H.button("Japanese", () => {
+			language = "Japanese"
+		}))
+
+		html.languages.add(H.button("Korean", () => {
+			language = "Korean"
+		}))
+
 		MicPermission.request(() => {
 		})
 
@@ -18,16 +32,15 @@ export class OpenAiWorld {
 				const transcribedText = await Transcribe(blob)
 				html.h1.text(transcribedText)
 
-				const translatedText = await Gpt("Translate to chinese: " + transcribedText)
+				const translatedText = await Gpt(`Translate to ${language}: ${transcribedText}`)
 				html.h1.text(translatedText)
 
-				Sound.playBlob(await Ttv(translatedText))
+				Sound.playBlob(await Ttv(translatedText, `Speak in ${language}`))
 			})
 		}
 
 	}
 
 	update() {
-
 	}
 }
