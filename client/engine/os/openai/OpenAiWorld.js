9b9a8254d0beb080eea8a7293c83b18bd1c94b4b
kristian <krispetter@gmail.com>
2026-06-19 14:34:48 +0200

    x

--- client/engine/os/openai/OpenAiWorld.js
+++ client/engine/os/openai/OpenAiWorld.js
@@ -14,27 +14,35 @@ export class OpenAiWorld {
 			}))
 		}
 
-		Gp.left = () => {
+		html.buttons.add(H.button("record", () => {
 			Vibrate(50)
 
 			console.log("pressed left start record")
 			html.h1.text("recording")
 			Mic.start()
+		}))
+		Gp.left = () => {
 		}
 
-		Gp.right = () => {
+		html.buttons.add(H.button("stop", () => {
 			Vibrate(200)
 
 			html.h1.textContent = "transciribng"
 			Mic.stop(async blob => {
-				const transcribedText = await Transcribe(blob)
-				html.h1.text(transcribedText)
+				// const transcribedText = await Transcribe(blob)
+				// html.h1.text(transcribedText)
 
-				const translatedText = await Gpt(`Translate to ${language}: ${transcribedText}`)
-				html.h1.text(translatedText)
+				// const translatedText = await Gpt(`Translate to ${language}: ${transcribedText}`)
+				// html.h1.text(translatedText)
 
-				Sound.playBlob(await Ttv(translatedText, `Speak in ${language}`))
+				// Sound.playBlob(await Ttv(translatedText, `Speak in ${language}`))
+				TestTranscribe(translatedText, t => {
+					Tts(t)
+				})
 			})
+			
+		}))
+		Gp.right = () => {
 		}
 
 	}
