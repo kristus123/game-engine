528119bc12ad593f384b1df81800725b9ec954ee
kristian <krispetter@gmail.com>
2026-06-20 10:30:07 +0200

    x

--- client/engine/quest/Task.js
+++ client/engine/quest/Task.js
@@ -1,37 +1,40 @@
 export class Task {
-	constructor(name, { onStart, onUpdate, markDoneIf, markDoneIfMoreThanMs } = {}) {
+	constructor(name, { start, onDone, markDoneIf, markDoneIfMoreThanMs } = {}) {
 		this.done = false
 		this.stopWatch = StopWatch()
 		this.started = false
+
+		this.onUpdate = () => {}
+
+		this.start = () => {
+			const {update} = start()
+			this.onUpdate = update
+
+			this.started = true
+			this.stopWatch.start()
+		}
 	}
 
 	markDone() {
 		this.done = true
 		this.stopWatch.stop()
+
+		this.onDone?.()
 	}
 
-	start() {
-		this.started = true
-		this.onStart?.()
-		this.stopWatch.start()
+	active() { // maybe not needed to be its own method
+		return this.started && !this.done
 	}
 
 	update() {
-		if (this.done) {
-			// finished running logic
-		}
-		else {
-			this.onUpdate?.()
+		if (this.active) {
+			this.onUpdate()
 
 			if (this.markDoneIf?.()) {
 				this.markDone()
 			}
 			else if (this.markDoneIfMoreThanMs && this.duration >= this.markDoneIfMoreThanMs) {
 				this.markDone()
-			}
-
-			if (this.done) {
-				onDone?.()
 			}
 		}
 	}
