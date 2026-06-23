1eb4986ce92f137784ea09b9d3fac65dd1a05517
kristian <krispetter@gmail.com>
2026-06-23 12:59:29 +0200

    x

--- transpiledBackend/server/http/endpoints/TestEndpoint.js
+++
@@ -1,5 +0,0 @@
-import { Methods } from '#root/transpiledBackend/server/http/Methods.js'; 
-
-Methods.add("test", ({ body }) => {
-	return body
-})
--- transpiledBackend/server/start.js
+++
@@ -1,8 +0,0 @@
-import { StartServer } from '#root/transpiledBackend/server/http/StartServer.js'; 
-
-// this is where stuff should init.
-// right now it inits in http/StartServer.js
-
-export function start() {
-	
-}
