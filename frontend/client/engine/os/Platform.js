a45b8321cebafcf1e6eaabb66e514d5163d5e74f
kristian <krispetter@gmail.com>
2026-06-24 15:33:15 +0200

    x

--- frontend/client/engine/os/Platform.js
+++ frontend/client/engine/os/Platform.js
@@ -1,33 +1,43 @@
 export class Platform {
 
 	static get ios() {
-		if (typeof window === "undefined" || !window.navigator) return false
+		if (typeof window == "undefined" || !window.navigator) {
+			return false
+		}
 		const ua = window.navigator.userAgent || ""
 		const platform = window.navigator.platform || ""
-		return /iPad|iPhone|iPod/.test(ua) || (platform === "MacIntel" && window.navigator.maxTouchPoints > 1)
+		return /iPad|iPhone|iPod/.test(ua) || (platform == "MacIntel" && window.navigator.maxTouchPoints > 1)
 	}
 
 	static get android() {
-		if (typeof window === "undefined" || !window.navigator) return false
+		if (typeof window == "undefined" || !window.navigator) {
+			return false
+		}
 		const ua = window.navigator.userAgent || ""
 		return /Android/i.test(ua)
 	}
 
 	static get phone() {
-		if (typeof window === "undefined" || !window.navigator) return false
+		if (typeof window == "undefined" || !window.navigator) {
+			return false
+		}
 		const ua = window.navigator.userAgent || ""
 		return this.ios || this.android || /Mobi|iPhone|iPod|iPad|Android|BlackBerry|IEMobile/i.test(ua)
 	}
 
 	static get tv() {
-		if (typeof window === "undefined" || !window.navigator) return false
+		if (typeof window == "undefined" || !window.navigator) {
+			return false
+		}
 		const ua = window.navigator.userAgent || ""
 		return /SmartTV|GoogleTV|AppleTV|HbbTV|CastNL|NetCast|Chromecast|Roku|AmazonWebServices|Opera TV|Viera/i.test(ua)
 	}
 
 	static get pwa() {
-		if (typeof window === "undefined") return false
-		return window.matchMedia("(display-mode: standalone)").matches || window.navigator.standalone === true
+		if (typeof window == "undefined") {
+			return false
+		}
+		return window.matchMedia("(display-mode: standalone)").matches || window.navigator.standalone == true
 	}
 
 	static get pc() {
@@ -35,7 +45,9 @@ export class Platform {
 	}
 
 	static get browser() {
-		if (typeof window === "undefined") return false
+		if (typeof window == "undefined") {
+			return false
+		}
 		return !this.pwa && !window.Capacitor && !window.cordova
 	}
 
