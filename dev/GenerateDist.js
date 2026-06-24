13df6f93f6d1ea29c354dd5d280590bbdb925ce7
kristian <krispetter@gmail.com>
2026-06-24 13:04:36 +0200

    x

--- dev/GenerateDist.js
+++ dev/GenerateDist.js
@@ -10,9 +10,15 @@ export function GenerateDist(env) {
 
 	Files.copyFolder(FileConfig.gameAssets, FileConfig.toDistPath(FileConfig.gameAssets))
 	Files.copyFolder(FileConfig.gameAudio, FileConfig.toDistPath(FileConfig.gameAudio))
-	// instead do
 	// Files.copyFolderToDist(FileConfig.gameAudio)
 	// Files.copyFolderToDist(FileConfig.gameAssets)
+
+	// we should have a better solution for this
+	// as we want it to be easy to place stuff in frontend/ and have it automatically be placed in dist/
+	Files.copyFile(FileConfig.favicon, FileConfig.toDistPath(FileConfig.favicon))
+	const manifest = "frontend/manifest.json"
+	Files.copyFile(manifest, FileConfig.toDistPath(manifest))
+	// we should have a better solution for this
 
 
 	const asepriteFiles = Files.at(FileConfig.asepriteAssets)
