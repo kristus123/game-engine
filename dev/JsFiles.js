2a9c5a99d288563fd557ce420619488ef3e525b8
kristian <krispetter@gmail.com>
2026-06-24 13:24:46 +0200

    x

--- dev/JsFiles.js
+++
@@ -1,6 +0,0 @@
-import { Files } from "#root/dev/Files.js"
-import { FileConfig } from "#root/FileConfig.js"
-
-export const JsFiles = Files.at(FileConfig.frontend)
-	.filter(f => f.endsWith(".js"))
-	.map(f => f.replaceAll("\\", "/")) // is this one needed?
