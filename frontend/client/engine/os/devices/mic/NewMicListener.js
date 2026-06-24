f7056860ff17f171e786e506933d06223fb5617f
sreejit <pillaisreejit94@gmail.com>
2026-06-24 18:13:35 +0530

    Refactor

--- frontend/client/engine/os/devices/mic/NewMicListener.js
+++ frontend/client/engine/os/devices/mic/NewMicListener.js
@@ -1,29 +1,25 @@
-
 export class NewMicListener {
 
-	static listener = null
+  static listener = this.createListener()
 
-	static onConnect(callback) {
-		this.listener ??= this.createListener()
-		this.listener.onConnect(callback)
-	}
+  static onConnect(callback) {
+    this.listener.onConnect(callback)
+  }
 
-	static onDisconnect(callback) {
-		this.listener ??= this.createListener()
-		this.listener.onDisconnect(callback)
-	}
+  static onDisconnect(callback) {
+    this.listener.onDisconnect(callback)
+  }
 
-	static async init() {
-		if (!MicPermission.granted) {
-			throw new Error("x")
-		}
+  static async init() {
+    if (!MicPermission.granted) {
+      throw new Error("x")
+    }
 
-		this.listener ??= this.createListener()
-		await this.listener.init()
-	}
+    await this.listener.init()
+  }
 
-	static createListener() {
-		return new NewDeviceListener(async () => AllMics.get())
-	}
+  static createListener() {
+    return new NewDeviceListener(async () => AllMics.get())
+  }
 
 }
