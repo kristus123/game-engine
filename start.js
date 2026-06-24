ce4cb990b0543dff7d9db43ab2d8cbca26b418af
kristian <krispetter@gmail.com>
2026-06-24 21:26:51 +0200

    x

---
+++ start.js
@@ -0,0 +1,9 @@
+import { GenerateBackend } from "#root/GenerateBackend.js"
+GenerateBackend() // todo pass environment - "DEVELOPMENT"
+
+// todo improve comment
+// Needs to be imported like this because the transpiled folder is non existent before and it does not like that.
+const { StartServer } = await import("#root/transpiledBackend/server/http/StartServer.js")
+const { SocketServer } = await import("#root/transpiledBackend/server/socket/SocketServer.js")
+
+StartServer()
