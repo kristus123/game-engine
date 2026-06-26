22ee8ca0b650944703a4c39cbcb6a9c82f7f22b0
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2026-06-26 10:21:47 +0530

    Update Camera.js

--- frontend/client/engine/camera/Camera.js
+++ frontend/client/engine/camera/Camera.js
@@ -14,6 +14,58 @@ export class Camera {
 		}
 	}
 
+	static top(percent = 1) {
+		const h = this.visiblePosition.height * Math.abs(percent)
+		const y = percent >= 0 ? this.visiblePosition.y : this.visiblePosition.y - h
+		return WorldPosition(
+			this.visiblePosition.x,
+			y,
+			this.visiblePosition.width,
+			h
+		)
+	}
+
+	static bottom(percent = 1) {
+		const h = this.visiblePosition.height * Math.abs(percent)
+		const y = percent >= 0 ? this.visiblePosition.y + this.visiblePosition.height - h : this.visiblePosition.y + this.visiblePosition.height
+		return WorldPosition(
+			this.visiblePosition.x,
+			y,
+			this.visiblePosition.width,
+			h
+		)
+	}
+
+	static left(percent = 1) {
+		const x = percent >= 0 ? this.visiblePosition.x : this.visiblePosition.x - w
+		return WorldPosition(
+			x,
+			this.visiblePosition.y,
+			w,
+			this.visiblePosition.height
+		)
+	}
+
+	static right(percent = 1) {
+		const w = this.visiblePosition.width * Math.abs(percent)
+		const x = percent >= 0 ? this.visiblePosition.x + this.visiblePosition.width - w : this.visiblePosition.x + this.visiblePosition.width
+		return WorldPosition(
+			x,
+			this.visiblePosition.y,
+			w,
+			this.visiblePosition.height
+		)
+	}
+
+	static get screenView() {
+		return {
+			top: (percent) => this.top(percent),
+			bottom: (percent) => this.bottom(percent),
+			left: (percent) => this.left(percent),
+			right: (percent) => this.right(percent),
+		}
+	}
+
 	static applyPositionContextThing(ctxes, run) {
 		for (const ctx of ctxes) {
 			ctx.save()
