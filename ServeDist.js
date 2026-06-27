9e56283edf1a1d96e0f34afa5d7c061d45ad3990
kristian <krispetter@gmail.com>
2026-06-27 10:39:11 +0200

    x

--- ServeDist.js
+++ ServeDist.js
@@ -4,21 +4,21 @@ import fs from "fs"
 import { FileConfig } from "#root/FileConfig.js"
 
 const mime = {
-  ".html": "text/html",
-  ".css": "text/css",
-  ".js": "application/javascript",
-  ".json": "application/json",
-  ".png": "image/png",
-  ".jpg": "image/jpeg",
-  ".jpeg": "image/jpeg",
-  ".svg": "image/svg+xml",
-  ".txt": "text/plain",
-  ".wasm": "application/wasm"
+	".html": "text/html",
+	".css": "text/css",
+	".js": "application/javascript",
+	".json": "application/json",
+	".png": "image/png",
+	".jpg": "image/jpeg",
+	".jpeg": "image/jpeg",
+	".svg": "image/svg+xml",
+	".txt": "text/plain",
+	".wasm": "application/wasm"
 }
 
 export function contentType(filePath) {
-  const ext = filePath.slice(filePath.lastIndexOf("."))
-  return mime[ext] || "application/octet-stream"
+	const ext = filePath.slice(filePath.lastIndexOf("."))
+	return mime[ext] || "application/octet-stream"
 }
 
 const server = http.createServer((req, res) => {
