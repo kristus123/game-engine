8a668b728f42a805c53311751bfe8194cade6c06
Nabir14 <mushfiquefarhannabir@gmail.com>
2026-07-01 21:33:29 +0600

    minor refactor

--- frontend/client/engine/start/LoadPersistedJson.js
+++ frontend/client/engine/start/LoadPersistedJson.js
@@ -1,29 +1,28 @@
 export async function LoadPersistedJson() {
-	try {
-		return HttpClient.allPersistedJsonFiles({
-			body: {},
-			ok: body => {
-				// 			for (let { name, content } of body) {
-				// 				PersistedJson[name] = Assert.jsonObject(content)
+	const { ok, body } = await HttpClient.allPersistedJsonFiles({
+		body: {}, 
+		ok: body => {
+			// 			for (let { name, content } of body) {
+			// 				PersistedJson[name] = Assert.jsonObject(content)
 
-				// 				Assert.notPresent(PersistedJson[name].save)
+			// 				Assert.notPresent(PersistedJson[name].save)
 
-				// 				PersistedJson[name].save = () => {
-				// 					return HttpClient.savePersistedJson({
-				//						body: {
-				// 							name: name,
-				// 							content: PersistedJson[name],
-				// 						}
-				//					})
-				// 				}
-				// 			}
-			}
-		})
+			// 				PersistedJson[name].save = () => {
+			// 					return HttpClient.savePersistedJson({
+			//						body: {
+			// 							name: name,
+			// 							content: PersistedJson[name],
+			// 						}
+			//					})
+			// 				}
+			// 			}
+		},
+	})
+
+	if (ok) {
+		return body
+	} else {
+		Toast(body)
+		console.error(body)
 	}
-	catch (e) {
-		Toast(e)
-		console.error(e)
-	}
-
 }
-
