66d15ba7a28f1812252262162d1508c69385eccb
kristian <krispetter@gmail.com>
2026-07-02 12:46:31 +0200

    risky commit. revert if crashing

--- dev/GenerateDist.js
+++ dev/GenerateDist.js
@@ -2,6 +2,7 @@ import { Files } from "#root/dev/Files.js"
 import { FileConfig } from "#root/FileConfig.js"
 import { Markdown } from "#root/dev/Markdown.js"
 import { Transpiler } from "#root/dev/Transpiler.js"
+import { PrepareIndexHtml } from "#root/dev/PrepareIndexHtml.js"
 
 // todo find better solution?
 // right now everyone creates their own list
@@ -14,87 +15,12 @@ export function GenerateDist(env) {
 		throw new Error("env cannot be null")
 	}
 
+	Files.copyFolder("frontend/", "dist/") // todo use FileConfig.js
 
 	Transpiler(env, jsFiles)
 
-	Files.copyFolder(FileConfig.gameAssets, FileConfig.toDistPath(FileConfig.gameAssets))
-	Files.copyFolder(FileConfig.gameAudio, FileConfig.toDistPath(FileConfig.gameAudio))
-	// Files.copyFolderToDist(FileConfig.gameAudio)
-	// Files.copyFolderToDist(FileConfig.gameAssets)
-
-	// we should have a better solution for this
-	// as we want it to be easy to place stuff in frontend/ and have it automatically be placed in dist/
-	Files.copyFile(FileConfig.favicon, FileConfig.toDistPath(FileConfig.favicon))
-	const manifest = "frontend/manifest.json"
-	Files.copyFile(manifest, FileConfig.toDistPath(manifest))
-	// we should have a better solution for this
-
-
-	const asepriteFiles = Files.at(FileConfig.asepriteAssets)
-		.map(f => f.replace("\\aseprite", "")) // windows compability // is this needed?
-		.map(f => f.replace(".aseprite", ""))
-		.map(f => f.startsWith("frontend/") ? f.substring("frontend/".length) : f)
-		.map(f => `/${f}`)
-		.map(f => `"${f}"`)
-		.map(f => f.replace(/\\/g, "/"))
-	Files.replace(FileConfig.engineIndex, "ASEPRITE_FILES", `[${asepriteFiles}]`)
-
-	const names = Files.at(FileConfig.client)
-		.filter(f => f.endsWith(".html") || f.endsWith(".md"))
-		.map(f => f.split("/").pop().replace(/\.html$/, "").replace(/\.md$/, ""))
-
-	const seen = new Set()
-	for (const name of names) {
-		if (seen.has(name)) {
-			throw new Error(`Duplicate file name found: ${name}`)
-		}
-		seen.add(name)
-	}
-
-	const htmlContents = Files.at(FileConfig.client) // rename to htmlTemplates
-		.filter(f => !f.includes("index.html"))
-		.filter(f => f.endsWith(".html") || f.endsWith(".md"))
-		.map(f => {
-			let content = Files.read(f)
-
-			if (f.endsWith(".md")) {
-				content = Markdown.toHtml(content)
-			}
-
-			content = content
-				.replace("\n", "")
-				.replace(/\s+/g, " ")
-				.trim()
-
-			const name = f.split("/").pop()
-				.replace(/\.html$/, "")
-				.replace(/\.md$/, "")
-
-			return JSON.stringify({ name: name, content: content })
-		})
-
-	Files.replace(FileConfig.engineIndex, "HTML_CONTENTS", `[${htmlContents}]`) // rename to HTML_TEMPLATES
-
-	const audioFiles = Files.at(FileConfig.gameAudio)
-		.filter(f => f.toLowerCase().endsWith(".mp3"))
-		.map(f => f.startsWith("frontend/") ? f.substring("frontend/".length) : f)
-		.map(f => `/${f}`)
-		.map(f => `"${f}"`)
-		.map(f => f.replace(/\\/g, "/"))
-	Files.replace(FileConfig.engineIndex, "AUDIO_FILES", `[${audioFiles}]`)
-
-	const cssImports = Files.at(FileConfig.cssFolder)
-		.map(f => f.replaceAll("\\", "/")) // windows compability
-		.map(f => Files.read(f))
-		.join("\n")
-
-	const indexHtml = Files.read(FileConfig.index_html)
-		.replace("CSS_IMPORTS", cssImports)
-	Files.write(FileConfig.toDistPath(FileConfig.index_html), indexHtml)
+	PrepareIndexHtml()
 }
-
-
-
 
 import { fileURLToPath } from "url"
 if (process.argv[1] == fileURLToPath(import.meta.url)) {
