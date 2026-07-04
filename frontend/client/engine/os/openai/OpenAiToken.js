3b30c6aa61b51fc311606d948c07070ead3d9ae5
kristian <krispetter@gmail.com>
2026-07-04 11:11:52 +0200

    x

--- frontend/client/engine/os/openai/OpenAiToken.js
+++ frontend/client/engine/os/openai/OpenAiToken.js
@@ -1,11 +1,11 @@
-// const token = localStorage.getItem("openaiToken")
+const token = localStorage.getItem("openaiToken")
 
-// if (token == null) {
-// 	const value = prompt("Paste your openAI token")
-// 	localStorage.setItem("openaiToken", value)
-// }
-// else {
-// 	console.log(token)
-// }
+if (token == null) {
+	const value = prompt("Paste your openAI token")
+	localStorage.setItem("openaiToken", value)
+}
+else {
+	console.log(token)
+}
 
-export const OpenAiToken = "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"
+export const OpenAiToken = token
