81c4df4d390b47dba903e8f7cf0ead38b7c2f3de
kristian <krispetter@gmail.com>
2026-07-13 14:16:47 +0200

    x

--- frontend/client/engine/palette/Phone.js
+++ frontend/client/engine/palette/Phone.js
@@ -1,18 +1,18 @@
 export class Phone {
 	static get current() {
-    	if (window.innerWidth > window.innerHeight) {
-        	return "horizontal"
-    	}
+		if (window.innerWidth > window.innerHeight) {
+    		return "horizontal"
+		}
 
-    	return "vertical"
+		return "vertical"
 	}
 
 	static get horizontal() {
-    	return this.current == "horizontal"
+		return this.current == "horizontal"
 	}
 
 	static get vertical() {
-    	return this.current == "vertical"
+		return this.current == "vertical"
 	}
 
 	static onChange(callback) {
@@ -20,8 +20,8 @@ export class Phone {
 
 	static {
 		// in the future all addEventListener should be moved into one file, to make it easy to keep track of which event listeners are used.
-    	window.addEventListener("resize", () => {
+		window.addEventListener("resize", () => {
 			// trigger resize
-    	})
+		})
 	}
 }
