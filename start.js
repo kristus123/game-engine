dcc88c113288409ff561e5d1ee77831c900f8445
kristian <krispetter@gmail.com>
2026-07-19 13:44:57 +0200

    x

--- start.js
+++ start.js
@@ -1,6 +1,7 @@
 import { AllImports } from "#root/AllImports.js"
 const { GenerateBackend } = AllImports
-GenerateBackend() // todo pass environment - "DEVELOPMENT"
+
+GenerateBackend(process.argv[2]) // todo pass environment - "DEVELOPMENT"
 
 // todo improve comment
 // Needs to be imported like this because the transpiled folder is non existent before and it does not like that.
