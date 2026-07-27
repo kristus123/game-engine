3358a0fa4552f6531dc079f9473d6ac9332e57ba
Nabir14 <mushfiquefarhannabir@gmail.com>
2026-07-27 18:49:49 +0600

    temporary workaround

--- prod/deploy.sh
+++ prod/deploy.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-cd game-engine/
+# cd game-engine/
 
-if [ "$(git branch --show-current)" != "master" ]; then
-    echo "Error: You must be on the master branch"
-    exit 1
-fi
+# if [ "$(git branch --show-current)" != "master" ]; then
+#     echo "Error: You must be on the master branch"
+#     exit 1
+# fi
 
-git pull
+# git pull
 
 echo "restarting game-engine"
 
