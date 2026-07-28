67978bcf23f58a1907a8de4df92ba5ef656d4fc8
kristian <krispetter@gmail.com>
2026-07-28 12:15:17 +0200

    x

--- prod/redeploy_if_changes.sh
+++ prod/redeploy_if_changes.sh
@@ -8,15 +8,16 @@ LOCAL=$(git rev-parse HEAD)
 REMOTE=$(git rev-parse @{u})
 
 if [ "$LOCAL" != "$REMOTE" ]; then
-    echo "New changes found. Updating..."
+    echo "New changes found"
+    echo "Redeploying"
 
     git pull --ff-only
 
     sudo systemctl restart game-engine.service
 
-    echo "new changes detected"
+    echo "New changes detected"
     echo "Restarted game-engine.service"
 else
     echo "Already up to date."
-    echo "not redeploying"
+    echo "Not redeploying"
 fi
