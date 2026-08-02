44a25b837d54691bfc3567925d57bbe79d67f8e0
kristian <krispetter@gmail.com>
2026-08-02 12:26:37 +0200

    Lskadjf (#345)
    * lskadjf
    
    * lint
    
    * x
    
    * lint
    
    * x

--- frontend/client/game/code/Kristian.js
+++ frontend/client/game/code/Kristian.js
@@ -33,33 +33,36 @@ export class Kristian {
 
 	constructor() {
 		const html = Html.matte()
+		Dom.add(html)
 
+		const challenge = ["1", "2", "tre", "4"]
 
-		for (const x of [4, 3, 2, 1]) {
+		for (const number of challenge.shuffle()) {
 			html.list.add(`
-				<div data-value="${x}" draggable style="width: 100px; height: 100px" class="bgWhite">
-					<p style="font-size:65px" center class="textWhite">${x}</p>
+				<div bg-nature data-number="${number}" draggable style="min-width: 100px; min-height: 100px" class="bgWhite">
+					<p style="font-size:85px" center class="textWhite">${number}</p>
 				</div>
 			`.toHtml())
 		}
 
-		Dom.add(html)
 		setTimeout(() => {
 			const copy = html.h1.copy()
 			copy.invisible()
 			html.main.prepend(copy)
-			shrink(copy, 1000)
+			shrink(copy, 800)
 			// unshrink(html.h1)
-			html.h1.animateTowards(html.otherBox, 200)
-		}, 500)
+			html.h1.animateTowards(html.otherBox, 400)
+		}, 200)
 
 		let placeholder = null
 
 		DomMouse.onDrag = (e) => {
+			Tts(e.data.number)
+			Mix.fx.play(Mp3.click)
 
 			placeholder = e.cloneNode(true)
 			Dom.floating(placeholder)
-			placeholder.followMouse()
+			placeholder.followMouse(0, -50)
 
 			e.invisible()
 		}
@@ -70,9 +73,16 @@ export class Kristian {
 
 			e.visible()
 
-			const c = html.list.map(c => c.data.value)
-			if (c.same(["1", "2", "3", "4"])) {
+			const c = html.list.map(c => c.data.number)
+			if (c.same(challenge)) {
 				html.h1.textContent = "BRA"
+
+				html.list.forEach((x, i) => {
+					setTimeout(() => {
+						x.bounce()
+						Mix.fx.play(Mp3.click)
+					}, 200 * i)
+				})
 			}
 		}
 
@@ -80,7 +90,7 @@ export class Kristian {
 		}
 
 		DomMouse.whileDragging = (e) => {
-			placeholder?.followMouse()
+			placeholder?.followMouse(0, -50)
 
 			for (const h of DomMouse.hovering) { // find better solution for this
 				const list = h.closest("[sortable]")
