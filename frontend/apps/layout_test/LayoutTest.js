d30d944d7143d06b722958ab69b28c14d3df58b1
kristian <krispetter@gmail.com>
2026-08-13 22:55:27 +0200

    x

--- frontend/apps/layout_test/LayoutTest.js
+++ frontend/apps/layout_test/LayoutTest.js
@@ -3,11 +3,18 @@ export class LayoutTest {
 	constructor() {
 		const html = Html.layoutTest()
 		Dom.add(html)
-		// html.test.scrollIntoView({
-		// 	behavior: "smooth",
-		// 	block: "center",
-		// 	inline: "center"
-		// })
+		for (const c of Iterate(20, () => ({
+			title: Random.choice(["wow", "yo", "omg"]),
+			p: Random.integerBetween(-30, 30),
+		}))) {
+			html.list.add(`
+				<div max-size class="bgWhite" style="top: ${c.p}%; width:300px; height:30%;">
+					<h1 float-top-mid>${c.title}</h1>
+					<h1 float-bot-right>swag</h1>
+					</div>
+				</div>
+			`.toHtml())
+		}
 	}
 
 	update() {
