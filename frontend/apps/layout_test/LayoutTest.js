7353432f346c419bb584dbe43a708d8b2668c8e3
kristian <krispetter@gmail.com>
2026-08-14 10:40:25 +0200

    x

--- frontend/apps/layout_test/LayoutTest.js
+++ frontend/apps/layout_test/LayoutTest.js
@@ -3,16 +3,15 @@ export class LayoutTest {
 	constructor() {
 		const html = Html.layoutTest()
 		Dom.add(html)
-		for (const c of Iterate(20, () => ({
+		for (const c of Iterate(80, () => ({
 			title: Random.choice(["wow", "yo", "omg"]),
 			p: Random.integerBetween(-30, 30),
 		}))) {
 			html.list.add(`
-				<div max-size class="bgWhite" style="top: ${c.p}%; width:700px; height:40%;">
+				<div class="bgWhite" style="top: ${c.p}%; width:100px; height:40%;">
 					<h1 float-top-mid>${c.title}</h1>
 					<h1 float-bot-right>swag</h1>
 					<h1 float-bot-left>yolo</h1>
-					</div>
 				</div>
 			`.toHtml())
 		}
