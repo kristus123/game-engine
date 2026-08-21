39db85dc3117ec5cd962bc7cd500ba971d8fcacf
kristian <krispetter@gmail.com>
2026-08-21 13:40:14 +0200

    x

--- frontend/index.js
+++ frontend/index.js
@@ -37,7 +37,7 @@ Promise.all([
 		initD2(Draw(Palette.d2.ctx))
 		initD3(Draw(Palette.d3.ctx))
 
-		const activeThing = Livestream()
+		const activeThing = CodeEditor()
 
 		GameLoop.start(() => {
 			Palette.main.fill("#10204f")
