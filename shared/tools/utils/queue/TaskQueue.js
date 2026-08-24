1ec704bf3c52e34c276c6c1ceea78b0a7e7635c6
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2026-08-24 18:30:13 +0530

    eslint fix

--- shared/tools/utils/queue/TaskQueue.js
+++ shared/tools/utils/queue/TaskQueue.js
@@ -1,35 +1,38 @@
 export class TaskQueue {
-	static {
-		this.global = new TaskQueue()
-	}
+    static {
+        this.global = new TaskQueue()
+    }
 
-	constructor() {
-		this.tasks = []
-		this.isProcessing = false
-	}
+    constructor() {
+        this.tasks = []
+        this.isProcessing = false
+    }
 
-	add(task) {
-		return new Promise((resolve, reject) => {
-			this.tasks.push(async () => {
-				try {
-					resolve(await task())
-				} catch (e) {
-					reject(e)
-				}
-			})
-			this.process()
-		})
-	}
+    add(task) {
+        return new Promise((resolve, reject) => {
+            this.tasks.push(async () => {
+                try {
+                    resolve(await task())
+                }
+                catch (e) {
+                    reject(e)
+                }
+            })
+            this.process()
+        })
+    }
 
-	async process() {
-		if (this.isProcessing || this.tasks.length === 0) return
-		this.isProcessing = true
+    async process() {
+        if (this.isProcessing || this.tasks.length === 0) {
+            return
+        }
+        this.isProcessing = true
 
-		while (this.tasks.length > 0) {
-			const task = this.tasks.shift()
-			await task()
-		}
+        while (this.tasks.length > 0) {
+            const task = this.tasks.shift()
+            await task()
+        }
 
-		this.isProcessing = false
-	}
+        this.isProcessing = false
+    }
 }
