5c596877fe2a8bc9ab3f5d23e3dc898d0d365429
kristian <krispetter@gmail.com>
2026-08-25 11:50:42 +0200

    x

--- shared/Log.js
+++ shared/Log.js
@@ -1,12 +1,28 @@
 export const SessionId = Random.uuid()
 
-export function Log(...args) {
-
-	console.log(args)
-
+function _sendToDiscord(args) {
 	fetch(`${Config.httpUrl}/log`, {
 		method: "POST",
 		headers: { "Content-Type": "application/json" },
 		body: JSON.stringify({ sessionId: SessionId, message: args.join(" ") })
 	}).catch(() => {})
+}
+
+export function Log(...args) {
+	console.log(args)
+	_sendToDiscord(args)
+}
+
+Log.info = (...args) => {
+	throw new Error("don't use Log.info, just use Log(x)")
+}
+
+Log.warn = (...args) => {
+	console.warn(args)
+	_sendToDiscord(args)
+}
+
+Log.error = (...args) => {
+	console.warn(args)
+	_sendToDiscord(args)
 }
