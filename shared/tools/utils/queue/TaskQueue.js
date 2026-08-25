8460a45386fedbe97bb49797ae4630773f43af4f
kristian <krispetter@gmail.com>
2026-08-25 12:11:59 +0200

    x

--- shared/tools/utils/queue/TaskQueue.js
+++ shared/tools/utils/queue/TaskQueue.js
@@ -1,38 +1,38 @@
 export class TaskQueue {
 	static {
-    	this.global = new TaskQueue()
+		this.global = new TaskQueue()
 	}
 
 	constructor() {
-    	this.tasks = []
-    	this.isProcessing = false
+		this.tasks = []
+		this.isProcessing = false
 	}
 
 	add(task) {
-    	return new Promise((resolve, reject) => {
-        	this.tasks.push(async () => {
-            	try {
-                	resolve(await task())
-            	}
-            	catch (e) {
-                	reject(e)
-            	}
-        	})
-        	this.process()
-    	})
+		return new Promise((resolve, reject) => {
+    		this.tasks.push(async () => {
+        		try {
+            		resolve(await task())
+        		}
+        		catch (e) {
+            		reject(e)
+        		}
+    		})
+    		this.process()
+		})
 	}
 
 	async process() {
-    	if (this.isProcessing || this.tasks.length == 0) {
-        	return
-    	}
-    	this.isProcessing = true
+		if (this.isProcessing || this.tasks.length == 0) {
+    		return
+		}
+		this.isProcessing = true
 
-    	while (this.tasks.length > 0) {
-        	const task = this.tasks.shift()
-        	await task()
-    	}
+		while (this.tasks.length > 0) {
+    		const task = this.tasks.shift()
+    		await task()
+		}
 
-    	this.isProcessing = false
+		this.isProcessing = false
 	}
 }
