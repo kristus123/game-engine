7bd7215491d56cf2c8c8866057dc41d85120a579
kristian <krispetter@gmail.com>
2026-08-25 11:29:53 +0200

    x

--- frontend/apps/code_editor/TestFile.js
+++ frontend/apps/code_editor/TestFile.js
@@ -1,35 +1,35 @@
-export const TestFile = `
-const users = ["bob", "alice", "john"]
+export const TestFile = (`
+	const users = ["bob", "alice", "john"]
 
-function getUser(name) {
-	for (let i = 0; i < users.length; i++) {
-		if (users[i] == name) {
-			return users[i]
+	function getUser(name) {
+		for (let i = 0; i < users.length; i++) {
+			if (users[i] == name) {
+				return users[i]
+			}
+		}
+		return null
+	}
+
+	function printUsers() {
+		for (let i = 0; i < users.length; i++) {
+			console.log("USER: " + users[i])
 		}
 	}
-	return null
-}
 
-function printUsers() {
-	for (let i = 0; i < users.length; i++) {
-		console.log("USER: " + users[i])
+	function addUser(name) {
+		if (name != null && name != "") {
+			users.push(name)
+		}
 	}
-}
 
-function addUser(name) {
-	if (name != null && name != "") {
-		users.push(name)
+	let user = getUser("bob")
+
+	if (user) {
+		console.log("found " + user)
+	} else {
+		console.log("not found")
 	}
-}
 
-let user = getUser("bob")
-
-if (user) {
-	console.log("found " + user)
-} else {
-	console.log("not found")
-}
-
-addUser("karen")
-printUsers()
-`
+	addUser("karen")
+	printUsers()
+`).dedent()
