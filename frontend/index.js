620419ab05939b544a5132840311de811667930b
kristian <krispetter@gmail.com>
2026-08-27 18:34:25 +0200

    x

--- frontend/index.js
+++ frontend/index.js
@@ -18,7 +18,6 @@ document.addEventListener("contextmenu", e => e.preventDefault());
 SocketClient.connect(() => {
 })
 
-
 SocketClient.onServerMessage("HOT_RELOAD", () => {
 	Dom.overlay(H.p("RELOADING").css("color:white; font-size:150px;"))
 	location.reload()
