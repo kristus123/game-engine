a23d8eee7d086513b9126cc008c256bd6e222e61
kristian <krispetter@gmail.com>
2026-09-02 12:14:49 +0200

    x

--- frontend/apps/livestream/Livestream.js
+++ frontend/apps/livestream/Livestream.js
@@ -2,6 +2,7 @@ export class Livestream {
 
 	static async start() {
 		const html = Page.init("index", H.create("livestream-body"))
+		Page.go("index")
 
 		SocketClient.onClientMessage("NEW_CHAT_MESSAGE", data => {
 			html.chatHistory.add(H.create("chat-line", {
@@ -11,17 +12,6 @@ export class Livestream {
 				},
 			}))
 		})
-
-		Page.go("index")
-
-		!async function() {
-			await Sim.click(html.openChat)
-
-			await Sim.type(html.message, "hello")
-
-			await Sim.click(() => html.send)
-			await Sim.click(html.closeChat)
-		}()
 	}
 
 	update() {
