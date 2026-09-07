353888759959dbc6f88e179cb6714c60115a775e
kristian <krispetter@gmail.com>
2026-09-07 13:04:46 +0200

    x

--- frontend/apps/rpg_sami_game/Toast.js
+++ frontend/apps/rpg_sami_game/Toast.js
@@ -1,5 +1,9 @@
 export function Toast(text) {
-	const t = Dom.add(Html.toast())
+	const t = Dom.add(`
+		<overlay>
+			<p class="bgWhite">${text}</p>
+		</overlay>
+	`.toHtml())
 	t.div.text(text)
 	setTimeout(() => {
 		t.remove()
