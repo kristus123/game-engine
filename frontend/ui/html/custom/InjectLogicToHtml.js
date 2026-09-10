0252622ea5f149c6f9f83433f5e7c27b25c59ebc
kristian <krispetter@gmail.com>
2026-09-10 11:07:24 +0200

    x

---
+++ frontend/ui/html/custom/InjectLogicToHtml.js
@@ -0,0 +1,38 @@
+export function InjectLogicToHtml(html, methods) {
+	html.walk(child => {
+		for (const attribute of child.attributes) {
+			switch (attribute.name) {
+				case "on-click-go-page":
+					child.listen("click", () => {
+						Page.go(attribute.value)
+					})
+				case "on-click":
+					child.listen("click", () => {
+						methods[attribute.value]?.()
+					})
+				case "on-click-set-state":
+					child.listen("click", () => {
+						html.walk(c => {
+							const showIf = c.getAttribute("show-if-state")
+
+							if (showIf) {
+								if (showIf == attribute.value) {
+									c.show()
+								}
+								else {
+									c.hide()
+								}
+							}
+						})
+					})
+				case "on-enter":
+					child.onEnter(() => {
+						methods[attribute.value]?.()
+					})
+				default: {
+					// ok
+				}
+			}
+		}
+	})
+}
--- frontend/ui/html/custom/RegisterCustomWebComponent.js
+++ frontend/ui/html/custom/RegisterCustomWebComponent.js
@@ -25,81 +25,31 @@ export async function RegisterCustomWebComponent(name, html, js = null) { // no-
 	customElements.define(name, class extends HTMLElement {
 		async connectedCallback() {
 			const content = template.content.cloneNode(true)
+			console.log("___")
+			console.log(name)
 
-			const slots = {}
-
-			for (const child of this.children) {
-				const slotName =
-					child.getAttribute("slot") ||
-					child.getAttribute("name") ||
-					""
-
-				if (!slots[slotName]) {
-					slots[slotName] = []
-				}
-
-				slots[slotName].push(child)
-			}
-
-			for (const slot of content.querySelectorAll("slot")) {
-				const slotName = slot.getAttribute("name") || ""
-				const nodes = slots[slotName]
-
-				if (nodes) {
-					slot.replaceWith(...nodes.map(node => node.cloneNode(true)))
-				}
-				else {
-					slot.replaceWith(...slot.childNodes)
+			for (const actualSlot of this.querySelectorAll("slot")) {
+				for (const slot of content.querySelectorAll("slot")) {
+					actualSlot.replaceWith()
 				}
 			}
 
 			this.replaceChildren(content)
 
-			this.walk(child => {
+			console.log(this)
+			console.log("___")
+			console.log("")
+			console.log("")
+			console.log("")
+
+			this.walk(child => { // needs to run before js.default is called
 				if (child.hasAttribute("id")) {
 					this[child.getAttribute("id")] = child
 				}
 			})
 
-			const { slots: slotMethods = {}, methods = {} } =
-				await js?.default({ html: this }) ?? {}
-
-			this.walk(child => {
-				for (const attribute of child.attributes) {
-					switch (attribute.name) {
-						case "on-click-go-page":
-							child.listen("click", () => {
-								Page.go(attribute.value)
-							})
-						case "on-click":
-							child.listen("click", () => {
-								methods[attribute.value]?.()
-							})
-						case "on-click-set-state":
-							child.listen("click", () => {
-								this.walk(c => {
-									const showIf = c.getAttribute("show-if-state")
-
-									if (showIf) {
-										if (showIf == attribute.value) {
-											c.show()
-										}
-										else {
-											c.hide()
-										}
-									}
-								})
-							})
-						case "on-enter":
-							child.onEnter(() => {
-								methods[attribute.value]?.()
-							})
-						default: {
-							// ok
-						}
-					}
-				}
-			})
+			const { methods = {} } = await js?.default({ html: this }) ?? {}
+			InjectLogicToHtml(this, methods)
 		}
 	})
 }
