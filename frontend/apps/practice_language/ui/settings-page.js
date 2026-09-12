d0db1c8d29bc1f737ff5f367f933b555e0f95f5a
kristian <krispetter@gmail.com>
2026-09-12 11:57:06 +0200

    x

--- frontend/apps/practice_language/ui/settings-page.js
+++ frontend/apps/practice_language/ui/settings-page.js
@@ -1,11 +1,10 @@
 export default async ({ html, setState }) => {
 
-	Pwa.init(() => {
-		html.mid.add(H.button("install", () => {
-			Pwa.install()
-		}))
-	})
-
 	return {
+		methods: {
+			installPwa: () => {
+				Pwa.install()
+			},
+		},
 	}
 }
--- frontend/tools/utils/device/Pwa.js
+++ frontend/tools/utils/device/Pwa.js
@@ -2,20 +2,26 @@ export class Pwa {
 
 	static deferredPrompt = null
 
-	static init(onReady) {
+	static init() {
+
+	}
+
+	static {
 		window.addEventListener("beforeinstallprompt", e => {
 			e.preventDefault()
 			this.deferredPrompt = e
-			onReady?.()
+			console.log("ready!")
 		})
 
 		window.addEventListener("appinstalled", () => {
+			console.log("appinstalled")
 			this.deferredPrompt = null
 		})
 	}
 
 	static async install() {
 		if (this.deferredPrompt) {
+			console.log("rock yolo")
 			this.deferredPrompt.prompt()
 			const choiceResult = await this.deferredPrompt.userChoice
 			console.log("User choice:", choiceResult.outcome)
