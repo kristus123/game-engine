24ccbd53e71bf4cfad0b5fca70c7c6c69a053b17
kristian <krispetter@gmail.com>
2026-09-14 22:48:26 +0200

    x

--- frontend/CanvasGame.js
+++ frontend/CanvasGame.js
@@ -2,65 +2,63 @@ import { initD1 } from "#root/tools/game/start/draw_layers/D1.js"
 import { initD2 } from "#root/tools/game/start/draw_layers/D2.js"
 import { initD3 } from "#root/tools/game/start/draw_layers/D3.js"
 
-export function CanvasGame(activeThing) {
+export async function CanvasGame(activeThing) {
 
-	Promise.all([
-		Promise.all(AssetPaths.aseprite.map(LoadAsepriteAssets)),
-		LoadAllAudio(AssetPaths.audio),
-		LoadAllImages(AssetPaths.image),
-		// LoadPersistedJson(),
-	])
-		.then((x) => {
+	await Promise.all(AssetPaths.aseprite.map(LoadAsepriteAssets))
+	await LoadAllAudio(AssetPaths.audio)
+	await LoadAllImages(AssetPaths.image)
+	// await LoadPersistedJson()
 
-			Mouse.initialize()
-			Camera.initialize()
-			Mouse.initializeAfterCameraIsInitialized()
+	try {
+		Mouse.initialize()
+		Camera.initialize()
+		Mouse.initializeAfterCameraIsInitialized()
 
-			initD1(Draw(Palette.d1.ctx))
-			initD2(Draw(Palette.d2.ctx))
-			initD3(Draw(Palette.d3.ctx))
+		initD1(Draw(Palette.d1.ctx))
+		initD2(Draw(Palette.d2.ctx))
+		initD3(Draw(Palette.d3.ctx))
 
-			GameLoop.start(() => {
-				Palette.main.fill("#10204f")
+		GameLoop.start(() => {
+			Palette.main.fill("#10204f")
 
-				Palette.d1.clear()
-				Palette.d2.clear()
-				Palette.d3.clear()
+			Palette.d1.clear()
+			Palette.d2.clear()
+			Palette.d3.clear()
 
-				Palette.light.clear()
+			Palette.light.clear()
 
-				Physics.update()
-				Controller.update()
-				Gp.update()
+			Physics.update()
+			Controller.update()
+			Gp.update()
 
-				Shadow.updateOutsideCameraContext()
+			Shadow.updateOutsideCameraContext()
 
-				Camera.applyPositionContextThing([
-					Palette.d1.ctx,
-					Palette.d2.ctx,
-					Palette.d3.ctx,
-					Palette.light.ctx,
-				], () => {
-					Camera.update()
-					activeThing.update()
-					Mouse.update()
-					for (const c of SuperClass.all) {
-						c.showTags()
-					}
-					Light.updateInsideCameraContext()
-				})
-
-				Palette.main.apply(Palette.d3)
-				Palette.main.apply(Palette.d2)
-				Palette.main.apply(Palette.d1)
-				Palette.main.apply(Palette.light)
+			Camera.applyPositionContextThing([
+				Palette.d1.ctx,
+				Palette.d2.ctx,
+				Palette.d3.ctx,
+				Palette.light.ctx,
+			], () => {
+				Camera.update()
+				activeThing.update()
+				Mouse.update()
+				for (const c of SuperClass.all) {
+					c.showTags()
+				}
+				Light.updateInsideCameraContext()
 			})
-		})
-		.catch(e => {
-			const lines = (e.stack || "").split("\n")
 
-			Dom.swapBody(lines.map(x => H.p(x)))
-
-			throw e
+			Palette.main.apply(Palette.d3)
+			Palette.main.apply(Palette.d2)
+			Palette.main.apply(Palette.d1)
+			Palette.main.apply(Palette.light)
 		})
+	}
+	catch (e) {
+		const lines = (e.stack || "").split("\n")
+
+		Dom.swapBody(lines.map(x => H.p(x)))
+
+		throw e
+	}
 }
