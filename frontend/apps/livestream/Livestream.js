7630ff8348cc1c699d4e708e9c57f6287d244391
kristian <krispetter@gmail.com>
2026-09-20 13:46:33 +0200

    x

--- frontend/apps/livestream/Livestream.js
+++ frontend/apps/livestream/Livestream.js
@@ -1,13 +1,13 @@
 export function Livestream() {
 	if (Token.isAdmin) {
-		Page.init("index", H.create("livestream-body"))
+		Page.init("index", H.create("streamer-page"))
 		Page.go("index")
 	}
 	else if (Token.isUser) {
-		Page.init("index", H.create("livestream-body"))
+		Page.init("index", H.create("viewer-page"))
 		Page.go("index")
 	}
 	else {
-		throw new Error("unsupported role")
+		throw new Error("unsupported role: " + Token.role)
 	}
 }
--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,7 @@
 Log.sendConsoleToServer()
 
-
-
 const backendId = LocalValue("backendId", -1)
 SocketClient.onServerMessage("HOT_RELOAD_BACKEND_ID", (data) => {
-	console.log(data)
-	console.log(backendId.value)
 	if (data.backendId > backendId.value) {
 		backendId.value = data.backendId
 		Dom.overlay(H.p("RELOADING").css("color:white; font-size:150px;"))
