120de7540c661d82bf4dec391af9fd9764d4d732
kristian <krispetter@gmail.com>
2026-09-25 23:59:52 +0200

    x

--- backend/http/endpoints/hls/Ffmpeg.js
+++ backend/http/endpoints/hls/Ffmpeg.js
@@ -31,9 +31,7 @@ export class Ffmpeg {
 			"-c:v",
 			"libx264",
 			"-preset",
-			"veryfast", // or ultrafast
-			"-crf",
-			"18",
+			"ultrafast", // veryfast or ultrafast
 			"-tune",
 			"zerolatency",
 			"-threads",
@@ -42,8 +40,6 @@ export class Ffmpeg {
 			"arnndn=m=backend/http/endpoints/hls/std.rnnn:mix=0.7", // mix between 0 and 1
 			"-c:a",
 			"aac",
-			"-b:a",
-			"128k",
 			"-f",
 			"hls",
 			"-hls_time",
