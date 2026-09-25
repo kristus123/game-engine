618ba4ee2f0bd194a8c91a5c31258c486dc9dbb5
kristian <krispetter@gmail.com>
2026-09-25 12:17:38 +0200

    x

--- shared/Log.js
+++ shared/Log.js
@@ -1,9 +1,25 @@
 export class Log {
 	static toServer(...args) {
+		args = args.map(x => {
+			if (x != null && typeof x == "object") {
+				try {
+					return JSON.stringify(x)
+				}
+				catch {
+					return String(x)
+				}
+			}
+
+			return String(x)
+		})
+
 		fetch(`${Config.httpUrl}/log`, {
 			method: "POST",
 			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ sessionId: "log_channel", message: args.join(" ") })
+			body: JSON.stringify({
+				sessionId: "log_channel",
+				message: args.join(" ")
+			})
 		}).catch(e => {
 			console.error("failed to send log to discord server: " + e)
 		})
@@ -28,7 +44,7 @@ export class Log {
 
 			console[type] = (...args) => {
 				original.apply(console, args)
-				this.toServer(args)
+				this.toServer(...args)
 			}
 		}
 
@@ -39,6 +55,5 @@ export class Log {
 		window.addEventListener("unhandledrejection", e => {
 			console.log("UNHANDLED PROMISE:", e.reason)
 		})
-
 	}
 }
