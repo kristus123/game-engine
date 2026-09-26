e613b0c61fa848ab2dba318471dff548c33e116f
kristian <krispetter@gmail.com>
2026-09-26 11:39:32 +0200

    x

--- backend/http/endpoints/hls/Ffmpeg.js
+++ backend/http/endpoints/hls/Ffmpeg.js
@@ -33,9 +33,9 @@ export class Ffmpeg {
 			"-pix_fmt", // x
 			"yuv420p", // x
 			"-preset",
-			"veryfast", // veryfast or ultrafast // x
+			"fast", // fast or veryfast or ultrafast // x
 			"-crf", // x
-			"23", // x
+			"18", // x
 			"-tune",
 			"zerolatency",
 			"-threads",
